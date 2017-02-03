ca7524a136b60ebff626289769de71efdbb9f1e8
Drew Griffin <drew.griffin@signalfx.com>
2017-02-03 15:21:21 -0500

    Update build to support agent image build

--- build.sh
+++ build.sh
@@ -2,146 +2,122 @@
 set -x
 set +e
 
-BASE_PACKAGE="github.com/signalfx/neo-agent"
-CONTAINER_HOSTNAME="agent-build"
-CONTAINER_ID=""
-GOPATH="/tmp/collectd/go"
-IMAGE_ID=""
-IMAGE_TAGNAME="agent-builder:latest"
-INCLUDE_DIR="/usr/include/collectd"
-LIB_DIR="/usr/lib/collectd"
-LOCAL_BIN="./.bin"
+AGENT_IMAGE_BUILD_DIR=`mktemp -d`
+BUILD_CONTAINER_ID=""
+BUILD_IMAGE_NAME="agent-builder:latest"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOCAL_BIN="${AGENT_IMAGE_BUILD_DIR}/scripts/agent-image/bin"
 REMOTE_PROJECT="/root/work/neo-agent"
 REMOTE_BIN="$REMOTE_PROJECT/.bin"
-PACKAGES=(
+GO_PACKAGES=(
      'cmd'
      'plugins'
      'services'
    )
 
-build_image(){
-	
-  IMAGE_ID=$(docker images -q ${IMAGE_TAGNAME})
-  if [[ "$IMAGE_ID" == "" ]]; then
-    IMAGE_ID=$(docker build --tag $IMAGE_TAGNAME .)
-  fi
-  echo "$IMAGE_ID"
-}
-
-run_container(){
-    if [[ -z $CONTAINER_ID ]]; then
-        CONTAINER_NAME_ARGS=""
-    else
-        CONTAINER_NAME_ARGS="--name $CONTAINER_ID"
-    fi
-
-    CONTAINER_ID=$(docker run \
+function run_build_container(){
+    BUILD_CONTAINER_ID=$(docker run \
     -d \
-    -h $CONTAINER_HOSTNAME \
-    $CONTAINER_NAME_ARGS \
-    $IMAGE_TAGNAME tail -f /dev/null)
+    $BUILD_IMAGE_NAME tail -f /dev/null)
 
-  echo "$CONTAINER_ID"  
+  echo "$BUILD_CONTAINER_ID"
 }
 
-stop_container(){
+function stop_build_container(){
 
-  if [ "$CONTAINER_ID" != "" ]; then
-    if [[ $(docker ps | grep "$CONTAINER_ID") ]]; then
-     docker stop $CONTAINER_ID
-     echo "container $CONTAINER_ID stopped"
+  if [ "$BUILD_CONTAINER_ID" != "" ]; then
+    if [[ $(docker ps | grep "$BUILD_CONTAINER_ID") ]]; then
+     docker stop $BUILD_CONTAINER_ID
+     echo "container $BUILD_CONTAINER_ID stopped"
     else
-     echo "container $CONTAINER_ID not running (stop_container)"
+     echo "container $BUILD_CONTAINER_ID not running (stop_buld_container)"
     fi
   else
-    echo "WARN: container id not set (stop_container)"
+    echo "WARN: build container id not set (stop_build_container)"
   fi
 }
 
-remove_container(){
+function remove_build_container(){
 
-  if [ "$CONTAINER_ID" != "" ]; then
-    if [[ $(docker ps -a | grep "$CONTAINER_ID") ]]; then
-     docker rm -f $CONTAINER_ID
-     echo "container $CONTAINER_ID removed"
+  if [ "$BUILD_CONTAINER_ID" != "" ]; then
+    if [[ $(docker ps -a | grep "$BUILD_CONTAINER_ID") ]]; then
+     docker rm -f $BUILD_CONTAINER_ID
+     echo "container $BUILD_CONTAINER_ID removed"
     else
-     echo "container $CONTAINER_ID not found (remove_container)"
+     echo "container $BUILD_CONTAINER_ID not found (remove_build_container)"
     fi
   else
-    echo "WARN: container id not set (remove_container)"  
+    echo "WARN: container id not set (remove_build_container)"
   fi
 }
 
-cleanup(){
+function cleanup_build_containers(){
 
-  CONTAINERS=$(docker ps -a --filter=ancestor=${IMAGE_TAGNAME})
+  CONTAINERS=$(docker ps -a --filter=ancestor=${BUILD_IMAGE_NAME})
   while read -r line;
   do
-    CONTAINER_ID=`echo "$line" | awk '{ print $1 }'`
-    if [ "$CONTAINER_ID" != "CONTAINER" ]; then
-      stop_container
-      remove_container
+    BUILD_CONTAINER_ID=`echo "$line" | awk '{ print $1 }'`
+    if [ "$BUILD_CONTAINER_ID" != "CONTAINER" ]; then
+      stop_build_container
+      remove_build_container
     fi
   done <<< "$CONTAINERS"
 
   echo "cleanup completed"
 }
 
-build(){
+function setup_build_container(){
 
-  echo "starting build"
+  run_build_container
 
-  # setup
-  build_image
+  $(docker cp ${DIR}/scripts/build-components.sh $BUILD_CONTAINER_ID:/tmp/build-components.sh)
+  $(docker exec $BUILD_CONTAINER_ID chmod +x /tmp/build-components.sh)
+  $(docker exec $BUILD_CONTAINER_ID mkdir -p $REMOTE_PROJECT)
+  $(docker cp ${DIR}/collectd-ext $BUILD_CONTAINER_ID:$REMOTE_PROJECT/collectd-ext)
+
+  for pkg in ${GO_PACKAGES[@]}; do
+    $(docker cp ${DIR}/$pkg $BUILD_CONTAINER_ID:$REMOTE_PROJECT/)
+  done
+}
+
+function download_built_artifacts(){
 
   if [ ! -d "$LOCAL_BIN" ]; then
     mkdir -p $LOCAL_BIN
   fi
 
-  run_container
+  $(docker cp $BUILD_CONTAINER_ID:${REMOTE_BIN}/libcollectd.so ${LOCAL_BIN}/libcollectd.so)
+  $(docker cp $BUILD_CONTAINER_ID:${REMOTE_BIN}/python.so ${LOCAL_BIN}/python.so)
+  $(docker cp $BUILD_CONTAINER_ID:${REMOTE_BIN}/signalfx-agent ${LOCAL_BIN}/signalfx-agent)
+}
 
-  BUILD_COLLECTD_LIB=true
-  BUILD_AGENT=true
+function build(){
 
-  $(docker cp ./build-components.sh $CONTAINER_ID:/tmp/build-components.sh)
-  $(docker exec $CONTAINER_ID chmod +x /tmp/build-components.sh)
-  $(docker exec $CONTAINER_ID mkdir -p ${REMOTE_PROJECT})
+  echo "starting build"
 
-  if [ "$BUILD_COLLECTD_LIB" = true ]; then
-    $(docker exec $CONTAINER_ID mkdir -p ${LIB_DIR} ${INCLUDE_DIR})
-    $(docker cp ./collectd-ext $CONTAINER_ID:${REMOTE_PROJECT}/collectd-ext)
-  fi
+  # setup build dir
+  cp -r ${DIR}/scripts $AGENT_IMAGE_BUILD_DIR/
+  cp -r ${DIR}/etc ${AGENT_IMAGE_BUILD_DIR}/scripts/agent-image/
 
-  if [ "$BUILD_AGENT" = true ]; then
-    for pkg in ${PACKAGES[@]}; do
-      $(docker cp ./$pkg $CONTAINER_ID:${REMOTE_PROJECT}/)
-    done
-  fi
+  # build agent build image
+  ${AGENT_IMAGE_BUILD_DIR}/scripts/agent-builder-image/build-image.sh
 
-  # build
-  echo "building components"
-  $(docker exec $CONTAINER_ID /tmp/build-components.sh)
-  echo "finished building components"
+  # setup container for component builds
+  setup_build_container
 
-  # download built artifacts
-  if [ "$BUILD_COLLECTD_LIB" = true ]; then
-    echo "downloading built collectd library" 
-    $(docker cp $CONTAINER_ID:${REMOTE_BIN}/libcollectd.so ${LOCAL_BIN}/libcollectd.so)
-    $(docker cp $CONTAINER_ID:${REMOTE_BIN}/python.so ${LOCAL_BIN}/python.so)
-  fi
+  # build components
+  $(docker exec $BUILD_CONTAINER_ID /tmp/build-components.sh)
 
-  if [ "$BUILD_AGENT" = true ]; then
-    echo "downloading built signalfx-agent"
-    $(docker cp $CONTAINER_ID:${REMOTE_BIN}/signalfx-agent ${LOCAL_BIN}/signalfx-agent)
-  fi
+  # put built artifacts in bin for agent image build
+  download_built_artifacts
 
-  # cleanup
-  if [ -f "${LOCAL_BIN}/signalfx-agent" ]; then
-    cleanup
-    echo "build completed!"
-  else
-    echo "build failed!"
-  fi
+  # remove build containers
+  cleanup_build_containers
+
+  # build the agent image
+  ${AGENT_IMAGE_BUILD_DIR}/scripts/agent-image/build-image.sh
+
+  echo "done"
 }
 
 build
---
+++ scripts/agent-image/build-image.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -xe
+
+BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKERFILE="Dockerfile"
+IMAGE_ID=""
+IMAGE_REGISTRYHOST="quay.io"
+IMAGE_REPOSITORY="signalfuse/signalfx-agent"
+IMAGE_TAGNAME="dev-latest"
+SIGNALFX_AGENT_BIN="bin"
+SIGNALFX_AGENT_CONFIGURATIONS="etc"
+SIGNALFX_AGENT_VERSION="dev-latest"
+
+function build_image(){
+  IMAGE_NAME="${IMAGE_REGISTRYHOST}/${IMAGE_REPOSITORY}:${IMAGE_TAGNAME}"
+  IMAGE_ID=$(docker images -q $IMAGE_NAME)
+  if [ "$IMAGE_ID" != "" ]; then
+    remove_image
+  fi  
+
+  cd $BUILD_DIR
+  IMAGE_ID=$(docker build -f $DOCKERFILE \
+             --build-arg signalfx_agent_bin="$SIGNALFX_AGENT_BIN" \
+             --build-arg signalfx_agent_configurations="$SIGNALFX_AGENT_CONFIGURATIONS" \
+             --build-arg signalfx_agent_version="$SIGNALFX_AGENT_VERSION" \
+             --tag ${IMAGE_NAME} .)
+  echo "$IMAGE_ID"
+}
+
+function remove_image(){
+  if [ "$IMAGE_ID" != "" ]; then
+    if [[ $(docker images | grep "$IMAGE_ID") ]]; then
+     docker rmi $IMAGE_ID
+     echo "image $IMAGE_ID removed"
+    else
+     echo "image $IMAGE_ID not found (remove_image)"
+    fi
+  else
+    echo "WARN: image id not set (remove_image)"    
+  fi  
+}
+
+while [[  "$#" -gt "0" ]]
+do
+  key="$1"
+  case $key in
+    -b|--agent-bin-dir)
+      shift
+      SIGNALFX_AGENT_BIN="$1"
+      ;;
+    -c|--agent-configs-dir)
+      shift
+      SIGNALFX_AGENT_CONFIGURATIONS="$1"
+      ;;
+    -d|--build-dir)
+      shift
+      BUILD_DIR="$1"
+      ;;
+    -f|--dockerfile)
+      shift
+      DOCKERFILE="$1"
+      ;;
+    -t|--image-tagname)
+      shift
+      IMAGE_TAGNAME="$1"
+      ;;
+    -v|--agent-version)
+      shift
+      SIGNALFX_AGENT_VERSION="$1"
+      ;;
+    *)
+      echo "Unknown Option"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+build_image
