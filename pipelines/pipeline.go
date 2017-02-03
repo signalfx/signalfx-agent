be79968f5c99545cb4489a8b4d1146a03ecbf6a9
Jay Camp <jay@signalfx.com>
2017-02-02 23:24:05 -0500

    Introduce concept of pipelines
    A pipeline is a series of source/sinks that consumes/produces service
    instances. Docker is a source, service rule filters are source and sinks, and
    collectd is a sink.

---
+++ pipelines/pipeline.go
@@ -0,0 +1,96 @@
+package pipelines
+
+import (
+	"errors"
+
+	"fmt"
+
+	"log"
+
+	"github.com/signalfx/neo-agent/plugins"
+	"github.com/signalfx/neo-agent/services"
+)
+
+// Source is an interface that only produces service instances
+type Source interface {
+	Read() (services.ServiceInstances, error)
+}
+
+// Sink is an interface that only consumes service instances
+type Sink interface {
+	Write(services.ServiceInstances) error
+}
+
+// SourceSink is an interface that consumes and products service instances
+type SourceSink interface {
+	Map(services.ServiceInstances) (services.ServiceInstances, error)
+}
+
+// Pipeline is a series of sink/source plugins to execute in a specific order
+type Pipeline struct {
+	name    string
+	plugins []plugins.IPlugin
+}
+
+// NewPipeline creates a Pipeline instance
+func NewPipeline(name string, pluginNames []string, plugins []plugins.IPlugin) (*Pipeline, error) {
+	pipeline := &Pipeline{name, nil}
+
+	// TODO: Should probably have a map of name -> plugin instance.
+	for _, pluginName := range pluginNames {
+		found := false
+		for _, pluginInstance := range plugins {
+			if pluginName == pluginInstance.Name() {
+				pipeline.plugins = append(pipeline.plugins, pluginInstance)
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return nil, fmt.Errorf("unable to find plugin instance for %s", pluginName)
+		}
+	}
+
+	return pipeline, nil
+}
+
+// Execute runs steps sequentially in the pipeline (returns after all stages have been run once)
+func (pipeline *Pipeline) Execute() error {
+	var services services.ServiceInstances
+	var err error
+
+	for i, s := range pipeline.plugins {
+		// Verify that the first entry in the pipeline is a source.
+		if i == 0 {
+			switch s.(type) {
+			case Source:
+			default:
+				return errors.New("first entry in pipeline must be a source")
+			}
+		}
+
+		// TODO - send cloned observer results to each sink/source?
+		switch t := s.(type) {
+		case Source:
+			log.Printf("reading source %s", t)
+			if services, err = t.Read(); err != nil {
+				return err
+			}
+		case Sink:
+			log.Printf("writing source %s", t)
+			if err := t.Write(services); err != nil {
+				return err
+			}
+		case SourceSink:
+			log.Printf("reading/writing source %s", t)
+			if services, err = t.Map(services); err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("%s does not support source/sink interface", s)
+		}
+	}
+
+	return nil
+}
