d7c8614a3882548b64699f1899b6a0f0de005984
Jay Camp <jay@signalfx.com>
2017-02-17 19:52:05 -0500

    Clean up circleci

---
+++ scripts/circle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ "$1" = "test" ]; then
+    go get github.com/tebeka/go2xunit
+    go get github.com/golang/lint/golint
+    mkdir -p $CIRCLE_TEST_REPORTS/reports
+
+    cd ../.go_workspace/src/github.com/signalfx/neo-agent
+    make lint
+    go test -v `glide novendor` | go2xunit > $CIRCLE_TEST_REPORTS/reports/unit.xml
+fi
