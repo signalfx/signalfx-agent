78c45692b14babe8bf89be3fb9a42fb5f927107d
Ben Keith <bkeith@signalfx.com>
2017-07-15 19:01:13 -0400

    Docker Multi-stage Build
    This replaces the two previous Dockerfiles and build script and replaces it
    with a single Dockerfile and a much simplified build script.  Should be much
    more efficient on changes and hopefully will increase dev cycle efficiency.
    
    Compiling collectd and using only compiled artifacts instead of the previous
    technique of installing collectd package in the final image.  The way that the
    build script selectively copied collectd plugins was rather convoluted.
    
    Installing collectd Python plugins from a script instead of directly in the
    Dockerfile.  There is now a file (`scripts/collectd-plugins.yaml`) that defines
    what plugins we want in the neo-agent and how to get them.
    
    Getting rid of the previous startup script that supported adding files through
    envvars.  They didn't appear to be used and it would be much better to just
    extend this Docker image to include files rather than using those.

---
+++ scripts/get-collectd-plugins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -exo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+y() {
+  filter=$1
+  # yq is like jq for yaml
+  cat $SCRIPT_DIR/collectd-plugins.yaml | yq -r "$filter"
+}
+
+mkdir -p /usr/share/collectd
+
+for i in $(seq 0 $(y '. | length - 1'))
+do
+  plugin_name=$(y ".[$i].name")
+  version=$(y ".[$i].version")
+  repo=$(y ".[$i].repo")
+  plugin_dir=/usr/share/collectd/${plugin_name}
+
+  git clone --branch $version --depth 1 --single-branch https://github.com/${repo}.git $plugin_dir
+  rm -rf $plugin_dir/.git
+
+  pip_install='pip install --target=/opt/collectd-py'
+
+  if $(y ".[$i] | has(\"pip_packages\")")
+  then
+    $pip_install $(y ".[$i].pip_packages | join(\" \")")
+  elif [[ -f ${plugin_dir}/requirements.txt ]]
+  then
+    $pip_install -r ${plugin_dir}/requirements.txt
+  fi
+
+  if $(y ".[$i] | has(\"can_remove\")")
+  then
+    for j in $(seq 0 $(y ".[$i].can_remove | length - 1"))
+    do
+      rm -rf ${plugin_dir}/$(y ".[$i].can_remove[$j]")
+    done
+  fi
+done
