5e212491b9f4566ba4bda575924f3522ad4ca230
Ben Keith <bkeith@signalfx.com>
2017-11-29 15:01:27 -0500

    Fixing docker label exclusion filters

--- monitors/collectd/docker/docker.go
+++ monitors/collectd/docker/docker.go
@@ -23,10 +23,10 @@ func init() {
 // Config is the monitor-specific config with the generic config embedded
 type Config struct {
 	config.MonitorConfig
-	DockerURL      string   `yaml:"dockerURL"`
-	ExcludedImages []string `yaml:"excludedImages"`
-	ExcludedNames  []string `yaml:"excludedNames"`
-	ExcludedLabels []string `yaml:"excludedLabels"`
+	DockerURL      string            `yaml:"dockerURL"`
+	ExcludedImages []string          `yaml:"excludedImages"`
+	ExcludedNames  []string          `yaml:"excludedNames"`
+	ExcludedLabels map[string]string `yaml:"excludedLabels"`
 }
 
 // Validate will check the config before the monitor is instantiated
