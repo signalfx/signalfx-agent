4939af009a08e9b2533737c05b75383b4345f84e
Jeff Cheng <jcheng@signalfx.com>
2018-04-03 11:24:14 -0400

    Deleted test-kube.sh

--- test-kube.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-docker run -d -p 5000:5000 --restart always --name registry registry 2>/dev/null || true
-docker tag quay.io/signalfx/signalfx-agent-dev:stage-final-image localhost:5000/signalfx-agent-dev
-docker push localhost:5000/signalfx-agent-dev
-
-mkdir -p test_output
-py3clean tests
-pytest -n auto --junitxml=test_output/integration_tests.xml --html=test_output/integration_tests.html --self-contained-html -m kubernetes --k8s_version=all --verbose tests
