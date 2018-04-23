85066c95c67226b4bce78ffabae932f189302256
Jeff Cheng <jcheng@signalfx.com>
2018-04-23 16:29:00 -0400

    Added global CONFIG var.

--- tests/kubernetes/services/nginx.py
+++ tests/kubernetes/services/nginx.py
@@ -1,27 +1,38 @@
 from tests.kubernetes import utils
 
-def deploy(configmap_data={}, image="nginx:latest", port=80, replicas=3, labels={"app": "nginx"}, namespace="default"):
-    if not configmap_data:
-        configmap_data = {"default.conf": '''
+CONFIG = {
+    "name": "nginx",
+    "pod_name": "nginx-replication-controller",
+    "image": "nginx:latest",
+    "port": 80,
+    "replicas": 3,
+    "labels": {"app": "nginx"},
+    "namespace": "default",
+}
+
+def deploy():
+    configmap_data = {
+        "default.conf": """
             server {
-                listen 80;
+                listen %d;
                 server_name  localhost;
                 location /nginx_status {
                     stub_status on;
                     access_log off;
                     allow all;
                 }
-            }'''}
+            }""" % CONFIG["port"]
+    }
     utils.create_configmap(
         name="nginx-status",
         data=configmap_data,
-        labels=labels,
-        namespace=namespace)
+        labels=CONFIG["labels"],
+        namespace=CONFIG["namespace"])
     pod_template = utils.get_pod_template(
-        name="nginx",
-        image=image,
-        port=port,
-        labels=labels,
+        name=CONFIG["name"],
+        image=CONFIG["image"],
+        port=CONFIG["port"],
+        labels=CONFIG["labels"],
         volume_mounts=[{"name": "nginx-conf", "mount_path": "/etc/nginx/conf.d", "configmap": "nginx-status"}])
     #create_deployment(
     #    name="nginx-deployment",
@@ -30,14 +41,15 @@ def deploy(configmap_data={}, image="nginx:latest", port=80, replicas=3, labels=
     #    labels=labels,
     #    namespace=namespace)
     utils.create_replication_controller(
-        name="nginx-replication-controller",
+        name=CONFIG["pod_name"],
         pod_template=pod_template,
-        replicas=replicas,
-        labels=labels,
-        namespace=namespace)
+        replicas=CONFIG["replicas"],
+        labels=CONFIG["labels"],
+        namespace=CONFIG["namespace"])
     utils.create_service(
         name="nginx-service",
-        port=port,
+        port=CONFIG["port"],
         service_type="NodePort",
-        labels=labels,
-        namespace=namespace)
+        labels=CONFIG["labels"],
+        namespace=CONFIG["namespace"])
+
