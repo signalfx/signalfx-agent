dea8e8ae0b98f3e8deb74af4f4f21e224ef7bbd7
Jeff Cheng <jcheng@signalfx.com>
2018-04-26 15:44:18 -0400

    Added collectd/activemq to MONITORS_TO_TEST. Added test_plaintext_passwords().

--- tests/kubernetes/kubernetes_test.py
+++ tests/kubernetes/kubernetes_test.py
@@ -8,11 +8,10 @@ import pytest
 
 pytestmark = [pytest.mark.k8s, pytest.mark.kubernetes]
 
-SERVICES_TO_DEPLOY = ["nginx"]
-
 # list of tuples for the monitors and respective metrics to test
 # the monitor should be a YAML-based dictionary which will be used for the signalfx-agent agent.yaml configuration
 MONITORS_TO_TEST = [
+    ({"type": "collectd/activemq", "discoveryRule": 'container_image =~ "activemq" && private_port == 1099', "username": "testuser", "password": "testing123"}, get_metrics_from_doc("collectd-activemq.md")),
     ({"type": "collectd/cpu"}, get_metrics_from_doc("collectd-cpu.md")),
     #({"type": "collectd/cpufreq"}, ["cpufreq.0"]),
     #({"type": "collectd/df", "hostFSPath": "/hostfs", "reportInodes": True}, get_metrics_from_doc("collectd-df.md")),
@@ -32,7 +31,7 @@ MONITORS_TO_TEST = [
 ]
 
 @pytest.mark.parametrize("monitor,expected_metrics", MONITORS_TO_TEST, ids=[m[0]["type"] for m in MONITORS_TO_TEST])
-def test_metrics(monitor, expected_metrics, minikube, k8s_test_timeout):
+def test_metrics(minikube, monitor, expected_metrics, k8s_test_timeout):
     if monitor["type"] == 'collectd/nginx' and minikube.agent.observer in ['docker', 'host']:
         pytest.skip("skipping monitor '%s' test for observer '%s'" % (monitor["type"], minikube.agent.observer))
     if len(expected_metrics) == 0:
@@ -41,10 +40,14 @@ def test_metrics(monitor, expected_metrics, minikube, k8s_test_timeout):
     metrics_not_found = check_for_metrics(minikube.agent.backend, expected_metrics, k8s_test_timeout)
     assert len(metrics_not_found) == 0, "timed out waiting for metric(s): %s\n\n%s\n\n" % (metrics_not_found, get_all_logs(minikube))
 
-def test_dims(expected_dims, minikube, k8s_test_timeout):
+def test_dims(minikube, expected_dims, k8s_test_timeout):
     if len(expected_dims) == 0:
         pytest.skip("expected dimensions is empty; skipping test")
     print("\nCollected %d dimension(s) to test." % len(expected_dims))
     dims_not_found = check_for_dims(minikube.agent.backend, expected_dims, k8s_test_timeout)
     assert len(dims_not_found) == 0, "timed out waiting for dimension(s): %s\n\n%s\n\n" % (dims_not_found, get_all_logs(minikube))
+
+def test_plaintext_passwords(minikube):
+    status = minikube.agent.get_status()
+    assert "testing123" not in status, "plaintext password(s) found in agent-status output!\n\n%s" % status
 
