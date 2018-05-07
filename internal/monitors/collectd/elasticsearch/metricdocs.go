0920b2c5ea7c5a581e7835fb5cc314f83eae1955
Jeff Cheng <jcheng@signalfx.com>
2018-05-07 13:48:30 -0400

    Added ES metrics.

---
+++ internal/monitors/collectd/elasticsearch/metricdocs.go
@@ -0,0 +1,90 @@
+package elasticsearch
+
+// COUNTER(counter.indices.get.total): The total number of get requests since node startup
+
+// COUNTER(counter.indices.indexing.index-total): The total number of index requests since node startup
+
+// COUNTER(counter.indices.merges.total): Total number of merges since node startup
+
+// COUNTER(counter.indices.search.query-time): Total time spent in search queries (milliseconds)
+
+// COUNTER(counter.indices.search.query-total): The total number of search requests since node startup
+
+// COUNTER(counter.indices.total.indexing.index-total): The total number of index requests per cluster
+
+// COUNTER(counter.indices.total.merges.total): Total number of merges per cluster
+
+// COUNTER(counter.indices.total.search.query-total): The total number of search requests per cluster
+
+// COUNTER(counter.jvm.gc.time): Total garbage collection time (milliseconds)
+
+// COUNTER(counter.thread_pool.bulk.rejected): Number of rejected bulk requests
+
+// COUNTER(counter.thread_pool.flush.rejected): Number of rejected flush requests
+
+// COUNTER(counter.thread_pool.generic.rejected): Number of rejected generic requests
+
+// COUNTER(counter.thread_pool.get.rejected): Number of rejected get requests
+
+// COUNTER(counter.thread_pool.index.rejected): Number of rejected index requests
+
+// COUNTER(counter.thread_pool.merge.rejected): Number of rejected merge requests
+
+// COUNTER(counter.thread_pool.optimize.rejected): Number of rejected optimize requests
+
+// COUNTER(counter.thread_pool.refresh.rejected): Number of rejected refresh requests
+
+// COUNTER(counter.thread_pool.rejected): Number of rejected thread pool requests
+
+// COUNTER(counter.thread_pool.search.rejected): Number of rejected search requests
+
+// COUNTER(counter.thread_pool.snapshot.rejected): Number of rejected snapshot requests
+
+// GAUGE(gauge.cluster.active-primary-shards): The number of active primary shards
+
+// GAUGE(gauge.cluster.active-shards): The number of active shards
+
+// GAUGE(gauge.cluster.initializing-shards): The number of currently initializing shards
+
+// GAUGE(gauge.cluster.number-of-data_nodes): The current number of data nodes in the cluster
+
+// GAUGE(gauge.cluster.number-of-nodes): Total number of nodes in the cluster
+
+// GAUGE(gauge.cluster.relocating-shards): The number of shards that are currently being relocated
+
+// GAUGE(gauge.cluster.status): The health status of the cluster
+
+// GAUGE(gauge.cluster.unassigned-shards): The number of shards that are currently unassigned
+
+// GAUGE(gauge.indices.cache.field.size): Field data size (bytes)
+
+// GAUGE(gauge.indices.cache.filter.size): Filter cache size (bytes)
+
+// GAUGE(gauge.indices.docs.count): Number of documents on this node
+
+// GAUGE(gauge.indices.docs.deleted): Number of deleted documents on this node
+
+// GAUGE(gauge.indices.merges.current): Number of active merges
+
+// GAUGE(gauge.indices.segments.count): Number of segments on this node
+
+// GAUGE(gauge.indices.total.docs.count): Number of documents in the cluster
+
+// GAUGE(gauge.indices.total.fielddata.memory-size): Field data size (bytes)
+
+// GAUGE(gauge.indices.total.filter-cache.memory-size): Filter cache size (bytes)
+
+// GAUGE(gauge.jvm.mem.heap-committed): Total heap committed by the process (bytes)
+
+// GAUGE(gauge.jvm.mem.heap-used): Total heap used (bytes)
+
+// GAUGE(gauge.process.open_file_descriptors): Number of currently open file descriptors
+
+// COUNTER(gauge.thread_pool.active): Number of active threads
+
+// COUNTER(gauge.thread_pool.largest): Highest active threads in thread pool
+
+// COUNTER(gauge.thread_pool.queue): Number of Tasks in thread pool
+
+// COUNTER(gauge.thread_pool.threads): Number of Threads in thread pool
+
