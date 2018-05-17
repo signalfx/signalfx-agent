1707ff6888f95c3ea4bdb97ad2ad95601c699c9b
charliesignalfx <charlie@signalfx.com>
2018-05-17 16:43:10 -0400

    clean up do_docker_build

--- scripts/windows/common.ps1
+++ scripts/windows/common.ps1
@@ -15,8 +15,6 @@ function do_docker_build([string]$image_name,
                          [string]$operating_system) {
     $agent_version = if ($agent_version -eq "") { $image_tag }
     $operating_system = if ($operating_system -eq "") { "windows" }
-    echo "$MY_SCRIPT_DIR"
-    $cache_flags = if ($PULL_CACHE -eq "yes") { "$MY_SCRIPT_DIR\..\docker-cache-from $target_stage" } else { "" } 
     docker build -t $image_name':'$image_tag `
     -f $MY_SCRIPT_DIR\..\..\Dockerfile `
     --build-arg agent_version=$agent_version `
@@ -24,5 +22,5 @@ function do_docker_build([string]$image_name,
     --target $target_stage `
     --label agent.version=$agent_version `
     $(extra_cflags_build_arg) `
-    $cache_flags $MY_SCRIPT_DIR\..\.. 
+    $MY_SCRIPT_DIR\..\.. 
 }
