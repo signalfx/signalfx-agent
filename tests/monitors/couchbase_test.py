853c7234ed58a65456c52ea7026bd3db611a0c1a
Charlie Smith <charlie@signalfx.com>
2018-05-31 13:55:20 -0400

    add couchbase test

--- tests/monitors/couchbase_test.py
+++ tests/monitors/couchbase_test.py
@@ -1,9 +1,10 @@
 from functools import partial as p
+import pytest
 import string
 
 from tests.helpers import fake_backend
 from tests.helpers.util import wait_for, run_agent, run_service, container_ip
-from tests.helpers.assertions import has_datapoint_with_dim, tcp_socket_open
+from tests.helpers.assertions import has_datapoint_with_dim, tcp_socket_open, http_status
 
 couchbase_config = string.Template("""
 monitors:
@@ -16,11 +17,16 @@ monitors:
 """)
 
 
-def test_couchbase():
-    with run_service("couchbase", hostname="node1.cluster") as couchbase_container:
+@pytest.mark.parametrize("tag", [
+    "enterprise-4.0.0",
+    "enterprise-5.1.0"
+])
+def test_couchbase(tag):
+    with run_service("couchbase", buildargs={"COUCHBASE_VERSION": tag}, hostname="node1.cluster") as couchbase_container:
         host = container_ip(couchbase_container)
         config = couchbase_config.substitute(host=host)
-        assert wait_for(p(tcp_socket_open, host, 8091), 60), "service didn't start"
+        assert wait_for(p(tcp_socket_open, host, 8091), 60), "service not listening on port"
+        assert wait_for(p(http_status,  200, "http://{0}:8091/pools".format(host)), 120), "service didn't start"
 
         with run_agent(config) as [backend, _, _]:
             assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "couchbase")), \
