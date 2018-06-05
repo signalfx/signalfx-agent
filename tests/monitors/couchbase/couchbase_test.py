f0404d7c9fdbcc766a60797d98054382171e799c
Jeff Cheng <jcheng@signalfx.com>
2018-06-05 14:10:56 -0400

    Added K8S couchbase test.

--- tests/monitors/couchbase/couchbase_test.py
+++ tests/monitors/couchbase/couchbase_test.py
@@ -1,9 +1,15 @@
 from functools import partial as p
+import os
 import pytest
 import string
 
 from tests.helpers.util import wait_for, run_agent, run_service, container_ip
 from tests.helpers.assertions import has_datapoint_with_dim, tcp_socket_open, http_status
+from tests.kubernetes.utils import (
+    run_k8s_monitors_test,
+    get_metrics_from_doc,
+    get_dims_from_doc,
+)
 
 pytestmark = [pytest.mark.collectd, pytest.mark.couchbase, pytest.mark.monitor_with_endpoints]
 
@@ -35,3 +41,27 @@ def test_couchbase(tag):
         with run_agent(config) as [backend, _, _]:
             assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "couchbase")), \
                    "Didn't get couchbase datapoints"
+
+
+@pytest.mark.k8s
+@pytest.mark.kubernetes
+def test_couchbase_in_k8s(agent_image, minikube, k8s_observer, k8s_test_timeout, k8s_namespace):
+    monitors = [
+        {"type": "collectd/couchbase",
+         "discoveryRule": 'container_image =~ "couchbase" && private_port == 8091 && kubernetes_namespace == "%s"' % k8s_namespace,
+         "collectTarget": "BUCKET",
+         "collectBucket": "travel-sample",
+         "username": "Administrator", "password": "password"}
+    ]
+    yamls = [os.path.join(os.path.dirname(os.path.realpath(__file__)), "couchbase-k8s.yaml")]
+    run_k8s_monitors_test(
+        agent_image,
+        minikube,
+        monitors,
+        namespace=k8s_namespace,
+        yamls=yamls,
+        observer=k8s_observer,
+        expected_metrics=get_metrics_from_doc("collectd-couchbase.md"),
+        expected_dims=get_dims_from_doc("collectd-couchbase.md"),
+        test_timeout=k8s_test_timeout)
+
