71edb8b16b45be99ef9d72e31fe4d25f2d7f1608
Ben Keith <bkeith@signalfx.com>
2018-06-05 12:54:24 -0400

    Make marathon integration test wait 120 seconds for startup

--- tests/monitors/marathon_test.py
+++ tests/monitors/marathon_test.py
@@ -13,20 +13,20 @@ monitors:
 """)
 
 
-@pytest.mark.parametrize("image", [
+@pytest.mark.parametrize("marathon_image", [
     "mesosphere/marathon:v1.1.1",
     "mesosphere/marathon:v1.6.352"
 ])
-def test_marathon(image):
+def test_marathon(marathon_image):
     with run_container("zookeeper:3.5") as zookeeper:
         zkhost = container_ip(zookeeper)
         assert wait_for(p(tcp_socket_open, zkhost, 2181), 60), "zookeeper didn't start"
-        with run_container(image,
+        with run_container(marathon_image,
                            command=["--master", "localhost:5050", "--zk", "zk://{0}:2181/marathon".format(zkhost)]
                            ) as service_container:
             host = container_ip(service_container)
             config = monitor_config.substitute(host=host)
-            assert wait_for(p(tcp_socket_open, host, 8080), 60), "service not listening on port"
+            assert wait_for(p(tcp_socket_open, host, 8080), 120), "marathon not listening on port"
             assert wait_for(p(http_status, url="http://{0}:8080/v2/info".format(host), status=[200]), 120), "service didn't start"
 
             with run_agent(config) as [backend, _, _]:
