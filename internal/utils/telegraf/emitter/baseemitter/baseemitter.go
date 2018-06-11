fc463e23c03931cca51317d73007835979c6c599
Charlie Smith <charlie@signalfx.com>
2018-06-11 11:46:23 -0400

    use lower case variable for pointer to structs

--- internal/utils/telegraf/emitter/baseemitter/baseemitter.go
+++ internal/utils/telegraf/emitter/baseemitter/baseemitter.go
@@ -46,126 +46,126 @@ type BaseEmitter struct {
 // AddTag adds a key/value pair to all measurement tags.  If a key conflicts
 // the key value pair in AddTag will override the original key on the
 // measurement
-func (B *BaseEmitter) AddTag(key string, val string) {
-	B.lock.Lock()
-	B.addTags[key] = val
-	B.lock.Unlock()
+func (b *BaseEmitter) AddTag(key string, val string) {
+	b.lock.Lock()
+	b.addTags[key] = val
+	b.lock.Unlock()
 }
 
 // AddTags adds a map of key value pairs to all measurement tags.  If a key
 // conflicts the key value pair in AddTags will override the original key on
 // the measurement.
-func (B *BaseEmitter) AddTags(tags map[string]string) {
+func (b *BaseEmitter) AddTags(tags map[string]string) {
 	for k, v := range tags {
-		B.AddTag(k, v)
+		b.AddTag(k, v)
 	}
 }
 
 // IncludeEvent a thread safe function for registering an event name to include
 // during emission. We disable all events by default because Telegraf has some
 // junk events.
-func (B *BaseEmitter) IncludeEvent(name string) {
-	B.lock.Lock()
-	B.included[name] = true
-	B.lock.Unlock()
+func (b *BaseEmitter) IncludeEvent(name string) {
+	b.lock.Lock()
+	b.included[name] = true
+	b.lock.Unlock()
 }
 
 // IncludeEvents a thread safe function for registering a list of event names to
 // include during emission. We disable all events by default because Telegraf
 // has some junk events.
-func (B *BaseEmitter) IncludeEvents(names []string) {
+func (b *BaseEmitter) IncludeEvents(names []string) {
 	for _, name := range names {
-		B.IncludeEvent(name)
+		b.IncludeEvent(name)
 	}
 }
 
 // Included - A thread safe function for checking if events should be included
 // during emission.  We disable all events by default because Telegraf has some
 // junk events.
-func (B *BaseEmitter) Included(name string) (included bool) {
-	B.lock.Lock()
-	included = B.included[name]
-	B.lock.Unlock()
+func (b *BaseEmitter) Included(name string) (included bool) {
+	b.lock.Lock()
+	included = b.included[name]
+	b.lock.Unlock()
 	return
 }
 
 // ExcludeDatum adds a name to the list of metrics and events to
 // exclude
-func (B *BaseEmitter) ExcludeDatum(name string) {
-	B.lock.Lock()
-	B.excluded[name] = true
-	B.lock.Unlock()
+func (b *BaseEmitter) ExcludeDatum(name string) {
+	b.lock.Lock()
+	b.excluded[name] = true
+	b.lock.Unlock()
 }
 
 // ExcludeData adds a list of names the list of metrics and events
 // to exclude
-func (B *BaseEmitter) ExcludeData(names []string) {
+func (b *BaseEmitter) ExcludeData(names []string) {
 	for _, name := range names {
-		B.ExcludeDatum(name)
+		b.ExcludeDatum(name)
 	}
 }
 
 // Excluded - A thread safe function for checking if events or metrics should be
 // excluded from emission
-func (B *BaseEmitter) Excluded(name string) (excluded bool) {
-	B.lock.Lock()
-	excluded = B.excluded[name]
-	B.lock.Unlock()
+func (b *BaseEmitter) Excluded(name string) (excluded bool) {
+	b.lock.Lock()
+	excluded = b.excluded[name]
+	b.lock.Unlock()
 	return
 }
 
 // OmitTag adds a tag to the list of tags to remove from measurements
-func (B *BaseEmitter) OmitTag(tag string) {
-	B.lock.Lock()
-	B.omittedTags[tag] = true
-	B.lock.Unlock()
+func (b *BaseEmitter) OmitTag(tag string) {
+	b.lock.Lock()
+	b.omittedTags[tag] = true
+	b.lock.Unlock()
 }
 
 // OmitTags adds a list of tags the list of tags to remove from measurements
-func (B *BaseEmitter) OmitTags(tags []string) {
+func (b *BaseEmitter) OmitTags(tags []string) {
 	for _, tag := range tags {
-		B.OmitTag(tag)
+		b.OmitTag(tag)
 	}
 }
 
 // FilterTags - filter function for util.CloneAndFilterStringMapWithFunc()
 // that will exclude any tags that
-func (B *BaseEmitter) FilterTags(key string, value string) (include bool) {
-	B.lock.Lock()
-	include = B.omittedTags[key]
-	B.lock.Unlock()
+func (b *BaseEmitter) FilterTags(key string, value string) (include bool) {
+	b.lock.Lock()
+	include = b.omittedTags[key]
+	b.lock.Unlock()
 	return
 }
 
 // Add parses measurements from telegraf and emits them through Output
-func (B *BaseEmitter) Add(measurement string, fields map[string]interface{},
+func (b *BaseEmitter) Add(measurement string, fields map[string]interface{},
 	tags map[string]string, metricType datapoint.MetricType,
 	originalMetricType string, t ...time.Time) {
 	for field, val := range fields {
 		// telegraf doc says that tags are owned by the calling plugin and they
 		// shouldn't be mutated.  So we copy the tags map
-		var metricDims = utils.CloneAndFilterStringMapWithFunc(tags, B.FilterTags)
+		var metricDims = utils.CloneAndFilterStringMapWithFunc(tags, b.FilterTags)
 
 		// add additional tags to the metricDims
-		if len(B.addTags) > 0 {
-			B.lock.Lock()
-			metricDims = utils.MergeStringMaps(metricDims, B.addTags)
-			B.lock.Unlock()
+		if len(b.addTags) > 0 {
+			b.lock.Lock()
+			metricDims = utils.MergeStringMaps(metricDims, b.addTags)
+			b.lock.Unlock()
 		}
 
 		// Generate the metric name
 		var metricName, isSFX = parse.GetMetricName(measurement, field, metricDims)
 
 		// Check if the metric is explicitly excluded
-		if B.Excluded(metricName) {
-			B.Logger.Debugf("excluding the following metric: %s", metricName)
+		if b.Excluded(metricName) {
+			b.Logger.Debugf("excluding the following metric: %s", metricName)
 			continue
 		}
 
 		// If eligible, move the dimension "property" to properties
 		metricProps, propErr := parse.ExtractProperty(metricName, metricDims)
 		if propErr != nil {
-			B.Logger.Error(propErr)
+			b.Logger.Error(propErr)
 			continue
 		}
 
@@ -186,10 +186,10 @@ func (B *BaseEmitter) Add(measurement string, fields map[string]interface{},
 				metricType,
 				GetTime(t...),
 			)
-			B.Output.SendDatapoint(dp)
+			b.Output.SendDatapoint(dp)
 		} else {
 			// Skip if it's not an sfx event and it's not included
-			if !isSFX && !B.Included(metricName) {
+			if !isSFX && !b.Included(metricName) {
 				continue
 			}
 			// We've already type checked field, so set property with value
@@ -201,14 +201,14 @@ func (B *BaseEmitter) Add(measurement string, fields map[string]interface{},
 				metricProps,
 				GetTime(t...),
 			)
-			B.Output.SendEvent(ev)
+			b.Output.SendEvent(ev)
 		}
 	}
 }
 
 // AddError handles errors reported to a telegraf accumulator
-func (B *BaseEmitter) AddError(err error) {
-	B.Logger.Error(err)
+func (b *BaseEmitter) AddError(err error) {
+	b.Logger.Error(err)
 }
 
 // NewEmitter returns a new BaseEmitter
--- internal/utils/telegraf/emitter/batchemitter/batchemitter.go
+++ internal/utils/telegraf/emitter/batchemitter/batchemitter.go
@@ -20,29 +20,29 @@ type BatchEmitter struct {
 
 // Add parses measurements from telegraf and saves them to the struct member
 // Measurements
-func (B *BatchEmitter) Add(m string, fields map[string]interface{},
+func (b *BatchEmitter) Add(m string, fields map[string]interface{},
 	tags map[string]string, metricType datapoint.MetricType,
 	originalMetricType string, t ...time.Time) {
-	B.lock.Lock()
-	B.Measurements = append(B.Measurements, &measurement.Measurement{
+	b.lock.Lock()
+	b.Measurements = append(b.Measurements, &measurement.Measurement{
 		Measurement:        m,
 		Fields:             fields,
-		Tags:               utils.CloneAndFilterStringMapWithFunc(tags, B.BaseEmitter.FilterTags),
+		Tags:               utils.CloneAndFilterStringMapWithFunc(tags, b.BaseEmitter.FilterTags),
 		MetricType:         metricType,
 		OriginalMetricType: originalMetricType,
 		T:                  t,
 	})
-	B.lock.Unlock()
+	b.lock.Unlock()
 }
 
 // Send the metrics in the batch through the agent
-func (B *BatchEmitter) Send() {
-	B.lock.Lock()
-	for _, m := range B.Measurements {
-		B.BaseEmitter.Add(m.Measurement, m.Fields, m.Tags, m.MetricType,
+func (b *BatchEmitter) Send() {
+	b.lock.Lock()
+	for _, m := range b.Measurements {
+		b.BaseEmitter.Add(m.Measurement, m.Fields, m.Tags, m.MetricType,
 			m.OriginalMetricType, m.T...)
 	}
-	B.lock.Unlock()
+	b.lock.Unlock()
 }
 
 // NewEmitter returns a new BatchEmitter that gathers a batch of telegraf
