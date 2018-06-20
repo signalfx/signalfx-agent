424dbb15270f90f1055962db9bd37ba1ed826f04
Charlie Smith <charlie@signalfx.com>
2018-06-20 13:27:42 -0400

    Win perf (#337)
    * add windows perf counter monitor

--- vendor/github.com/signalfx/telegraf/plugins/outputs/signalfx/parse/parse.go
+++ vendor/github.com/signalfx/telegraf/plugins/outputs/signalfx/parse/parse.go
@@ -8,8 +8,6 @@ import (
 func RemoveSFXDimensions(metricDims map[string]string) {
 	// remove the sf_metric dimension
 	delete(metricDims, "sf_metric")
-	// remove sf_prefix if it exists in the dimension map
-	delete(metricDims, "sf_prefix")
 }
 
 // SetPluginDimension sets the plugin dimension to the metric name if it is not already supplied
@@ -23,31 +21,32 @@ func SetPluginDimension(metricName string, metricDims map[string]string) {
 // GetMetricName combines telegraf fields and tags into a full metric name
 func GetMetricName(metric string, field string, dims map[string]string) (string, bool) {
 	// If sf_metric is provided
-	if name, isSFX := dims["sf_metric"]; isSFX {
-		return name, isSFX
+	if sfmetric, isSFX := dims["sf_metric"]; isSFX {
+		return sfmetric, isSFX
 	}
 
-	// Include field when it adds to the metric name
+	// If it isn't a sf_metric then use metric name
+	name := metric
+
+	// Include field in metric name when it adds to the metric name
 	if field != "value" {
-		return fmt.Sprintf("%s.%s", metric, field), false
+		name = fmt.Sprintf("%s.%s", name, field)
 	}
 
-	return metric, false
+	return name, false
 }
 
 // ExtractProperty of the metric according to the following rules
-func ExtractProperty(name string, dims map[string]string) (props map[string]interface{}, err error) {
-	props = make(map[string]interface{}, 1)
+func ExtractProperty(name string, dims map[string]string) (map[string]interface{}, error) {
+	props := make(map[string]interface{}, 1)
 	// if the metric is a metadata object
 	if name == "objects.host-meta-data" {
 		// If property exists remap it
-		if _, in := dims["property"]; in {
-			props["property"] = dims["property"]
-			delete(dims, "property")
-		} else {
-			// This is a malformed metadata event
-			err = fmt.Errorf("E! objects.host-metadata object doesn't have a property")
+		if _, in := dims["property"]; !in {
+			return props, fmt.Errorf("E! objects.host-metadata object doesn't have a property")
 		}
+		props["property"] = dims["property"]
+		delete(dims, "property")
 	}
-	return
+	return props, nil
 }
