b64887faf777f49099d49fd74ae0d99faf795986
Charlie Smith <charlie@signalfx.com>
2018-06-20 13:27:42 -0400

    Win perf (#337)
    * add windows perf counter monitor

---
+++ scripts/windows/make.ps1
@@ -0,0 +1,19 @@
+Set-PSDebug -Trace 1
+$env:CGO_ENABLED = 0
+
+function signalfx-agent([string]$AGENTVERSION="") {
+    $date = Get-Date -UFormat "%Y-%m-%dT%T%Z"
+    go build -ldflags "-X main.Version='$AGENTVERSION' -X main.BuiltTime='$date'" -o signalfx-agent.exe github.com/signalfx/signalfx-agent/cmd/agent    
+}
+
+function lint() {
+    golint -set_exit_status ./cmd/... ./internal/...
+}
+
+function vendor() {
+    dep ensure
+}
+
+function test() {
+    go test ./...
+}
