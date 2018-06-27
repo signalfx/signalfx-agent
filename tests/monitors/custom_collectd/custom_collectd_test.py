373c41fe62b864f8e4aeba822b7ae21a2c108af8
Jeff Cheng <jcheng@signalfx.com>
2018-06-27 13:51:12 -0400

    Removed K8S tests

--- tests/monitors/custom_collectd/custom_collectd_test.py
+++ tests/monitors/custom_collectd/custom_collectd_test.py
@@ -6,12 +6,6 @@ from tests.helpers.assertions import has_datapoint_with_dim
 import pytest
 import time
 
-from tests.kubernetes.utils import (
-    run_k8s_monitors_test,
-    get_metrics_from_doc,
-    get_dims_from_doc,
-)
-
 pytestmark = [pytest.mark.collectd, pytest.mark.custom, pytest.mark.custom_collectd, pytest.mark.monitor_without_endpoints]
 
 
@@ -88,30 +82,4 @@ def test_custom_collectd_shutdown():
         """))
 
         assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "ping")), "Didn't get ping datapoints"
-
-
-@pytest.mark.k8s
-@pytest.mark.kubernetes
-def test_custom_collectd_in_k8s(agent_image, minikube, k8s_test_timeout, k8s_namespace):
-    monitors = dedent("""
-        - type: collectd/custom
-          templates:
-            - |
-              LoadPlugin "cpu"
-            - |
-              LoadPlugin "ping"
-              <Plugin ping>
-                Host "google.com"
-              </Plugin>
-        - type: collectd/df
-        - type: collectd/signalfx-metadata
-        """)
-    run_k8s_monitors_test(
-        agent_image,
-        minikube,
-        monitors,
-        namespace=k8s_namespace,
-        expected_metrics=get_metrics_from_doc("collectd-cpu.md").union(get_metrics_from_doc("collectd-df.md")),
-        expected_dims=get_dims_from_doc("collectd-cpu.md").union(get_dims_from_doc("collectd-df.md")),
-        test_timeout=k8s_test_timeout)
 
