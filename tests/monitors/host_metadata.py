ebad6d47d124774c646ca1931d34285254b245b4
Charlie Smith <charlie@signalfx.com>
2018-07-03 11:38:04 -0400

    add tests for host metadata plugin

---
+++ tests/monitors/host_metadata.py
@@ -0,0 +1,20 @@
+from functools import partial as p
+
+from tests.helpers.util import wait_for, run_agent
+from tests.helpers.assertions import has_datapoint_with_metric_name, has_event_with_dim
+
+
+monitor_config = """
+monitors:
+  - type: host-metadata
+"""
+
+
+def test_host_metadata_monitor():
+    with run_agent(monitor_config) as [backend, _, _]:
+        assert wait_for(p(has_datapoint_with_metric_name, backend,
+                        "gauge.sf.host-plugin_uptime")), "Didn't get uptime datapoints"
+        # wait for up to 90 seconds to receive metadata properties
+        # they are guaranteed to emit in with in the first minute
+        assert wait_for(p(has_event_with_dim, backend, "plugin",
+                        "signalfx-metadata"), 90), "Didn't get metadata properties"
