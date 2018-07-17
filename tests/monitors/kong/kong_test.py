c0bbfdc7a283062d692654a26bcd8df9ffb5ec64
Jeff Cheng <36963516+jchengsfx@users.noreply.github.com>
2018-07-17 14:18:09 -0400

    Added K8S test for kong (#363)

--- tests/monitors/kong/kong_test.py
+++ tests/monitors/kong/kong_test.py
@@ -1,13 +1,21 @@
 from functools import partial as p
+from requests import get, RequestException
 from textwrap import dedent
 from io import BytesIO
-import string
-
-from requests import get, RequestException
+import os
 import pytest
+import string
 
 from tests.helpers.util import wait_for, run_agent, run_container, container_ip, get_docker_client
 from tests.helpers.assertions import has_datapoint_with_dim
+from tests.helpers.util import (
+    get_monitor_metrics_from_selfdescribe,
+    get_monitor_dims_from_selfdescribe
+)
+from tests.kubernetes.utils import (
+    run_k8s_monitors_test,
+    get_discovery_rule,
+)
 
 pytestmark = [pytest.mark.collectd, pytest.mark.kong, pytest.mark.monitor_with_endpoints]
 
@@ -81,3 +89,26 @@ def test_kong(kong_image):
 
             with run_agent(config) as [backend, _, _]:
                 assert wait_for(p(has_datapoint_with_dim, backend, 'plugin', 'kong')), "Didn't get Kong data point"
+
+
+@pytest.mark.k8s
+@pytest.mark.kubernetes
+def test_kong_in_k8s(agent_image, minikube, k8s_observer, k8s_test_timeout, k8s_namespace):
+    yaml = os.path.join(os.path.dirname(os.path.realpath(__file__)), "kong-k8s.yaml")
+    dockerfile_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../test-services/kong")
+    build_opts = {"tag": "kong:k8s-test"}
+    minikube.build_image(dockerfile_dir, build_opts)
+    monitors = [
+        {"type": "collectd/kong",
+         "discoveryRule": get_discovery_rule(yaml, k8s_observer, namespace=k8s_namespace)}
+    ]
+    run_k8s_monitors_test(
+        agent_image,
+        minikube,
+        monitors,
+        namespace=k8s_namespace,
+        yamls=[yaml],
+        observer=k8s_observer,
+        expected_metrics=get_monitor_metrics_from_selfdescribe(monitors[0]["type"]),
+        expected_dims=get_monitor_dims_from_selfdescribe(monitors[0]["type"]),
+        test_timeout=k8s_test_timeout)
