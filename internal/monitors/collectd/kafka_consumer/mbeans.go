ef086b8298bc8f78c999ec5a2bf27ad564b2b523
Akash Suresh <akash@signalfx.com>
2018-07-23 11:23:34 -0400

    Add few metrics for kafka_consumer monitor

--- internal/monitors/collectd/kafka_consumer/mbeans.go
+++ internal/monitors/collectd/kafka_consumer/mbeans.go
@@ -1,3 +1,47 @@
 package kafka_consumer
 
-var defaultMBeanYAML = ``
+var defaultMBeanYAML = `
+records-lag-max:
+  objectName: "kafka.consumer:client-id=*,type=consumer-fetch-manager-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.consumer.records-lag-max"
+    type: "gauge"
+    table: false
+    attribute: "records-lag-max"
+
+bytes-consumed-rate:
+  objectName: "kafka.consumer:client-id=*,type=consumer-fetch-manager-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.consumer.bytes-consumed-rate"
+    type: "gauge"
+    table: false
+    attribute: "bytes-consumed-rate"
+
+records-consumed-rate:
+  objectName: "kafka.consumer:client-id=*,type=consumer-fetch-manager-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.consumer.records-consumed-rate"
+    type: "gauge"
+    table: false
+    attribute: "records-consumed-rate"
+
+fetch-rate:
+  objectName: "kafka.consumer:client-id=*,type=consumer-fetch-manager-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.consumer.fetch-rate"
+    type: "gauge"
+    table: false
+    attribute: "fetch-rate"
+`
