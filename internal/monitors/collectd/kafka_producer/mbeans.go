74cccefd3d1c7506e99cc78aeec8995d538c2433
Akash Suresh <akash@signalfx.com>
2018-07-23 11:46:44 -0400

    Add few metrics to kafka_producer monitor

--- internal/monitors/collectd/kafka_producer/mbeans.go
+++ internal/monitors/collectd/kafka_producer/mbeans.go
@@ -1,3 +1,69 @@
 package kafka_producer
 
-var defaultMBeanYAML = ``
+var defaultMBeanYAML = `
+response-rate:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.response-rate"
+    type: "gauge"
+    table: false
+    attribute: "response-rate"
+
+request-rate:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.request-rate"
+    type: "gauge"
+    table: false
+    attribute: "request-rate"
+
+request-latency-avg:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.request-latency-avg"
+    type: "gauge"
+    table: false
+    attribute: "request-latency-avg"
+
+outgoing-byte-rate:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.outgoing-byte-rate"
+    type: "gauge"
+    table: false
+    attribute: "outgoing-byte-rate"
+
+io-time-ns-avg:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.io-time-ns-avg"
+    type: "gauge"
+    table: false
+    attribute: "io-time-ns-avg"
+
+io-wait-time-ns-avg:
+  objectName: "kafka.producer:client-id=*,type=producer-metrics"
+  instancePrefix: "all"
+  dimensions:
+  - client-id
+  values:
+  - instancePrefix: "kafka.producer.io-wait-time-ns-avg"
+    type: "gauge"
+    table: false
+    attribute: "io-wait-time-ns-avg"
+`
