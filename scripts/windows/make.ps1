6b6b799c32730b41bb74408b416675c0592cbcfb
Jeff Cheng <jcheng@signalfx.com>
2018-08-15 13:12:44 -0400

    Update make.ps1

--- scripts/windows/make.ps1
+++ scripts/windows/make.ps1
@@ -4,20 +4,25 @@ $env:CGO_ENABLED = 0
 function signalfx-agent([string]$AGENTVERSION="") {
     $date = Get-Date -UFormat "%Y-%m-%dT%T%Z"
     go build -ldflags "-X main.Version='$AGENTVERSION' -X main.BuiltTime='$date'" -o signalfx-agent.exe github.com/signalfx/signalfx-agent/cmd/agent    
+    exit $LASTEXITCODE
 }
 
 function lint() {
     golint -set_exit_status ./cmd/... ./internal/...
+    exit $LASTEXITCODE
 }
 
 function vendor() {
     dep ensure
+    exit $LASTEXITCODE
 }
 
 function test() {
     go test ./...
+    exit $LASTEXITCODE
 }
 
 function vet() {
     go vet ./...
+    exit $LASTEXITCODE
 }
