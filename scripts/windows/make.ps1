ae318c5556bdd53ff03fe277743e962c54ac456a
Jeff Cheng <jcheng@signalfx.com>
2018-08-16 10:46:22 -0400

    Try returning $? instead of exiting

--- scripts/windows/make.ps1
+++ scripts/windows/make.ps1
@@ -4,27 +4,26 @@ $env:CGO_ENABLED = 0
 function signalfx-agent([string]$AGENTVERSION="") {
     $date = Get-Date -UFormat "%Y-%m-%dT%T%Z"
     go build -ldflags "-X main.Version='$AGENTVERSION' -X main.BuiltTime='$date'" -o signalfx-agent.exe github.com/signalfx/signalfx-agent/cmd/agent    
-    if ($lastexitcode -ne 0){ exit $lastexitcode }
+    return $?
 }
 
 function lint() {
     golint -set_exit_status ./cmd/... ./internal/...
-    if ($lastexitcode -ne 0){ exit $lastexitcode }
+    return $?
 }
 
 function vendor() {
     dep ensure
-    if ($lastexitcode -ne 0){ exit $lastexitcode }
+    return $?
 }
 
 function test() {
     go generate ./internal/monitors/...
-    if ($lastexitcode -ne 0){ exit $lastexitcode }
-    $(& go test -v ./... 2>&1; $ec=$lastexitcode) | go2xunit > xunit.xml
-    if ($ec -ne 0 -or $lastexitcode -ne 0){ exit 1 }
+    if ($? -ne 0){ return $? }
+    go test -v ./... 2>&1 | go2xunit > xunit.xml
 }
 
 function vet() {
     go vet ./... 2>&1 | Select-String -Pattern "\.go" | Select-String -NotMatch -Pattern "_test\.go" -outvariable gofiles
-    if ($gofiles){ echo $gofiles; exit 1 }
+    if ($gofiles){ echo $gofiles; return 1 }
 }
