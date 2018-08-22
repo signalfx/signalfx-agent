76875d19f705eeac262d0f6a1a503a606444e318
Jeff Cheng <jcheng@signalfx.com>
2018-08-22 16:59:54 -0400

    Parametrize win-perf-counters tests

--- tests/monitors/telegraf/win_perf_counters/win_perf_counters_test.py
+++ tests/monitors/telegraf/win_perf_counters/win_perf_counters_test.py
@@ -1,6 +1,6 @@
 from functools import partial as p
-from textwrap import dedent
 import pytest
+import string
 import sys
 
 from tests.helpers.util import wait_for, run_agent
@@ -13,146 +13,74 @@ pytestmark = [
     pytest.mark.win_perf_counters
 ]
 
+config = string.Template("""
+monitors:
+ - type: telegraf/win_perf_counters
+   printValid: true
+   useWildCardExpansion: true
+   objects:
+    - objectName: "$object_name"
+      instances:
+       - "$instance"
+      counters:
+       - "*"
+      includeTotal: true
+      measurement: "$measurement"
+""")
 
-def test_win_cpu():
-    config = dedent("""
-        monitors:
-         - type: telegraf/win_perf_counters
-           printValid: true
-           useWildCardExpansion: true
-           objects:
-            - objectName: "Processor"
-              instances:
-               - "*"
-              counters:
-               - "*"
-              includeTotal: true
-              measurement: "win_cpu"
-        """)
-    metrics = [
+metrics = {
+    "win_cpu": [
         "win_cpu.Percent_Idle_Time",
         "win_cpu.Percent_Interrupt_Time",
         "win_cpu.Percent_Privileged_Time",
         "win_cpu.Percent_Processor_Time",
-        "win_cpu.Percent_User_Time",
-    ]
-    with run_agent(config) as [backend, get_output, _]:
-        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_cpu")), "Didn't get win_cpu datapoints"
-        assert wait_for(p(has_datapoint_with_dim, backend, "instance", "_Total")), "Didn't get _Total datapoints"
-        for metric in metrics:
-            assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
-        assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
-
-
-def test_win_disk():
-    config = dedent("""
-        monitors:
-         - type: telegraf/win_perf_counters
-           printValid: true
-           useWildCardExpansion: true
-           objects:
-            - objectName: "LogicalDisk"
-              instances:
-               - "*"
-              counters:
-               - "*"
-              includeTotal: true
-              measurement: "win_disk"
-        """)
-    metrics = [
+        "win_cpu.Percent_User_Time"
+    ],
+    "win_disk": [
         "win_disk.Percent_Idle_Time",
         "win_disk.Percent_Disk_Time",
         "win_disk.Percent_Disk_Read_Time",
         "win_disk.Percent_Disk_Write_Time",
         "win_disk.Current_Disk_Queue_Length",
-    ]
-    with run_agent(config) as [backend, get_output, _]:
-        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_disk")), "Didn't get win_disk datapoints"
-        assert wait_for(p(has_datapoint_with_dim, backend, "instance", "_Total")), "Didn't get _Total datapoints"
-        for metric in metrics:
-            assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
-        assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
-
-
-def test_win_system():
-    config = dedent("""
-        monitors:
-         - type: telegraf/win_perf_counters
-           printValid: true
-           useWildCardExpansion: true
-           objects:
-            - objectName: "System"
-              instances:
-               - "------"
-              counters:
-               - "*"
-              measurement: "win_system"
-        """)
-    metrics = [
+    ],
+    "win_system": [
         "win_system.Context_Switches_persec",
         "win_system.Processes",
         "win_system.System_Calls_persec",
         "win_system.System_Up_Time",
         "win_system.Threads",
-    ]
-    with run_agent(config) as [backend, get_output, _]:
-        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_system")), "Didn't get win_system datapoints"
-        for metric in metrics:
-            assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
-        assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
-
-
-def test_win_mem():
-    config = dedent("""
-        monitors:
-         - type: telegraf/win_perf_counters
-           printValid: true
-           useWildCardExpansion: true
-           objects:
-            - objectName: "Memory"
-              instances:
-               - "------"
-              counters:
-               - "*"
-              measurement: "win_mem"
-        """)
-    metrics = [
+    ],
+    "win_mem": [
         "win_mem.Available_Bytes",
         "win_mem.Cache_Bytes",
         "win_mem.Committed_Bytes",
         "win_mem.Pages_persec",
         "win_mem.Write_Copies_persec",
-    ]
-    with run_agent(config) as [backend, get_output, _]:
-        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_mem")), "Didn't get win_mem datapoints"
-        for metric in metrics:
-            assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
-        assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
-
-
-def test_win_net():
-    config = dedent("""
-        monitors:
-         - type: telegraf/win_perf_counters
-           printValid: true
-           useWildCardExpansion: true
-           objects:
-            - objectName: "Network Interface"
-              instances:
-               - "*"
-              counters:
-               - "*"
-              measurement: "win_net"
-        """)
-    metrics = [
+    ],
+    "win_net": [
         "win_net.Bytes_Received_persec",
         "win_net.Bytes_Sent_persec",
         "win_net.Bytes_Total_persec",
         "win_net.Current_Bandwidth",
         "win_net.Packets_persec",
-    ]
-    with run_agent(config) as [backend, get_output, _]:
-        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_net")), "Didn't get win_net datapoints"
-        for metric in metrics:
+    ],
+}
+
+
+@pytest.mark.parametrize(
+    "object_name, instance, measurement",
+    [("Processor", "*", "win_cpu"),
+     ("LogicalDisk", "*", "win_disk"),
+     ("System", "------", "win_system"),
+     ("Memory", "------", "win_mem"),
+     ("Network Interface", "*", "win_net")]
+)
+def test_perf_counter(object_name, instance, measurement):
+    monitor_config = config.substitute(object_name=object_name, instance=instance, measurement=measurement)
+    with run_agent(monitor_config) as [backend, get_output, _]:
+        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", measurement)), "Didn't get %s datapoints" % measurement
+        if instance == "*":
+            assert wait_for(p(has_datapoint_with_dim, backend, "instance", "_Total")), "Didn't get _Total datapoints"
+        for metric in metrics[measurement]:
             assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
         assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
