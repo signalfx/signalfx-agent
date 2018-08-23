82bf3d9a6c08a6751acb074415795275589295b1
Jeff Cheng <jcheng@signalfx.com>
2018-08-23 11:28:56 -0400

    Parametrize win-perf-counters tests

--- tests/monitors/telegraf/win_perf_counters_test.py
+++ tests/monitors/telegraf/win_perf_counters_test.py
@@ -28,65 +28,66 @@ monitors:
       measurement: "$measurement"
 """)
 
-metrics = {
-    "win_cpu": [
-        "win_cpu.Percent_Idle_Time",
-        "win_cpu.Percent_Interrupt_Time",
-        "win_cpu.Percent_Privileged_Time",
-        "win_cpu.Percent_Processor_Time",
-        "win_cpu.Percent_User_Time"
-    ],
-    "win_disk": [
-        "win_disk.Percent_Idle_Time",
-        "win_disk.Percent_Disk_Time",
-        "win_disk.Percent_Disk_Read_Time",
-        "win_disk.Percent_Disk_Write_Time",
-        "win_disk.Current_Disk_Queue_Length",
-    ],
-    "win_system": [
-        "win_system.Context_Switches_persec",
-        "win_system.Processes",
-        "win_system.System_Calls_persec",
-        "win_system.System_Up_Time",
-        "win_system.Threads",
-    ],
-    "win_mem": [
-        "win_mem.Available_Bytes",
-        "win_mem.Cache_Bytes",
-        "win_mem.Committed_Bytes",
-        "win_mem.Pages_persec",
-        "win_mem.Write_Copies_persec",
-    ],
-    "win_net": [
-        "win_net.Bytes_Received_persec",
-        "win_net.Bytes_Sent_persec",
-        "win_net.Bytes_Total_persec",
-        "win_net.Current_Bandwidth",
-        "win_net.Packets_persec",
-    ],
+params = {
+    "win_cpu": {
+        "object_name": "Processor",
+        "instance": "*",
+        "include_total": "true",
+        "metrics": ["win_cpu.Percent_Idle_Time",
+                    "win_cpu.Percent_Interrupt_Time",
+                    "win_cpu.Percent_Privileged_Time",
+                    "win_cpu.Percent_Processor_Time",
+                    "win_cpu.Percent_User_Time"]},
+    "win_disk": {
+        "object_name": "LogicalDisk",
+        "instance": "*",
+        "include_total": "true",
+        "metrics": ["win_disk.Percent_Idle_Time",
+                    "win_disk.Percent_Disk_Time",
+                    "win_disk.Percent_Disk_Read_Time",
+                    "win_disk.Percent_Disk_Write_Time",
+                    "win_disk.Current_Disk_Queue_Length"]},
+    "win_system": {
+        "object_name": "System",
+        "instance": "------",
+        "include_total": "false",
+        "metrics": ["win_system.Context_Switches_persec",
+                    "win_system.Processes",
+                    "win_system.System_Calls_persec",
+                    "win_system.System_Up_Time",
+                    "win_system.Threads"]},
+    "win_mem": {
+        "object_name": "Memory",
+        "instance": "------",
+        "include_total": "false",
+        "metrics": ["win_mem.Available_Bytes",
+                    "win_mem.Cache_Bytes",
+                    "win_mem.Committed_Bytes",
+                    "win_mem.Pages_persec",
+                    "win_mem.Write_Copies_persec"]},
+    "win_net": {
+        "object_name": "Network Interface",
+        "instance": "*",
+        "include_total": "false",
+        "metrics": ["win_net.Bytes_Received_persec",
+                    "win_net.Bytes_Sent_persec",
+                    "win_net.Bytes_Total_persec",
+                    "win_net.Current_Bandwidth",
+                    "win_net.Packets_persec"]},
 }
 
-params = [
-    ("win_cpu", "Processor", "*", "true"),
-    ("win_disk", "LogicalDisk", "*", "true"),
-    ("win_system", "System", "------", "false"),
-    ("win_mem", "Memory", "------", "false"),
-    ("win_net", "Network Interface", "*", "false"),
-]
 
-
-@pytest.mark.parametrize("measurement, object_name, instance, include_total", params, ids=[p[0] for p in params])
-def test_win_perf_counters(measurement, object_name, instance, include_total):
+@pytest.mark.parametrize("measurement", params.keys())
+def test_win_perf_counters(measurement):
     config = config_template.substitute(
         measurement=measurement,
-        object_name=object_name,
-        instance=instance,
-        include_total=include_total)
+        object_name=params[measurement]["object_name"],
+        instance=params[measurement]["instance"],
+        include_total=params[measurement]["include_total"])
     with run_agent(config) as [backend, get_output, _]:
         assert wait_for(p(has_datapoint_with_dim, backend, "plugin", measurement)), "Didn't get %s datapoints" % measurement
-        if include_total == "true":
+        if params[measurement]["include_total"] == "true":
             assert wait_for(p(has_datapoint_with_dim, backend, "instance", "_Total")), "Didn't get _Total datapoints"
-        for metric in metrics[measurement]:
+        for metric in params[measurement]["metrics"]:
             assert wait_for(p(has_datapoint_with_metric_name, backend, metric)), "Didn't get metric %s" % metric
-        print("output = " + get_output())
         assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
