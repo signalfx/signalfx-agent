251a3ecc01017b200426e7f7ac81f39fe656f69b
Charlie Smith <charlie@signalfx.com>
2018-09-13 16:12:29 -0400

    make elasticsearch monitor run on python runner

--- internal/monitors/collectd/elasticsearch/elasticsearch.go
+++ internal/monitors/collectd/elasticsearch/elasticsearch.go
@@ -2,12 +2,13 @@
 
 package elasticsearch
 
-//go:generate collectd-template-to-go elasticsearch.tmpl
-
 import (
 	"github.com/signalfx/signalfx-agent/internal/core/config"
-	"github.com/signalfx/signalfx-agent/internal/monitors"
 	"github.com/signalfx/signalfx-agent/internal/monitors/collectd"
+
+	"github.com/signalfx/signalfx-agent/internal/monitors"
+	"github.com/signalfx/signalfx-agent/internal/monitors/collectd/python"
+	"github.com/signalfx/signalfx-agent/internal/monitors/pyrunner"
 )
 
 const monitorType = "collectd/elasticsearch"
@@ -20,7 +21,9 @@ const monitorType = "collectd/elasticsearch"
 func init() {
 	monitors.Register(monitorType, func() interface{} {
 		return &Monitor{
-			*collectd.NewMonitorCore(CollectdTemplate),
+			python.PyMonitor{
+				MonitorCore: pyrunner.New("sfxcollectd"),
+			},
 		}
 	}, &Config{})
 }
@@ -28,25 +31,25 @@ func init() {
 // Config is the monitor-specific config with the generic config embedded
 type Config struct {
 	config.MonitorConfig `yaml:",inline" acceptsEndpoints:"true"`
-
-	Host string `yaml:"host" validate:"required"`
-	Port uint16 `yaml:"port" validate:"required"`
+	pyConf               *python.Config
+	Host                 string `yaml:"host" validate:"required"`
+	Port                 uint16 `yaml:"port" validate:"required"`
 	// AdditionalMetrics to report on
 	AdditionalMetrics []string `yaml:"additionalMetrics"`
 	// DetailedMetrics turns on additional metric time series
-	DetailedMetrics bool `yaml:"detailedMetrics" default:"true"`
+	DetailedMetrics *bool `yaml:"detailedMetrics" default:"true"`
 	// EnableClusterHealth enables reporting on the cluster health
-	EnableClusterHealth bool `yaml:"enableClusterHealth" default:"true"`
+	EnableClusterHealth *bool `yaml:"enableClusterHealth" default:"true"`
 	// EnableIndexStats reports metrics about indexes
-	EnableIndexStats bool `yaml:"enableIndexStats" default:"true"`
+	EnableIndexStats *bool `yaml:"enableIndexStats" default:"true"`
 	// Indexes to report on
 	Indexes []string `yaml:"indexes" default:"[\"_all\"]"`
 	// IndexInterval is an interval in seconds at which the plugin will report index stats.
 	// It must be greater than or equal, and divisible by the Interval configuration
-	IndexInterval uint `yaml:"indexInterval" default:"300"`
+	IndexInterval *uint `yaml:"indexInterval" default:"300"`
 	// IndexStatsMasterOnly sends index stats from the master only
-	IndexStatsMasterOnly bool `yaml:"indexStatsMasterOnly" default:"false"`
-	IndexSummaryOnly     bool `yaml:"indexSummaryOnly" default:"false"`
+	IndexStatsMasterOnly *bool `yaml:"indexStatsMasterOnly" default:"false"`
+	IndexSummaryOnly     *bool `yaml:"indexSummaryOnly" default:"false"`
 	// Password used to access elasticsearch stats api
 	Password string `yaml:"password" neverLog:"true"`
 	// Protocol used to connect: http or https
@@ -58,12 +61,54 @@ type Config struct {
 	Version  string `yaml:"version"`
 }
 
+// PythonConfig returns the embedded python.Config struct from the interface
+func (c *Config) PythonConfig() *python.Config {
+	return c.pyConf
+}
+
 // Monitor is the main type that represents the monitor
 type Monitor struct {
-	collectd.MonitorCore
+	python.PyMonitor
 }
 
 // Configure configures and runs the plugin in collectd
 func (m *Monitor) Configure(conf *Config) error {
-	return m.SetConfigurationAndRun(conf)
+	conf.pyConf = &python.Config{
+		MonitorConfig: conf.MonitorConfig,
+		Host:          conf.Host,
+		Port:          conf.Port,
+		ModuleName:    "elasticsearch_collectd",
+		ModulePaths:   []string{collectd.MakePath("elasticsearch")},
+		TypesDBPaths:  []string{collectd.MakePath("types.db")},
+		PluginConfig: map[string]interface{}{
+			"Host":                 conf.Host,
+			"Port":                 conf.Port,
+			"DetailedMetrics":      conf.DetailedMetrics,
+			"EnableClusterHealth":  conf.EnableClusterHealth,
+			"EnableIndexStats":     conf.EnableIndexStats,
+			"IndexInterval":        conf.IndexInterval,
+			"IndexStatsMasterOnly": conf.IndexStatsMasterOnly,
+			"IndexSummaryOnly":     conf.IndexSummaryOnly,
+			"Interval":             conf.IntervalSeconds,
+			"Verbose":              false,
+			"AdditionalMetrics": map[string]interface{}{
+				"#flatten": true,
+				"values":   conf.AdditionalMetrics,
+			},
+			"Indexes": map[string]interface{}{
+				"#flatten": true,
+				"values":   conf.Indexes,
+			},
+			"Password": conf.Password,
+			"Protocol": conf.Protocol,
+			"Username": conf.Username,
+			"ThreadPools": map[string]interface{}{
+				"#flatten": true,
+				"values":   conf.ThreadPools,
+			},
+			"Version": conf.Version,
+		},
+	}
+
+	return m.PyMonitor.Configure(conf)
 }
