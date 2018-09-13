12e50e21d819c76c55668ddaf781994a0ef1b116
Charlie Smith <charlie@signalfx.com>
2018-09-13 16:18:28 -0400

    make zookeepr monitor run on python runner

--- internal/monitors/collectd/zookeeper/zookeeper.go
+++ internal/monitors/collectd/zookeeper/zookeeper.go
@@ -2,12 +2,14 @@
 
 package zookeeper
 
-//go:generate collectd-template-to-go zookeeper.tmpl
-
 import (
-	"github.com/signalfx/signalfx-agent/internal/core/config"
-	"github.com/signalfx/signalfx-agent/internal/monitors"
 	"github.com/signalfx/signalfx-agent/internal/monitors/collectd"
+
+	"github.com/signalfx/signalfx-agent/internal/core/config"
+
+	"github.com/signalfx/signalfx-agent/internal/monitors"
+	"github.com/signalfx/signalfx-agent/internal/monitors/collectd/python"
+	"github.com/signalfx/signalfx-agent/internal/monitors/pyrunner"
 )
 
 const monitorType = "collectd/zookeeper"
@@ -23,7 +25,9 @@ const monitorType = "collectd/zookeeper"
 func init() {
 	monitors.Register(monitorType, func() interface{} {
 		return &Monitor{
-			*collectd.NewMonitorCore(CollectdTemplate),
+			python.PyMonitor{
+				MonitorCore: pyrunner.New("sfxcollectd"),
+			},
 		}
 	}, &Config{})
 }
@@ -31,18 +35,37 @@ func init() {
 // Config is the monitor-specific config with the generic config embedded
 type Config struct {
 	config.MonitorConfig `yaml:",inline" acceptsEndpoints:"true"`
+	pyConf               *python.Config
+	Host                 string `yaml:"host" validate:"required"`
+	Port                 uint16 `yaml:"port" validate:"required"`
+	Name                 string `yaml:"name"`
+}
 
-	Host string `yaml:"host" validate:"required"`
-	Port uint16 `yaml:"port" validate:"required"`
-	Name string `yaml:"name"`
+// PythonConfig returns the embedded python.Config struct from the interface
+func (c *Config) PythonConfig() *python.Config {
+	return c.pyConf
 }
 
 // Monitor is the main type that represents the monitor
 type Monitor struct {
-	collectd.MonitorCore
+	python.PyMonitor
 }
 
-// Configure configures and runs the plugin in collectd
-func (am *Monitor) Configure(conf *Config) error {
-	return am.SetConfigurationAndRun(conf)
+// Configure configures and runs the plugin in python
+func (rm *Monitor) Configure(conf *Config) error {
+	conf.pyConf = &python.Config{
+		MonitorConfig: conf.MonitorConfig,
+		ModuleName:    "zk-collectd",
+		ModulePaths:   []string{collectd.MakePath("zookeeper")},
+		TypesDBPaths:  []string{collectd.MakePath("types.db")},
+		Host:          conf.Host,
+		Port:          conf.Port,
+		PluginConfig: map[string]interface{}{
+			"Hosts":    conf.Host,
+			"Port":     conf.Port,
+			"Instance": conf.Name,
+		},
+	}
+
+	return rm.PyMonitor.Configure(conf)
 }
