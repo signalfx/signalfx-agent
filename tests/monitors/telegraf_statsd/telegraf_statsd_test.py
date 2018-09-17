1650234edef4abce265faf73e692ca904093d22f
Charlie Smith <charlie@signalfx.com>
2018-09-17 17:42:04 -0400

    add integration tests

---
+++ tests/monitors/telegraf_statsd/telegraf_statsd_test.py
@@ -0,0 +1,48 @@
+from functools import partial as p
+import pytest
+import time
+
+from tests.helpers.util import *
+from tests.helpers.assertions import *
+
+
+pytestmark = [pytest.mark.windows,
+              pytest.mark.telegraf_statsd,
+              pytest.mark.telegraf]
+
+# regex used to scrape the address and port taht dogstatsd is listening on
+statsdRE = re.compile(r'(?<=listener listening on:(?=(\s+)(?=(\d+.\d+.\d+.\d+)\:(\d+))))')
+
+monitor_config = """
+monitors:
+  - type: telegraf/statsd
+    protocol: udp
+    serviceAddress: "127.0.0.1:0"
+    parseDataDogTags: true
+    metricSeparator: '.'
+"""
+
+
+def test_telegraf_statsd():
+    with run_agent(monitor_config) as [backend, get_output, _]:
+        # wait until the dogstatsd plugin logs the address and port it is listening on
+        # wait until the dogstatsd plugin logs the address and port it is listening on
+        assert wait_for(p(regex_search_matches_output, get_output, statsdRE.search))
+
+        # scrape the host and port that the statsd plugin is listening on
+        regex_results = statsdRE.search(get_output())
+
+        host = regex_results.groups()[1]
+        port = int(regex_results.groups()[2])
+
+        # wait for dogstatsd port to open
+        assert wait_for(p(udp_port_open_locally, port))
+
+        # send datapoints to the dogstatsd listener
+        for i in range(0,10):
+            send_udp_message(host, port, "dogstatsd.test.metric:55555|g|#dimension1:value1,dimension2:value2")
+            time.sleep(1)
+
+        # wait for fake ingest to receive the dogstatsd metrics
+        assert wait_for(p(has_datapoint_with_metric_name, backend, "dogstatsd.test.metric"))
+        # assert wait_for(p(has_datapoint_with_dim, backend, "customtag1", "foo")), "didn't get datapoint written before startup"
