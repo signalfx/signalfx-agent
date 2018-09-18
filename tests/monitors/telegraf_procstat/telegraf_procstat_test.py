df024b5124aa9788746e53c49a2f7df4ffc2a1e1
Charlie Smith <charlie@signalfx.com>
2018-09-18 12:36:34 -0400

    add integration test for procstat monitor

---
+++ tests/monitors/telegraf_procstat/telegraf_procstat_test.py
@@ -0,0 +1,23 @@
+from functools import partial as p
+import pytest
+
+from tests.helpers.util import *
+from tests.helpers.assertions import *
+
+
+pytestmark = [pytest.mark.windows,
+              pytest.mark.telegraf_procstat,
+              pytest.mark.telegraf]
+
+monitor_config = """
+monitors:
+  - type: telegraf/procstat
+    pattern: signalfx-agent*
+"""
+
+
+def test_telegraf_procstat():
+    with run_agent(monitor_config) as [backend, _, _]:
+        # wait for fake ingest to receive the procstat metrics
+        assert wait_for(p(has_datapoint_with_metric_name, backend, "procstat.cpu_usage")), "no cpu usage datapoint found for process"
+        assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "procstat")), "plugin dimension not set"
