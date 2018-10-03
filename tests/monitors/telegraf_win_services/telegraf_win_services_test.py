517cdd6cbceb72dd4cf24c42a2e8aad8c2455e57
Charlie Smith <charlie@signalfx.com>
2018-10-03 09:15:19 -0400

    fix win services test

--- tests/monitors/telegraf_win_services/telegraf_win_services_test.py
+++ tests/monitors/telegraf_win_services/telegraf_win_services_test.py
@@ -17,6 +17,6 @@ monitors:
 
 
 def test_win_services():
-        with run_agent(config) as [backend, _, _]:
+        with run_agent(monitor_config) as [backend, _, _]:
             assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "win_services")), "didn't get datapoint with expected plugin dimension"
 
