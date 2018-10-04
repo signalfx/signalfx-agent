237416f022683307ebdff910d59ba5674c61d5eb
Charlie Smith <charlie@signalfx.com>
2018-10-04 14:56:23 -0400

    Fix python integration tests to conform to new linter and formatter (#449)
    * set python version to 3.6 in appveyor
    
    * fix imports in windows integration tests
    
    * Removed select blocking for windows compatibility (#448)
    
    * lint and format integration tests

--- tests/monitors/tail/tail_test.py
+++ tests/monitors/tail/tail_test.py
@@ -3,15 +3,14 @@ import pytest
 import string
 import tempfile
 
-from tests.helpers.util import wait_for, run_agent
-from tests.helpers.assertions import has_datapoint_with_dim
+from helpers.util import wait_for, run_agent
+from helpers.assertions import has_datapoint_with_dim
 
 
-pytestmark = [pytest.mark.windows,
-              pytest.mark.tail,
-              pytest.mark.telegraf]
+pytestmark = [pytest.mark.windows, pytest.mark.tail, pytest.mark.telegraf]
 
-monitor_config = string.Template("""
+monitor_config = string.Template(
+    """
 monitors:
   - type: telegraf/tail
     files:
@@ -20,18 +19,24 @@ monitors:
     fromBeginning: true     # specify to read from the beginning
     telegrafParser:         # configure the telegrafParser
       dataFormat: influx  # set the parseer format to "influx"
-""")
+"""
+)
 
 
 def test_tail():
-    with tempfile.NamedTemporaryFile('w+b') as f:
+    with tempfile.NamedTemporaryFile("w+b") as f:
         config = monitor_config.substitute(file=f.name)
-        f.write(b'disk,customtag1=foo bytes=1024\n')
+        f.write(b"disk,customtag1=foo bytes=1024\n")
         f.flush()
         with run_agent(config) as [backend, _, _]:
-            assert wait_for(p(has_datapoint_with_dim, backend, "customtag1", "foo")), "didn't get datapoint written before startup"
-            f.write(b'mem,customtag2=foo2 bytes=1024\n')
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "customtag1", "foo")
+            ), "didn't get datapoint written before startup"
+            f.write(b"mem,customtag2=foo2 bytes=1024\n")
             f.flush()
-            assert wait_for(p(has_datapoint_with_dim, backend, "customtag2", "foo2")), "didn't get datapoint written after startup"
-            assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "telegraf-tail")), "didn't get datapoint with expected plugin dimension"
-
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "customtag2", "foo2")
+            ), "didn't get datapoint written after startup"
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "telegraf-tail")
+            ), "didn't get datapoint with expected plugin dimension"
