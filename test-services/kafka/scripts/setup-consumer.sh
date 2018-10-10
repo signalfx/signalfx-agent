388ef9b244752ca2362c2e4378e9f2571d9a693f
Jeff Cheng <jcheng@signalfx.com>
2018-10-10 11:11:58 -0400

    Added integration test support for kafka 2.0.0

--- test-services/kafka/scripts/setup-consumer.sh
+++ test-services/kafka/scripts/setup-consumer.sh
@@ -5,6 +5,10 @@ fi
 sleep 30
 while true;
  do
-   ./opt/kafka_2.11-"$KAFKA_VERSION"/bin/kafka-console-consumer.sh --bootstrap-server "$KAFKA_BROKER" --new-consumer --topic sfx-employee --max-messages $((10 +RANDOM % 100))
+   if [ "$KAFKA_VERSION" = "2.0.0" ]; then
+     ./opt/kafka_2.11-"$KAFKA_VERSION"/bin/kafka-console-consumer.sh --bootstrap-server "$KAFKA_BROKER" --topic sfx-employee --max-messages $((10 +RANDOM % 100))
+   else
+     ./opt/kafka_2.11-"$KAFKA_VERSION"/bin/kafka-console-consumer.sh --bootstrap-server "$KAFKA_BROKER" --new-consumer --topic sfx-employee --max-messages $((10 +RANDOM % 100))
+   fi
    sleep 5
  done
