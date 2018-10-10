ca3c4a952074ae69b4d7e1794755f79a9ce50c94
Jeff Cheng <36963516+jchengsfx@users.noreply.github.com>
2018-10-10 14:06:06 -0400

    Added openstack monitor test using devstack dind image (#453)

---
+++ tests/monitors/openstack/openstack_test.py
@@ -0,0 +1,25 @@
+from functools import partial as p
+from textwrap import dedent
+import pytest
+
+from helpers.util import container_ip, wait_for, run_agent, get_monitor_metrics_from_selfdescribe
+from helpers.assertions import any_metric_found
+
+pytestmark = [pytest.mark.collectd, pytest.mark.openstack, pytest.mark.monitor_without_endpoints]
+
+
+@pytest.mark.flaky(reruns=1)
+def test_openstack(devstack):
+    host = container_ip(devstack)
+    config = dedent(
+        f"""
+            monitors:
+              - type: collectd/openstack
+                authURL: http://{host}/identity/v3
+                username: admin
+                password: testing123
+        """
+    )
+    with run_agent(config) as [backend, _, _]:
+        expected_metrics = get_monitor_metrics_from_selfdescribe("collectd/openstack")
+        assert wait_for(p(any_metric_found, backend, expected_metrics), 60), "Timed out waiting for openstack metrics"
