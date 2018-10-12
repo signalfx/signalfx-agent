37563f5cdc3e108bd0a7a73634c64d14fc19723c
Charlie Smith <charlie@signalfx.com>
2018-10-12 15:44:02 -0400

    add options to docker observer to only observe ports bound to host

--- internal/core/common/docker/portmap.go
+++ internal/core/common/docker/portmap.go
@@ -6,14 +6,15 @@ import (
 )
 
 // FindHostMappedPort returns the port number of the docker port binding to the
-// underlying host, or 0 if none exists.
-func FindHostMappedPort(cont *dtypes.ContainerJSON, exposedPort nat.Port) int {
+// underlying host, or 0 if none exists.  It also returns the mapped ip that the
+// port is bound to on the underlying host, or "" if none exists.
+func FindHostMappedPort(cont *dtypes.ContainerJSON, exposedPort nat.Port) (int, string) {
 	bindings := cont.NetworkSettings.Ports[exposedPort]
 
-	for i := range bindings {
-		if port, err := nat.ParsePort(bindings[i].HostPort); err == nil {
-			return port
+	for _, binding := range bindings {
+		if port, err := nat.ParsePort(binding.HostPort); err == nil {
+			return port, binding.HostIP
 		}
 	}
-	return 0
+	return 0, ""
 }
--- tests/observers/docker_test.py
+++ tests/observers/docker_test.py
@@ -4,6 +4,7 @@ Integration tests for the docker observer
 import time
 from functools import partial as p
 from textwrap import dedent
+import string
 
 from helpers.assertions import has_datapoint_with_dim
 from helpers.util import ensure_always, run_agent, run_service, wait_for
@@ -20,12 +21,56 @@ monitors:
     intervalSeconds: 1
 """
 
+HOST_BINDING_CONFIG = string.Template(
+    """
+observers:
+  - type: docker
+    useHostBindings: true
+    ignoreNonHostBindings: true
+    labelsToDimensions:
+      mylabel: mydim
+
+monitors:
+  - type: collectd/nginx
+    discoveryRule: container_name =~ "nginx-non-host-binding" && port == 80
+    intervalSeconds: 1
+  - type: collectd/nginx
+    discoveryRule: container_name =~ "nginx-with-host-binding" && private_port == 80 && public_port == $port
+    intervalSeconds: 1
+"""
+)
+
 
 def test_docker_observer():
     with run_agent(CONFIG) as [backend, _, _]:
         with run_service("nginx", name="nginx-discovery", labels={"mylabel": "abc"}):
             assert wait_for(p(has_datapoint_with_dim, backend, "plugin", "nginx")), "Didn't get nginx datapoints"
             assert wait_for(p(has_datapoint_with_dim, backend, "mydim", "abc")), "Didn't get custom label dimension"
+        # Let nginx be removed by docker observer and collectd restart
+        time.sleep(5)
+        backend.datapoints.clear()
+        assert ensure_always(lambda: not has_datapoint_with_dim(backend, "plugin", "nginx"), 10)
+
+
+def test_docker_observer_use_host_bindings():
+    with run_service("nginx", name="nginx-non-host-binding", labels={"mylabel": "non-host-binding"}):
+        with run_service(
+            "nginx",
+            name="nginx-with-host-binding",
+            labels={"mylabel": "with-host-binding"},
+            ports={"80/tcp": ("127.0.0.1", 0)},
+        ) as container_bind:
+            with run_agent(
+                HOST_BINDING_CONFIG.substitute(
+                    port=container_bind.attrs["NetworkSettings"]["Ports"]["80/tcp"][0]["HostPort"]
+                )
+            ) as [backend, _, _]:
+                assert not wait_for(
+                    p(has_datapoint_with_dim, backend, "mydim", "non-host-binding")
+                ), "Didn't get custom label dimension"
+                assert wait_for(
+                    p(has_datapoint_with_dim, backend, "mydim", "with-host-binding")
+                ), "Didn't get custom label dimension"
         # Let nginx be removed by docker observer and collectd restart
         time.sleep(5)
         backend.datapoints.clear()
