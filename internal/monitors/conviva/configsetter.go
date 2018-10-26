7cf09824f1a995295229fd72589ac0aeb94fa976
Bremer Jonathan <bjonathan@signalfx.com>
2018-10-26 12:33:11 -0400

    fixing vet errors about calling context cancel()

--- internal/monitors/conviva/configsetter.go
+++ internal/monitors/conviva/configsetter.go
@@ -1,31 +1,34 @@
 package conviva
 
 import (
+	"context"
 	"strconv"
 	"strings"
+	"time"
 )
 
+var metriclensMetrics = map[string][]string {
+	"quality_metriclens": {
+		"Total Attempts",
+		"Video Start Failures (%)",
+		"Exits Before Video Start (%)",
+		"Plays (%)",
+		"Video Startup Time (sec)",
+		"Rebuffering Ratio (%)",
+		"Average Bitrate (kbps)",
+		"Video Playback Failures (%)",
+		"Ended Plays",
+		"Connection Induced Rebuffering Ratio (%)",
+		"Video Restart Time",
+	},
+	"audience_metriclens": {
+		"Concurrent Plays",
+		"Plays",
+		"Ended Plays",
+	},
+}
+
 func setConfigFields(m *Monitor, conf *Config)  {
-	conf.metriclensMetrics = map[string][]string{
-		"quality_metriclens": {
-			"Total Attempts",
-			"Video Start Failures (%)",
-			"Exits Before Video Start (%)",
-			"Plays (%)",
-			"Video Startup Time (sec)",
-			"Rebuffering Ratio (%)",
-			"Average Bitrate (kbps)",
-			"Video Playback Failures (%)",
-			"Ended Plays",
-			"Connection Induced Rebuffering Ratio (%)",
-			"Video Restart Time",
-		},
-		"audience_metriclens": {
-			"Concurrent Plays",
-			"Plays",
-			"Ended Plays",
-		},
-	}
 	if conf.MetricConfigs == nil {
 		conf.MetricConfigs = []*MetricConfig{{Metric: "quality_metriclens"}}
 	}
@@ -35,11 +38,13 @@ func setConfigFields(m *Monitor, conf *Config)  {
 	setMetriclensFilters(m, conf)
 }
 
-func setAccounts(m *Monitor, conf *Config)  {
-	jsonResponse, err := get(m, conf, "https://api.conviva.com/insights/2.4/accounts.json")
+func setAccounts(m *Monitor, conf *Config) error {
+	ctx, cancel := context.WithTimeout(m.ctx, time.Duration(conf.TimeoutSeconds) * time.Second)
+	defer cancel()
+	jsonResponse, err := get(ctx, m, conf, "https://api.conviva.com/insights/2.4/accounts.json")
 	if err != nil {
 		logger.Errorf("Get accounts request failed %v\n", err)
-		return
+		return err
 	}
 	accounts := jsonResponse["accounts"].(map[string]interface{})
 	for _, metricConfig := range conf.MetricConfigs {
@@ -52,9 +57,10 @@ func setAccounts(m *Monitor, conf *Config)  {
 			}
 		}
 	}
+	return nil
 }
 
-func setFilters(m *Monitor, conf *Config) {
+func setFilters(m *Monitor, conf *Config) error {
 	var jsonResponse   = map[string]map[string]interface{}{}
 	var filterIDByName = map[string]map[string]string{}
 	if conf.filterNameByID == nil {
@@ -62,12 +68,15 @@ func setFilters(m *Monitor, conf *Config) {
 	}
 	for _, metricConfig := range conf.MetricConfigs {
 		if len(jsonResponse[metricConfig.Account]) == 0 {
+			ctx, cancel := context.WithTimeout(m.ctx, time.Duration(conf.TimeoutSeconds) * time.Second)
 			var err error
 			url := "https://api.conviva.com/insights/2.4/filters.json?account=" + metricConfig.accountID
-			if jsonResponse[metricConfig.Account], err = get(m, conf, url); err != nil {
+			if jsonResponse[metricConfig.Account], err = get(ctx, m, conf, url); err != nil {
 				logger.Errorf("Failed to get filters for account %s: \n%v\n", metricConfig.Account, err)
-				continue
+				cancel()
+				return err
 			}
+			cancel()
 		}
 		if len(filterIDByName[metricConfig.Account]) == 0 {
 			filterIDByName[metricConfig.Account] = map[string]string{}
@@ -99,37 +108,44 @@ func setFilters(m *Monitor, conf *Config) {
 			}
 		}
 	}
+	return nil
 }
 
-func setMetriclensFilters(m *Monitor, conf *Config) {
+func setMetriclensFilters(m *Monitor, conf *Config) error {
 	for _, metricConfig := range conf.MetricConfigs {
 		if strings.Contains(metricConfig.Metric, "metriclens") {
 			aDimension := metricConfig.MetriclensDimensions[0]
 			for _, filterID := range metricConfig.filterIDs {
+				ctx, cancel := context.WithTimeout(m.ctx, time.Duration(conf.TimeoutSeconds) * time.Second)
 				url := "https://api.conviva.com/insights/2.4/metrics.json?metrics=" + metricConfig.Metric +
 					"&filter_ids=" + filterID +
 					"&metriclens_dimension_id=" + conf.metriclensDimensionIDByAccountAndName[metricConfig.Account][aDimension]
-				if _, err := get(m, conf, url); err == nil {
+				if _, err := get(ctx, m, conf, url); err == nil {
 					metricConfig.metriclensFilterIDs = append(metricConfig.metriclensFilterIDs, filterID)
 				}
+				cancel()
 			}
 		}
 	}
+	return nil
 }
 
-func setMetriclensDimensions(m *Monitor, conf *Config) {
+func setMetriclensDimensions(m *Monitor, conf *Config) error {
 	var metriclensDimensionsResponse = make(map[string]map[string]interface{})
 	if conf.metriclensDimensionIDByAccountAndName == nil {
 		conf.metriclensDimensionIDByAccountAndName = make(map[string]map[string]string)
 	}
 	for _, metricConfig := range conf.MetricConfigs {
 		if len(metriclensDimensionsResponse[metricConfig.Account]) == 0 {
+			ctx, cancel := context.WithTimeout(m.ctx, time.Duration(conf.TimeoutSeconds) * time.Second)
 			var err error
-			metriclensDimensionsResponse[metricConfig.Account], err = get(m, conf, "https://api.conviva.com/insights/2.4/metriclens_dimension_list.json?account=" + metricConfig.accountID)
+			metriclensDimensionsResponse[metricConfig.Account], err = get(ctx, m, conf, "https://api.conviva.com/insights/2.4/metriclens_dimension_list.json?account=" + metricConfig.accountID)
 			if err != nil {
 				logger.Errorf("Failed to get metriclens dimensions list for account %s: \n%v\n", metricConfig.Account, err)
-				continue
+				cancel()
+				return err
 			}
+			cancel()
 		}
 		var noMetriclensDimensionIDByAccountAndName, noMetriclensDimensions bool
 		if noMetriclensDimensionIDByAccountAndName = len(conf.metriclensDimensionIDByAccountAndName[metricConfig.Account]) == 0; noMetriclensDimensionIDByAccountAndName {
@@ -147,5 +163,6 @@ func setMetriclensDimensions(m *Monitor, conf *Config) {
 			}
 		}
 	}
+	return nil
 }
 
