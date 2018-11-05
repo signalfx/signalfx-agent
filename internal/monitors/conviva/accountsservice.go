2b71ba5304fd28cf7a9a835033028f529f27f919
Bremer Jonathan <bjonathan@signalfx.com>
2018-11-05 17:30:17 -0500

    returning error for functions and methods

---
+++ internal/monitors/conviva/accountsservice.go
@@ -0,0 +1,233 @@
+package conviva
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	accountsURLFormat             = "https://api.conviva.com/insights/2.4/accounts.json"
+	filtersURLFormat              = "https://api.conviva.com/insights/2.4/filters.json?account=%s"
+	metriclensDimensionsURLFormat = "https://api.conviva.com/insights/2.4/metriclens_dimension_list.json?account=%s"
+)
+
+// Account for Conviva account data
+type account struct {
+	id                   string
+	Name                 string
+	filters              map[string]string
+	metricLensFilters    map[string]string
+	metricLensDimensions map[string]float64
+}
+
+// AccountService interface for Account related methods
+type accountsService interface {
+	getDefault()                                                             (*account, error)
+	getMetricLensDimensionMap(accountName string)                            (map[string]float64, error)
+	getID(accountName string)                                                (string, error)
+	getFilters(accountName string)                                           (map[string]string, error)
+	getMetricLensFilters(accountName string)                                 (map[string]string, error)
+	getFilterID(accountName string, filterName string)                       (string, error)
+	getMetricLensDimensionID(accountName string, metricLensDimension string) (float64, error)
+}
+
+type accountsServiceImpl struct {
+	defaultAccount      *account
+	accounts            map[string]*account
+	ctx                 context.Context
+	timeout             *time.Duration
+	client              *httpClient
+	mutex               *sync.RWMutex
+	accountsInitialized bool
+}
+
+// NewAccountService factory function creating AccountService
+func newAccountsService(ctx context.Context, timeout *time.Duration, client *httpClient) accountsService {
+	service := accountsServiceImpl{ctx: ctx, timeout: timeout, client: client, mutex: &sync.RWMutex{},}
+	return &service
+}
+
+func (s *accountsServiceImpl) getDefault() (*account, error) {
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+	if s.defaultAccount == nil {
+		return nil, fmt.Errorf("could not find default account")
+	}
+	return s.defaultAccount, nil
+}
+
+func (s *accountsServiceImpl) getMetricLensDimensionMap(accountName string) (map[string]float64, error) {
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+	if a := s.accounts[accountName]; a != nil {
+			return a.metricLensDimensions, nil
+	}
+	return nil, fmt.Errorf("could not find account %s", accountName)
+}
+
+func (s *accountsServiceImpl) getID(accountName string) (string, error) {
+	if err := s.init(); err != nil {
+		return "", err
+	}
+	if a := s.accounts[accountName]; a != nil {
+		return a.id, nil
+	}
+	return "", fmt.Errorf("could not find account %s", accountName)
+}
+
+func (s *accountsServiceImpl) getFilters(accountName string) (map[string]string, error) {
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+	if a := s.accounts[accountName]; a != nil {
+		return a.filters, nil
+	}
+	return nil, fmt.Errorf("could not find account %s", accountName)
+}
+
+func (s *accountsServiceImpl) getMetricLensFilters(accountName string) (map[string]string, error) {
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+	if a := s.accounts[accountName]; a != nil {
+		return a.metricLensFilters, nil
+	}
+	return nil, fmt.Errorf("could not find account %s", accountName)
+}
+
+func (s *accountsServiceImpl) getFilterID(accountName string, filterName string) (string, error) {
+	if err := s.init(); err != nil {
+		return "", err
+	}
+	if a := s.accounts[accountName]; a != nil {
+			for id, name := range a.filters {
+				if name == filterName {
+					return id, nil
+				}
+			}
+		}
+	return "", fmt.Errorf("no filter id for filter %s in account %s", filterName, accountName)
+}
+
+func (s *accountsServiceImpl) getMetricLensDimensionID(accountName string, metricLensDimension string) (float64, error) {
+	if err := s.init(); err != nil {
+		return 0, err
+	}
+	if a := s.accounts[accountName]; a != nil {
+		if m := a.metricLensDimensions; m != nil {
+			return m[metricLensDimension], nil
+		}
+	}
+	return 0, fmt.Errorf("account %s has no MetricLens dimensions", accountName)
+}
+
+func (s *accountsServiceImpl) init() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if !s.accountsInitialized {
+		ctx, cancel := context.WithTimeout(s.ctx, *s.timeout)
+		defer cancel()
+		res := struct {
+			Default  string            `json:"default"`
+			Count    float64           `json:"count"`
+			Accounts map[string]string `json:"accounts"`
+		}{}
+		if _, err := (*s.client).get(ctx, &res, accountsURLFormat); err != nil {
+			return err
+		}
+		s.accounts = make(map[string]*account, len(res.Accounts))
+		for name, id := range res.Accounts {
+			a := account{Name: name, id: id,}
+			if a.Name == res.Default {
+				s.defaultAccount = &a
+			}
+			s.accounts[name] = &a
+		}
+		if err := s.initFilters(); err != nil {
+			return err
+		}
+		if err := s.initMetriclensDimensions(); err != nil {
+			return err
+		}
+		if err := s.initMetricLensFilters(); err != nil {
+			return err
+		}
+		s.accountsInitialized = true
+	}
+	return nil
+}
+
+func (s *accountsServiceImpl) initFilters() error {
+	var g errgroup.Group
+	for _, a := range s.accounts {
+		newA := a
+		newA.filters = map[string]string{}
+		g.Go(func() error {
+			ctx, cancel := context.WithTimeout(s.ctx, *s.timeout)
+			defer cancel()
+			if _, err := (*s.client).get(ctx, &newA.filters, fmt.Sprintf(filtersURLFormat, newA.id)); err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	return g.Wait()
+}
+
+func (s *accountsServiceImpl) initMetriclensDimensions() error {
+	var g errgroup.Group
+	for _, a := range s.accounts {
+		newA := a
+		newA.metricLensDimensions = map[string]float64{}
+		g.Go(func() error {
+			ctx, cancel := context.WithTimeout(s.ctx, *s.timeout)
+			defer cancel()
+			if _, err := (*s.client).get(ctx, &newA.metricLensDimensions, fmt.Sprintf(metriclensDimensionsURLFormat, newA.id)); err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	return g.Wait()
+}
+
+// initMetricLensFilters() creates a map of MetricLens enabled filters from the map of filters of accounts.
+// A filter is MetricLens enabled if getting quality_metriclens metrics is successful (i.e. no errors and the status code is 200).
+// A filter is not MetricLens enabled if the status code is 400. Otherwise an error occurred and that error gets returned.
+func (s *accountsServiceImpl) initMetricLensFilters() error {
+	var (
+		g errgroup.Group
+		mutex  sync.RWMutex
+	)
+	for _, a := range s.accounts {
+		newA := a
+		newA.metricLensFilters = map[string]string{}
+		for filterID, filter := range newA.filters {
+			newFilterID := filterID
+			newFilter   := filter
+			g.Go(func() error {
+				ctx, cancel := context.WithTimeout(s.ctx, *s.timeout)
+				defer cancel()
+				for _, dimID := range newA.metricLensDimensions {
+					if statusCode, err := (*s.client).get(ctx, &map[string]interface{}{}, fmt.Sprintf(metricLensURLFormat, "quality_metriclens", newA.id, newFilterID, int(dimID))); err == nil && statusCode == 200 {
+						mutex.Lock()
+						newA.metricLensFilters[newFilterID] = newFilter
+						mutex.Unlock()
+					} else {
+						if statusCode < 400 || statusCode > 499 {
+							return err
+						}
+					}
+					break
+				}
+				return nil
+			})
+		}
+	}
+	return g.Wait()
+}
