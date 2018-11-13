0173e81f1b1e934f703698c71c80054bd3cc46f0
Akash Suresh <akash@signalfx.com>
2018-11-13 17:29:57 -0500

    Add test for additionalMetrics option

--- tests/monitors/elasticsearch/elasticsearch_test.py
+++ tests/monitors/elasticsearch/elasticsearch_test.py
@@ -3,7 +3,7 @@ from textwrap import dedent
 import os
 import pytest
 
-from helpers.assertions import has_datapoint_with_dim, http_status, has_log_message
+from helpers.assertions import has_datapoint_with_dim, http_status, has_log_message, has_datapoint_with_metric_name
 from helpers.kubernetes.utils import get_discovery_rule, run_k8s_monitors_test
 from helpers.util import (
     get_monitor_dims_from_selfdescribe,
@@ -136,6 +136,39 @@ def test_elasticsearch_with_threadpool():
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "thread_pool", "bulk")
             ), "Didn't get bulk thread pool metrics"
+            assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
+
+
+@pytest.mark.flaky(reruns=2)
+def test_elasticsearch_with_additionalMetrics():
+    with run_service("elasticsearch/6.2.0", environment={"cluster.name": "testCluster"}) as es_container:
+        host = container_ip(es_container)
+        assert wait_for(
+            p(http_status, url=f"http://{host}:9200/_nodes/_local", status=[200]), 180
+        ), "service didn't start"
+        config = dedent(
+            f"""
+            monitors:
+            - type: collectd/elasticsearch
+              host: {host}
+              port: 9200
+              username: elastic
+              password: testing123
+              additionalMetrics:
+               - cluster.initializing-shards
+               - thread_pool.threads
+            """
+        )
+        with run_agent(config) as [backend, get_output, _]:
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "elasticsearch")
+            ), "Didn't get elasticsearch datapoints"
+            assert wait_for(
+                p(has_datapoint_with_metric_name, backend, "gauge.cluster.initializing-shards")
+            ), "Didn't get gauge.cluster.initializing-shards metric"
+            assert wait_for(
+                p(has_datapoint_with_metric_name, backend, "gauge.thread_pool.threads")
+            ), "Didn't get gauge.thread_pool.threads metric"
             assert not has_log_message(get_output().lower(), "error"), "error found in agent output!"
 
 
