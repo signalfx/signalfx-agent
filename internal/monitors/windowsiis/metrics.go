1b3e50874aaa030851a6b38b20e50e7d597ef29c
Charlie Smith <charlie@signalfx.com>
2019-01-18 16:30:45 -0500

    add windows iis monitor

---
+++ internal/monitors/windowsiis/metrics.go
@@ -0,0 +1,71 @@
+package windowsiis
+
+// GAUGE(web_service.current_connections): Number of current connections to the
+// web service
+
+// GAUGE(web_service.connection_attempts_sec): Rate that connections to web
+// service are attempted Requests
+
+// GAUGE(web_service.post_requests_sec): Rate of HTTP POST requests
+
+// GAUGE(web_service.get_requests_sec): Rate of HTTP GET requests
+
+// GAUGE(web_service.total_method_requests_sec): Rate at which all HTTP requests
+// are received
+
+// GAUGE(web_service.bytes_received_sec): Rate that data is received by web
+// service
+
+// GAUGE(web_service.bytes_sent_sec): Rate that data is sent by web service
+
+// GAUGE(web_service.files_received_sec): Rate at which files are received by
+// web service
+
+// GAUGE(web_service.files_sent_sec): Rate at which files are sent by web
+// service
+
+// GAUGE(web_service.not_found_errors_sec): Rate of 'Not Found' Errors
+
+// GAUGE(web_service.anonymous_users_sec): Rate at which users are making
+// anonymous requests to the web service
+
+// GAUGE(web_service.nonanonymous_users_sec): Rate at which users are making
+// nonanonymous requests to the web service
+
+// COUNTER(web_service.service_uptime): Service uptime
+
+// GAUGE(web_service.isapi_extension_requests_sec): Rate of ISAPI extension
+// request processed simultaneously by the web service
+
+// GAUGE(process.handle_count): The total number of handles currently open by
+// this process. This number is equal to the sum of the handles currently open
+// by each thread in this process.
+
+// GAUGE(process.pct_processor_time): The percentage of elapsed time that all
+// process threads used the processor to execution instructions. Code executed
+// to handle some hardware interrupts and trap conditions are included in this
+// count.
+
+// GAUGE(process.id_process): The unique identifier of this process. ID Process
+// numbers are reused, so they only identify a process for the lifetime of that
+// process.
+
+// GAUGE(process.private_bytes): The current size, in bytes, of memory that this
+// process has allocated that cannot be shared with other processes.
+
+// GAUGE(process.thread_count): The number of threads currently active in this
+// process. Every running process has at least one thread.
+
+// GAUGE(process.virtual_bytes): The current size, in bytes, of the virtual
+// address space the process is using. Use of virtual address space does not
+// necessarily imply corresponding use of either disk or main memory pages.
+// Virtual space is finite, and the process can limit its ability to load
+// libraries.
+
+// GAUGE(process.working_set): The current size, in bytes, of the Working Set of
+// this process. The Working Set is the set of memory pages touched recently by
+// the threads in the process. If free memory in the computer is above a
+// threshold, pages are left in the Working Set of a process even if they are
+// not in use. When free memory falls below a threshold, pages are trimmed from
+// Working Sets. If they are needed, they will then be soft-faulted back into
+// the Working Set before leaving main memory.
---
+++ internal/monitors/windowsiis/windowsiis.go
@@ -0,0 +1,55 @@
+package windowsiis
+
+import (
+	"time"
+
+	"github.com/signalfx/signalfx-agent/internal/core/config"
+	"github.com/signalfx/signalfx-agent/internal/monitors"
+	"github.com/signalfx/signalfx-agent/internal/monitors/types"
+	log "github.com/sirupsen/logrus"
+)
+
+const monitorType = "windows-iis"
+
+// MONITOR(windows-iis):
+// This monitor reports metrics for Windows Internet Information Services.
+// It is used to drive the Windows IIS dashboard content.
+//
+// ## Windows Performance Counters
+// The underlying source for these metrics are Windows Performance Counters.
+// Most of the performance counters that we query in this monitor are actually Gauges
+// that represent rates per second and percentages.
+// This monitor reports the instantaneous values for these Windows Performance Counters.
+// This means that in between a collection interval, spikes could occur on the
+// Performance Counters.  The best way to mitigate this limitation is to increase
+// the reporting interval on this monitor to collect more frequently.
+//
+// Sample YAML configuration:
+//
+// ```yaml
+// monitors:
+//  - type: windows-iis
+// ```
+
+var logger = log.WithFields(log.Fields{"monitorType": monitorType})
+
+// Config for this monitor
+type Config struct {
+	config.MonitorConfig `singleInstance:"true" acceptsEndpoints:"false"`
+	// (Windows Only) Number of seconds that wildcards in counter paths should
+	// be expanded and how often to refresh counters from configuration.
+	CountersRefreshInterval time.Duration `yaml:"counterRefreshInterval" default:"60s"`
+	// (Windows Only) Print out the configurations that match available
+	// performance counters.  This used for debugging.
+	PrintValid bool `yaml:"printValid"`
+}
+
+func init() {
+	monitors.Register(monitorType, func() interface{} { return &Monitor{} }, &Config{})
+}
+
+// Monitor for Utilization
+type Monitor struct {
+	Output types.Output
+	cancel func()
+}
