c6dcfe780e26e47a51306db08c455328a08699ac
Charlie Smith <charlie@signalfx.com>
2019-01-27 16:00:10 -0500

    add function to rename a measurement field with the value of a tag

--- internal/monitors/telegraf/common/measurement/measurement.go
+++ internal/monitors/telegraf/common/measurement/measurement.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/signalfx/golib/datapoint"
+	"github.com/signalfx/signalfx-agent/internal/utils"
 )
 
 // Measurement is a storage struct for storing measurements from telegraf
@@ -20,13 +21,29 @@ type Measurement struct {
 
 // RenameFieldWithTag - takes the value of a specified tag and uses it to rename a specified field
 // the tag is deleted and the original field name is overwritten
-func RenameFieldWithTag(m *Measurement, tagName string, fieldName string, replacer *strings.Replacer) {
+func (m *Measurement) RenameFieldWithTag(tagName string, fieldName string, replacer *strings.Replacer) {
 	if tagVal, ok := m.Tags[tagName]; ok {
-		tagVal = replacer.Replace(strings.ToLower(tagVal))
+		tagVal = replacer.Replace(tagVal)
 		if val, ok := m.Fields[fieldName]; ok && tagVal != "" {
 			m.Fields[tagVal] = val
 			delete(m.Fields, fieldName)
 			delete(m.Tags, tagName)
 		}
+	}
+}
+
+// New creates a new measurement from telegraf measurement name, fields, tags, etc.
+func New(measurement string, fields map[string]interface{},
+	tags map[string]string, metricType datapoint.MetricType,
+	originalMetricType string, t ...time.Time) *Measurement {
+	return &Measurement{
+		Measurement: measurement,
+		Fields:      fields,
+		// telegraf doc says that tags are owned by the calling plugin and they
+		// shouldn't be mutated.  So we copy the tags map
+		Tags:               utils.CloneStringMap(tags),
+		MetricType:         metricType,
+		OriginalMetricType: originalMetricType,
+		Timestamps:         t,
 	}
 }
