53e7b76f129de88f5f1f8c63e72f620d6e1954c2
Charlie Smith <charlie@signalfx.com>
2019-01-27 22:19:05 -0500

    fix duplicate documentation in winservices monitor

--- internal/monitors/telegraf/monitors/winservices/winservices.go
+++ internal/monitors/telegraf/monitors/winservices/winservices.go
@@ -31,16 +31,6 @@ const monitorType = "telegraf/win_services"
 // ```
 //
 
-// Metrics
-
-// GAUGE(win_services.state): The current state of the service: 1 (stopped),
-// 2 (start pending), 3 (stop pending), 4 (running), 5 (continue pending),
-// 6 (pause pending), or 7 (paused)
-
-// GAUGE(win_services.startup_mode): The startup mode configured for the
-// service: 0 (boot start), 1 (system start), 2 (auto start), 3 (demand start),
-// or 4 (disabled)
-
 var logger = log.WithFields(log.Fields{"monitorType": monitorType})
 
 func init() {
