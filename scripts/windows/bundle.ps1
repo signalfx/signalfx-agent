1f7351038ed1caa8a41f689f8311fe30268ce132
Charlie Smith <charlie@signalfx.com>
2019-01-30 09:12:35 -0500

    add default config to windows packaging (#552)
    * prevent make file vagrant up target from reloading if vagrant already created
    
    * add script to extend trial on windows server 2008 image
    
    * exclude boxcutter and vagrant stuff from being copied into dev image
    
    * refactor windows bundle script
    
    * cache dependencies in build dir
    * restructure bundle
    * update appveyor path to agent bin
    
    * add default agent.yaml for windows
    
    * add relative default config path for windows
    
    * add default config path on windows
    
    * disable check for errors in ms sqlserver test temporarily
    
    * remove files from package build area if they exist
    
    * fix bad path in win agent.yaml

--- scripts/windows/bundle.ps1
+++ scripts/windows/bundle.ps1
@@ -31,8 +31,16 @@ function download_python([string]$url=$PYTHON_MSI_URL, [string]$outputDir=$BUILD
     download_file -url $url -outputDir $outputDir -fileName $installerName
 }
 
-function copy_types_db([string]$collectdCommit, [string]$buildDir=$BUILD_DIR) {
-    cp "$buildDir\collectd\collectd-$collectdCommit\src\types.db" "$buildDir\plugins\collectd\types.db"
+function copy_types_db([string]$collectdCommit, [string]$buildDir=$BUILD_DIR, [string]$agentName="SignalFxAgent") {
+    cp "$buildDir\collectd\collectd-$collectdCommit\src\types.db" "$buildDir\$agentName\plugins\collectd\types.db"
+}
+
+function copy_whitelist([string]$buildDir=$BUILD_DIR, [string]$agentName="SignalFxAgent"){
+    cp "$scriptDir\..\..\whitelist.json" "$buildDIR\$agentName\lib\whitelist.json"
+}
+
+function copy_default_config([string]$buildDir=$BUILD_DIR, [string]$agentName="SignalFxAgent"){
+    cp "$scriptDir\..\..\packaging\win\agent.yaml" "$buildDir\$agentName\etc\signalfx\agent.yaml"
 }
 
 function install_python([string]$buildDir=$BUILD_DIR, [string]$installerName=$PYTHON_INSTALLER_NAME) {
@@ -66,7 +74,7 @@ function install_pip([string]$buildDir=$BUILD_DIR) {
 # install sfxpython package from the local directory
 function bundle_python_runner($buildDir=".\build") {
     $python = Resolve-Path -Path "$buildDir\python\python.exe"
-    $bundlePath = Resolve-Path -Path "$buildDir\..\..\python"
+    $bundlePath = Resolve-Path -Path "$buildDir\..\python"
     $arguments = "-m", "pip", "install", "-qq", "$bundlePath", "--upgrade"
     $env:PYTHONHOME="$buildDir\python"
     & $python $arguments
