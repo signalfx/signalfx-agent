7d268abacad3bf8e645f44e75725010f448bc83d
Charlie Smith <charlie@signalfx.com>
2019-01-29 19:06:32 -0500

    refactor windows bundle script
    * cache dependencies in build dir
    * restructure bundle
    * update appveyor path to agent bin

--- scripts/windows/make.ps1
+++ scripts/windows/make.ps1
@@ -20,58 +20,61 @@ function bundle (
         [string]$AGENT_VERSION="",
         [string]$buildDir="$scriptDir\..\..\build",
         [bool]$BUILD_AGENT=$true,
-        [bool]$DOWNLOAD_PYTHON=$true,
-        [bool]$DOWNLOAD_COLLECTD=$true,
-        [bool]$DOWNLOAD_COLLECTD_PLUGINS=$true,
-        [bool]$REMOVE_UNECESSARY_FILES=$true,
+        [bool]$DOWNLOAD_PYTHON=$false,
+        [bool]$DOWNLOAD_COLLECTD=$false,
+        [bool]$DOWNLOAD_COLLECTD_PLUGINS=$false,
         [bool]$ZIP_BUNDLE=$true,
-        [bool]$ONLY_BUILD_AGENT=$false) {
+        [bool]$ONLY_BUILD_AGENT=$false,
+        [string]$AGENT_NAME="SignalFxAgent") {
 
     if ((!$AGENT_VERSION) -Or ($AGENT_VERSION="")){
         $AGENT_VERSION = & git rev-parse HEAD
     }
 
-    if ($ONLY_BUILD_AGENT) {
-        $DOWNLOAD_COLLECTD = $false
-        $DOWNLOAD_PYTHON = $false
-        $DOWNLOAD_COLLECTD_PLUGINS = $false
-        $REMOVE_UNECESSARY_FILES = $false
-        $ZIP_BUNDLE = $false
+    # create directories in the agent directory
+    mkdir "$buildDir\$AGENT_NAME\bin" -ErrorAction Ignore
+    mkdir "$buildDir\$AGENT_NAME\etc\signalfx" -ErrorAction Ignore
+    mkdir "$buildDir\$AGENT_NAME\lib" -ErrorAction Ignore
+
+    if ($BUILD_AGENT) {
+        Remove-Item -Recurse -Force "$buildDir\$AGENT_NAME\bin\signalfx-agent.exe" -ErrorAction Ignore
+        signalfx-agent -AGENT_VERSION "$AGENT_VERSION" -AGENT_BIN "$buildDir\$AGENT_NAME\bin\signalfx-agent.exe"
     }
 
-    $buildDir = "$buildDir\signalfx-agent"
-    mkdir "$buildDir\bin" -ErrorAction Ignore
-    
-    if ($BUILD_AGENT) {
-        Remove-Item -Recurse -Force "$buildDir\bin\signalfx-agent.exe" -ErrorAction Ignore
-        signalfx-agent -AGENT_VERSION "$AGENT_VERSION" -AGENT_BIN "$buildDir\bin\signalfx-agent.exe"
-    }
-    if ($DOWNLOAD_PYTHON) {
+    if (($DOWNLOAD_PYTHON -Or !(Test-Path -Path "$buildDir\python")) -And !$ONLY_BUILD_AGENT) {
         Remove-Item -Recurse -Force "$buildDir\python" -ErrorAction Ignore
         download_python -outputDir $buildDir
         install_python -buildDir $buildDir
         install_pip -buildDir $buildDir
-        bundle_python_runner -buildDir $buildDir
     }
-    if ($DOWNLOAD_COLLECTD_PLUGINS) {
+
+    if (($DOWNLOAD_COLLECTD_PLUGINS -Or !(Test-Path -Path "$buildDir\plugins")) -And !$ONLY_BUILD_AGENT) {
         Remove-Item -Recurse -Force "$buildDir\plugins\collectd" -ErrorAction Ignore
+        bundle_python_runner -buildDir "$buildDir"
         get_collectd_plugins -buildDir "$buildDir"
     }
-    if ($DOWNLOAD_COLLECTD) {
+
+    if (($DOWNLOAD_COLLECTD -Or !(Test-Path -Path "$buildDir\collectd")) -And !$ONLY_BUILD_AGENT) {
         Remove-Item -Recurse -Force "$buildDir\collectd" -ErrorAction Ignore
         mkdir "$buildDir\collectd" -ErrorAction Ignore
         download_collectd -collectdCommit $COLLECTD_COMMIT -outputDir "$buildDir"
         unzip_file -zipFile "$buildDir\collectd.zip" -outputDir "$buildDir\collectd"
     }
-    copy_types_db -collectdCommit $COLLECTD_COMMIT -buildDir "$buildDir"
-    if ($REMOVE_UNECESSARY_FILES) {
-        Remove-Item -Recurse -Force "$buildDir\collectd"
-        Remove-Item -Recurse -Force "$buildDir\collectd.zip"
-        Remove-Item -Recurse -Force "$buildDir\python-installer.msi"
-    }
-    if ($ZIP_BUNDLE) {
-        zip_file -src "$buildDir\..\signalfx-agent" -dest "$buildDir\..\signalfx-agent"
-        mv "$buildDir\..\signalfx-agent.zip" "$buildDir\..\signalfx-agent-$AGENT_VERSION-win64.zip"
+
+    # copy default whitelist into agent directory
+    copy_whitelist -buildDir "$buildDir" -AGENT_NAME "$AGENT_NAME"
+    # copy default config into agent directory
+    copy_default_config -buildDir "$buildDir" -AGENT_NAME "$AGENT_NAME"
+    # copy python into agent directory
+    Copy-Item -Path "$buildDir\python" -Destination "$buildDir\$AGENT_NAME\python" -recurse -Force
+    # copy plugins into agent directory
+    Copy-Item -Path "$buildDir\plugins" -Destination "$buildDir\$AGENT_NAME\plugins" -recurse -Force
+    # copy types.db file into agent directory
+    copy_types_db -collectdCommit $COLLECTD_COMMIT -buildDir "$buildDir" -agentName "$AGENT_NAME"
+
+    if ($ZIP_BUNDLE -And !$ONLY_BUILD_AGENT) {
+        zip_file -src "$buildDir\$AGENT_NAME" -dest "$buildDir\$AGENT_NAME"
+        mv "$buildDir\$AGENT_NAME.zip" "$buildDir\$AGENT_NAME-$AGENT_VERSION-win64.zip"
     }
 }
 
