a65dc7cc336b2f3a6043217475e221cb94f2172e
charliesignalfx <charlie@signalfx.com>
2019-02-13 13:54:12 -0500

    fix typo in taildoc

--- internal/monitors/telegraf/monitors/tail/tail.go
+++ internal/monitors/telegraf/monitors/tail/tail.go
@@ -23,7 +23,7 @@ const monitorType = "telegraf/tail"
 
 // MONITOR(telegraf/tail): This monitor is based on the Telegraf tail plugin.  The monitor tails files and
 // named pipes.  The Telegraf parser configured with this monitor extracts metrics in different
-// [formats[(https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md)
+// [formats](https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md)
 // from the tailed output. More information about the Telegraf plugin
 // can be found [here](https://github.com/influxdata/telegraf/tree/master/plugins/inputs/tail).
 // All metrics emitted from this monitor will have the `plugin` dimension set to `telegraf-tail`
