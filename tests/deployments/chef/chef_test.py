ae0fa6432b91276adae1cd5517fb5577110f9bf8
Jeff Cheng <jcheng@signalfx.com>
2019-02-27 15:42:08 -0500

    Test upgrade/downgrade with chef cookbook

--- tests/deployments/chef/chef_test.py
+++ tests/deployments/chef/chef_test.py
@@ -1,8 +1,11 @@
 # Tests of the chef cookbook
 
 import os
+import re
+import tempfile
 from functools import partial as p
 
+import json
 import pytest
 
 from tests.helpers.assertions import has_datapoint_with_dim
@@ -20,7 +23,8 @@ from tests.packaging.common import (
 
 pytestmark = [pytest.mark.chef, pytest.mark.deployment]
 
-CHEF_CMD = "chef-client -z -o 'recipe[signalfx_agent::default]' -j cookbooks/signalfx_agent/attributes.json"
+ATTRIBUTES_PATH = os.path.join(PROJECT_DIR, "deployments/chef/example_attrs.json")
+CHEF_CMD = "chef-client -z -o 'recipe[signalfx_agent::default]' -j {0}"
 DOCKERFILES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "images"))
 
 SUPPORTED_DISTROS = [
@@ -37,21 +41,89 @@ SUPPORTED_DISTROS = [
 ]
 
 
+def get_agent_version(cont):
+    code, output = cont.exec_run("signalfx-agent -version")
+    output = output.decode("utf-8").strip()
+    assert code == 0, "command 'signalfx-agent -version' failed:\n%s" % output
+    match = re.match("^.+?: (.+)?,", output)
+    assert match and match.group(1).strip(), "failed to parse agent version from command output:\n%s" % output
+    return match.group(1).strip()
+
+
+def run_chef_client(cont, package_version=None):
+    attributes = json.loads(open(ATTRIBUTES_PATH, "r").read())
+    attributes["signalfx_agent"]["package_version"] = package_version
+    print(attributes)
+    with tempfile.NamedTemporaryFile(mode="w", dir="/tmp/scratch") as fd:
+        fd.write(json.dumps(attributes))
+        fd.flush()
+        print('running "%s" ...' % CHEF_CMD.format(fd.name))
+        code, output = cont.exec_run(CHEF_CMD.format(fd.name))
+        output = output.decode("utf-8").strip()
+        assert code == 0, "failed to install agent:\n%s" % output
+        print(output)
+    assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
+    return get_agent_version(cont)
+
+
 @pytest.mark.parametrize("base_image,init_system", SUPPORTED_DISTROS)
 def test_chef(base_image, init_system):
-    if base_image in ("debian-7-wheezy", "ubuntu1404"):
-        pytest.skip("Wait for fix in debian init.d script to be released")
+    if base_image == "debian-7-wheezy":
+        pytest.skip(
+            "Bug in chef-client, fails with 'Malformed version number string' when parsing the apt version. \
+            Latest chef-client no longer supports debian 7."
+        )
+
     dockerfile = os.path.join(DOCKERFILES_DIR, "Dockerfile.%s" % base_image)
     with run_init_system_image(base_image, path=PROJECT_DIR, dockerfile=dockerfile) as [cont, backend]:
-        code, output = cont.exec_run(CHEF_CMD)
-        print(output.decode("utf-8"))
-        assert code == 0, "failed to install agent"
-
         try:
+            # install latest agent
+            run_chef_client(cont)
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "signalfx-metadata")
             ), "Datapoints didn't come through"
-            assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
+        finally:
+            print("Agent log:")
+            print_lines(get_agent_logs(cont, init_system))
+
+
+@pytest.mark.upgrade_downgrade
+@pytest.mark.parametrize("base_image,init_system", SUPPORTED_DISTROS)
+def test_chef_upgrade_downgrade(base_image, init_system):
+    if base_image == "debian-7-wheezy":
+        pytest.skip(
+            "Bug in chef-client, fails with 'Malformed version number string' when parsing the apt version. \
+            Latest chef-client no longer supports debian 7."
+        )
+
+    if base_image == "ubuntu1404":
+        pytest.skip("Wait for multiple agent versions to be released with the fix in the debian init.d script")
+
+    dockerfile = os.path.join(DOCKERFILES_DIR, "Dockerfile.%s" % base_image)
+    with run_init_system_image(base_image, path=PROJECT_DIR, dockerfile=dockerfile) as [cont, backend]:
+        try:
+            agent_version = run_chef_client(cont, "4.0.1-1")
+            assert agent_version == "4.0.1", "agent version is not 4.0.1: %s" % agent_version
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "signalfx-metadata")
+            ), "Datapoints didn't come through"
+
+            # upgrade agent
+            agent_version = run_chef_client(cont, "4.0.2-1")
+            assert agent_version == "4.0.2", "agent version is not 4.0.2: %s" % agent_version
+            backend.reset_datapoints()
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "signalfx-metadata")
+            ), "Datapoints didn't come through"
+
+            # downgrade agent for distros that support package downgrades
+            if base_image not in ("debian-7-wheezy", "debian-8-jessie", "ubuntu1404"):
+                agent_version = run_chef_client(cont, "4.0.0-1")
+                assert agent_version == "4.0.0", "agent version is not 4.0.0: %s" % agent_version
+                backend.reset_datapoints()
+                assert wait_for(
+                    p(has_datapoint_with_dim, backend, "plugin", "signalfx-metadata")
+                ), "Datapoints didn't come through"
         finally:
             print("Agent log:")
             print_lines(get_agent_logs(cont, init_system))
