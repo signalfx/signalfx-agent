df7b16c1dfdcb46aa57361aba39c05fce930677a
Jeff Cheng <jcheng@signalfx.com>
2019-03-04 11:02:58 -0500

    Added consul integration test

--- tests/monitors/consul/consul_test.py
+++ tests/monitors/consul/consul_test.py
@@ -1,11 +1,45 @@
 import os
+import string
+from functools import partial as p
 
 import pytest
 
+from tests.helpers.assertions import has_datapoint_with_metric_name, tcp_socket_open
 from tests.helpers.kubernetes.utils import get_discovery_rule, run_k8s_monitors_test
-from tests.helpers.util import get_monitor_dims_from_selfdescribe, get_monitor_metrics_from_selfdescribe
+from tests.helpers.util import (
+    container_ip,
+    get_monitor_dims_from_selfdescribe,
+    get_monitor_metrics_from_selfdescribe,
+    run_agent,
+    run_container,
+    wait_for,
+)
 
 pytestmark = [pytest.mark.collectd, pytest.mark.consul, pytest.mark.monitor_with_endpoints]
+
+
+CONSUL_CONFIG = string.Template(
+    """
+monitors:
+  - type: collectd/consul
+    host: $host
+    port: 8500
+    enhancedMetrics: true
+"""
+)
+
+
+def test_consul():
+    with run_container("consul:0.9.3") as consul_cont:
+        host = container_ip(consul_cont)
+        config = CONSUL_CONFIG.substitute(host=host)
+
+        assert wait_for(p(tcp_socket_open, host, 8500), 60), "consul service didn't start"
+
+        with run_agent(config) as [backend, _, _]:
+            assert wait_for(
+                p(has_datapoint_with_metric_name, backend, "gauge.consul.catalog.services.total"), 60
+            ), "Didn't get consul datapoints"
 
 
 @pytest.mark.k8s
