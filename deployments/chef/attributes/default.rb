b2698199baf60faa6d3c0f5c1b6f30d8646661df
Ben Keith <bkeith@signalfx.com>
2019-03-27 14:12:13 -0400

    Add 'agent_version' attribute to Chef cookbook
    This is used to derive package_version

--- deployments/chef/attributes/default.rb
+++ deployments/chef/attributes/default.rb
@@ -13,6 +13,8 @@ default['signalfx_agent']['windows_repo_url'] = "#{node['signalfx_agent']['repo_
 
 default['signalfx_agent']['service_name'] = 'signalfx-agent'
 
+default['signalfx_agent']['package_version'] = nil
+
 case node['platform_family']
 when 'windows'
   default['signalfx_agent']['conf_file_path'] = '\ProgramData\SignalFxAgent\agent.yaml'
@@ -21,11 +23,16 @@ when 'windows'
   default['signalfx_agent']['user'] = 'Administrator'
   default['signalfx_agent']['group'] = 'Administrator'
   default['signalfx_agent']['package_url'] = "#{node['signalfx_agent']['windows_repo_url']}/#{node['signalfx_agent']['package_stage']}/zip/SignalFxAgent-#{node['signalfx_agent']['package_version']}-win64.zip"
+  if node['signalfx_agent']['agent_version']
+    default['signalfx_agent']['package_version'] = default['signalfx_agent']['agent_version'].sub('v', '')
+  end
 else
   default['signalfx_agent']['conf_file_path'] = '/etc/signalfx/agent.yaml'
   default['signalfx_agent']['user'] = 'signalfx-agent'
   default['signalfx_agent']['group'] = 'signalfx-agent'
+  if node['signalfx_agent']['agent_version']
+    default['signalfx_agent']['package_version'] = node['signalfx_agent']['agent_version'].sub('v', '') + '-1'
+  end
 end
-default['signalfx_agent']['package_version'] = nil
 
 default['signalfx_agent']['conf'] = {}
