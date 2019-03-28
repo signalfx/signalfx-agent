d97038dafc957065cd0aa68a9a5af50b2035d2be
Jeff Cheng <jcheng@signalfx.com>
2019-03-28 10:25:19 -0400

    Release chef cookbook 0.2.5

--- deployments/chef/metadata.rb
+++ deployments/chef/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'SignalFx, Inc.'
 maintainer_email 'support@signalfx.com'
 license 'Apache-2.0'
 description 'Installs/Configures the SignalFx Agent'
-version '0.2.4'
+version '0.2.5'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'amazon'
