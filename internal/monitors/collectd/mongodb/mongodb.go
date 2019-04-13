185b8b7557b49b44072416e500399dddd97f257c
Mark Stumpf <mstumpf@signalfx.com>
2019-04-13 08:26:08 -0700

    Add 2 mongodb config options to map to collectd config (#711)

--- internal/monitors/collectd/mongodb/mongodb.go
+++ internal/monitors/collectd/mongodb/mongodb.go
@@ -27,18 +27,20 @@ func init() {
 
 // Config is the monitor-specific config with the generic config embedded
 type Config struct {
-	config.MonitorConfig   `yaml:",inline" acceptsEndpoints:"true"`
-	pyConf                 *python.Config
-	Host                   string   `yaml:"host" validate:"required"`
-	Port                   uint16   `yaml:"port" validate:"required"`
-	Databases              []string `yaml:"databases" validate:"required"`
-	Username               string   `yaml:"username"`
-	Password               string   `yaml:"password" neverLog:"true"`
-	UseTLS                 *bool    `yaml:"useTLS"`
-	CACerts                string   `yaml:"caCerts"`
-	TLSClientCert          string   `yaml:"tlsClientCert"`
-	TLSClientKey           string   `yaml:"tlsClientKey"`
-	TLSClientKeyPassPhrase string   `yaml:"tlsClientKeyPassPhrase"`
+	config.MonitorConfig     `yaml:",inline" acceptsEndpoints:"true"`
+	pyConf                   *python.Config
+	Host                     string   `yaml:"host" validate:"required"`
+	Port                     uint16   `yaml:"port" validate:"required"`
+	Databases                []string `yaml:"databases" validate:"required"`
+	Username                 string   `yaml:"username"`
+	Password                 string   `yaml:"password" neverLog:"true"`
+	UseTLS                   *bool    `yaml:"useTLS"`
+	CACerts                  string   `yaml:"caCerts"`
+	TLSClientCert            string   `yaml:"tlsClientCert"`
+	TLSClientKey             string   `yaml:"tlsClientKey"`
+	TLSClientKeyPassPhrase   string   `yaml:"tlsClientKeyPassPhrase"`
+	SendCollectionMetrics    *bool    `yaml:"sendCollectionMetrics"`
+	SendCollectionTopMetrics *bool    `yaml:"sendCollectionTopMetrics"`
 }
 
 // PythonConfig returns the embedded python.Config struct from the interface
@@ -69,16 +71,18 @@ func (m *Monitor) Configure(conf *Config) error {
 		ModulePaths:   []string{collectd.MakePath("mongodb")},
 		TypesDBPaths:  []string{collectd.MakePath("types.db")},
 		PluginConfig: map[string]interface{}{
-			"Host":                   conf.Host,
-			"Port":                   conf.Port,
-			"Database":               conf.Databases,
-			"UseTLS":                 conf.UseTLS,
-			"User":                   conf.Username,
-			"Password":               conf.Password,
-			"CACerts":                conf.CACerts,
-			"TLSClientCert":          conf.TLSClientCert,
-			"TLSClientKey":           conf.TLSClientKey,
-			"TLSClientKeyPassphrase": conf.TLSClientKeyPassPhrase,
+			"Host":                     conf.Host,
+			"Port":                     conf.Port,
+			"Database":                 conf.Databases,
+			"UseTLS":                   conf.UseTLS,
+			"User":                     conf.Username,
+			"Password":                 conf.Password,
+			"CACerts":                  conf.CACerts,
+			"TLSClientCert":            conf.TLSClientCert,
+			"TLSClientKey":             conf.TLSClientKey,
+			"TLSClientKeyPassphrase":   conf.TLSClientKeyPassPhrase,
+			"SendCollectionMetrics":    conf.SendCollectionMetrics,
+			"SendCollectionTopMetrics": conf.SendCollectionTopMetrics,
 		},
 	}
 
