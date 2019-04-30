675cfdfacb6cc72a0973dbafcd1d14e0ba8531d6
Peter Hardy <peter@hardy.dropbear.id.au>
2019-04-30 15:12:29 +1000

    BUILDENG-16540: Add an empty repos key.

--- deployments/puppet/manifests/debian_repo.pp
+++ deployments/puppet/manifests/debian_repo.pp
@@ -6,6 +6,7 @@ class signalfx_agent::debian_repo ($repo_base, $package_stage) {
   apt::source { 'signalfx-agent':
     location => "https://${repo_base}/debs/signalfx-agent/${package_stage}",
     release  => '/',
+    repos    => '',
     key      => {
       id     => '91668001288D1C6D2885D651185894C15AE495F6',
       source => "https://${repo_base}/debian.gpg",
