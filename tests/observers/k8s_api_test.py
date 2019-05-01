20c2156a576e820394cd509fb8b1c5a921563630
Jeff Cheng <jcheng@signalfx.com>
2019-05-01 16:04:29 -0400

    Removed get_discovery_rule from observer tests

--- tests/observers/k8s_api_test.py
+++ tests/observers/k8s_api_test.py
@@ -3,7 +3,7 @@ from functools import partial as p
 import pytest
 import yaml
 from tests.helpers.assertions import has_datapoint
-from tests.helpers.kubernetes.utils import add_pod_spec_annotations, get_discovery_rule
+from tests.helpers.kubernetes.utils import add_pod_spec_annotations
 from tests.helpers.util import wait_for
 from tests.paths import TEST_SERVICES_DIR
 
@@ -11,14 +11,13 @@ from tests.paths import TEST_SERVICES_DIR
 @pytest.mark.kubernetes
 def test_k8s_api_observer_basic(k8s_cluster):
     nginx_yaml = TEST_SERVICES_DIR / "nginx/nginx-k8s.yaml"
-    discovery_rule = get_discovery_rule(nginx_yaml, "k8s-api", namespace=k8s_cluster.test_namespace)
     with k8s_cluster.create_resources([nginx_yaml]):
         config = f"""
             observers:
              - type: k8s-api
             monitors:
              - type: collectd/nginx
-               discoveryRule: '{discovery_rule}'
+               discoveryRule: 'discovered_by == "k8s-api" && kubernetes_namespace == "{k8s_cluster.test_namespace}" && port == 80 && container_spec_name == "nginx"'
                url: "http://{{{{.Host}}}}:{{{{.Port}}}}/nginx_status"
         """
         with k8s_cluster.run_agent(config) as agent:
--- tests/observers/k8s_kubelet_test.py
+++ tests/observers/k8s_kubelet_test.py
@@ -2,7 +2,6 @@ from functools import partial as p
 
 import pytest
 from tests.helpers.assertions import has_datapoint
-from tests.helpers.kubernetes.utils import get_discovery_rule
 from tests.helpers.util import wait_for
 from tests.paths import TEST_SERVICES_DIR
 
@@ -10,14 +9,13 @@ from tests.paths import TEST_SERVICES_DIR
 @pytest.mark.kubernetes
 def test_k8s_kubelet_observer_basic(k8s_cluster):
     nginx_yaml = TEST_SERVICES_DIR / "nginx/nginx-k8s.yaml"
-    discovery_rule = get_discovery_rule(nginx_yaml, "k8s-kubelet", namespace=k8s_cluster.test_namespace)
     with k8s_cluster.create_resources([nginx_yaml]):
         config = f"""
             observers:
              - type: k8s-kubelet
             monitors:
              - type: collectd/nginx
-               discoveryRule: '{discovery_rule}'
+               discoveryRule: 'discovered_by == "k8s-kubelet" && kubernetes_namespace == "{k8s_cluster.test_namespace}" && port == 80 && container_spec_name == "nginx"'
                url: "http://{{{{.Host}}}}:{{{{.Port}}}}/nginx_status"
         """
         with k8s_cluster.run_agent(config) as agent:
