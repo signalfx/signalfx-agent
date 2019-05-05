c8af071aeda9e4a0c52c10da1cb8ce9c60c054b2
Bremer Jonathan <bjonathan@signalfx.com>
2019-05-05 16:36:38 -0400

    fix failing test

--- tests/monitors/df/df_test.py
+++ tests/monitors/df/df_test.py
@@ -10,33 +10,30 @@ METADATA = Metadata.from_package("collectd/df")
 
 def test_df_included_metrics():
     expected_metrics = METADATA.included_metrics
-    verify_custom(
-        """
+    agent_config = """
         monitors:
           - type: collectd/df
             hostFSPath: /
-        """,
-        expected_metrics,
-    )
+        """
+    verify_custom(agent_config, expected_metrics)
 
 
 def test_df_extra_metrics():
-    extra_metric_1, extra_metric_2 = "df_complex.reserved", "df_inodes.reserved"
-    expected_metrics = METADATA.included_metrics | {extra_metric_1, extra_metric_2}
-    verify_custom(
-        f"""
+    extra_metric_config_1, extra_metric_config_2 = "df_complex.reserved", "df_inodes.reserved"
+    expected_metrics = METADATA.included_metrics | {extra_metric_config_1, extra_metric_config_2}
+    agent_config = f"""
         monitors:
           - type: collectd/df
             hostFSPath: /
             extraMetrics:
-            - {extra_metric_1}
-            - {extra_metric_2}
-        """,
-        expected_metrics,
-    )
+            - {extra_metric_config_1}
+            - {extra_metric_config_2}
+        """
+    verify_custom(agent_config, expected_metrics)
 
 
-def test_df_percent_prefixed_metrics():
+def test_df_extra_metrics_by_wildcard():
+    percent_prefixed_wildcard_extra_metric_config = "percent_*"
     expected_metrics = METADATA.included_metrics | {
         "percent_bytes.free",
         "percent_bytes.reserved",
@@ -45,61 +42,37 @@ def test_df_percent_prefixed_metrics():
         "percent_inodes.reserved",
         "percent_inodes.used",
     }
-    percent_prefixed_wildcard = "percent_*"
-    verify_custom(
-        f"""
+    agent_config = f"""
         monitors:
           - type: collectd/df
             hostFSPath: /
             extraMetrics:
-            - {percent_prefixed_wildcard}
-        """,
-        expected_metrics,
-    )
+            - {percent_prefixed_wildcard_extra_metric_config}
+        """
+    verify_custom(agent_config, expected_metrics)
 
 
 def test_df_invalid_extra_metric():
     expected_metrics = METADATA.included_metrics
-    invalid_extra_metric = "Y2W8OBrdZZ"
-    verify_custom(
-        f"""
+    invalid_extra_metric_config = "Y2W8OBrdZZ"
+    agent_config = f"""
         monitors:
           - type: collectd/df
             hostFSPath: /
             extraMetrics:
-            - {invalid_extra_metric}
-        """,
-        expected_metrics,
-    )
-
-
-# TODO: Fix failing test
-@pytest.mark.skip(reason="failing due to bug")
-def test_df_blank_extra_metric():
-    expected_metrics = METADATA.included_metrics
-    blank_extra_metric = " "
-    verify_custom(
-        f"""
-        monitors:
-          - type: collectd/df
-            hostFSPath: /
-            extraMetrics:
-            - {blank_extra_metric}
-        """,
-        expected_metrics,
-    )
+            - {invalid_extra_metric_config}
+        """
+    verify_custom(agent_config, expected_metrics)
 
 
 def test_df_extra_metrics_all():
     expected_metrics = METADATA.all_metrics
-    any_wildcard_in_quotes = "'*'"
-    verify_custom(
-        f"""
+    any_wildcard_in_quotes_extra_metric_config = "'*'"
+    agent_config = f"""
         monitors:
           - type: collectd/df
             hostFSPath: /
             extraMetrics:
-            - {any_wildcard_in_quotes}
-        """,
-        expected_metrics,
-    )
+            - {any_wildcard_in_quotes_extra_metric_config}
+        """
+    verify_custom(agent_config, expected_metrics)
