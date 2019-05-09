54f8e4e8cd37ea5f3daa49827f99c758299160ba
Jay Camp <jcamp@signalfx.com>
2019-05-08 23:14:26 -0400

    collectd/memory

---
+++ tests/monitors/collectd_memory/memory_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+from tests.helpers.metadata import Metadata
+from tests.helpers.verify import verify_included_metrics
+
+pytestmark = [pytest.mark.collectd, pytest.mark.memory, pytest.mark.monitor_without_endpoints]
+
+METADATA = Metadata.from_package("collectd/memory")
+
+
+def test_collectd_memory_included():
+    verify_included_metrics(
+        """
+        monitors:
+        - type: collectd/memory
+        """,
+        METADATA,
+    )
+
+
+# Only has included metrics so no test for all metrics.
