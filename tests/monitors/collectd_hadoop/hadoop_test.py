460bb2b68256caea5c52b342e699b63635d3339f
Jay Camp <jcamp@signalfx.com>
2019-05-17 10:51:32 -0400

    Add more collectd/hadoop metadata
    This adds metadata from https://github.com/signalfx/collectd-hadoop. The
    integration tests don't yet cover all metrics.

--- tests/monitors/collectd_hadoop/hadoop_test.py
+++ tests/monitors/collectd_hadoop/hadoop_test.py
@@ -1,24 +1,38 @@
-import string
 from functools import partial as p
 from pathlib import Path
 
 import pytest
-from tests.helpers.agent import Agent
+
 from tests.helpers.assertions import has_datapoint, has_datapoint_with_dim, http_status, tcp_socket_open
+from tests.helpers.metadata import Metadata
 from tests.helpers.util import container_ip, print_lines, run_container, wait_for
+from tests.helpers.verify import run_agent_verify_included_metrics, run_agent_verify
 
 pytestmark = [pytest.mark.collectd, pytest.mark.hadoop, pytest.mark.monitor_with_endpoints]
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
-HADOOP_CONFIG = string.Template(
-    """
-monitors:
-  - type: collectd/hadoop
-    host: $host
-    port: $port
-    verbose: true
-"""
-)
+METADATA = Metadata.from_package("collectd/hadoop")
+
+# TODO: figure out how to get more metrics
+EXCLUDED = {
+    "gauge.hadoop.cluster.metrics.total_mb",
+    "gauge.hadoop.cluster.metrics.total_virtual_cores",
+    "gauge.hadoop.mapreduce.job.elapsedTime",
+    "gauge.hadoop.mapreduce.job.failedMapAttempts",
+    "gauge.hadoop.mapreduce.job.failedReduceAttempts",
+    "gauge.hadoop.mapreduce.job.mapsTotal",
+    "gauge.hadoop.mapreduce.job.successfulMapAttempts",
+    "gauge.hadoop.mapreduce.job.successfulReduceAttempts",
+    "gauge.hadoop.resource.manager.apps.allocatedMB",
+    "gauge.hadoop.resource.manager.apps.allocatedVCores",
+    "gauge.hadoop.resource.manager.apps.clusterUsagePercentage",
+    "gauge.hadoop.resource.manager.apps.memorySeconds",
+    "gauge.hadoop.resource.manager.apps.priority",
+    "gauge.hadoop.resource.manager.apps.progress",
+    "gauge.hadoop.resource.manager.apps.queueUsagePercentage",
+    "gauge.hadoop.resource.manager.apps.runningContainers",
+    "gauge.hadoop.resource.manager.apps.vcoreSeconds",
+}
 
 
 def distribute_hostnames(containers):
@@ -58,7 +72,7 @@ def start_hadoop(hadoop_master, hadoop_worker1):
 
 @pytest.mark.flaky(reruns=2, reruns_delay=5)
 @pytest.mark.parametrize("version", ["2.9.1", "3.0.3"])
-def test_hadoop(version):
+def test_hadoop_included(version):
     """
     Any new versions of hadoop should be manually built, tagged, and pushed to quay.io, i.e.
     docker build \
@@ -67,16 +81,23 @@ def test_hadoop(version):
         <repo_root>/test-services/hadoop
     docker push quay.io/signalfx/hadoop-test:<version>
     """
-    with run_container("quay.io/signalfx/hadoop-test:%s" % version, hostname="hadoop-master") as hadoop_master:
-        with run_container("quay.io/signalfx/hadoop-test:%s" % version, hostname="hadoop-worker1") as hadoop_worker1:
-            host = start_hadoop(hadoop_master, hadoop_worker1)
+    with run_container(
+        "quay.io/signalfx/hadoop-test:%s" % version, hostname="hadoop-master"
+    ) as hadoop_master, run_container(
+        "quay.io/signalfx/hadoop-test:%s" % version, hostname="hadoop-worker1"
+    ) as hadoop_worker1:
+        host = start_hadoop(hadoop_master, hadoop_worker1)
 
-            # start the agent with hadoop config
-            config = HADOOP_CONFIG.substitute(host=host, port=8088)
-            with Agent.run(config) as agent:
-                assert wait_for(
-                    p(has_datapoint_with_dim, agent.fake_services, "plugin", "apache_hadoop")
-                ), "Didn't get hadoop datapoints"
-                assert wait_for(
-                    p(has_datapoint, agent.fake_services, "gauge.hadoop.cluster.metrics.active_nodes", {}, 1)
-                ), "expected 1 hadoop worker node"
+        # start the agent with hadoop config
+        config = f"""
+            monitors:
+              - type: collectd/hadoop
+                host: {host}
+                port: 8088
+                verbose: true
+            """
+        agent = run_agent_verify(config, METADATA.included_metrics - EXCLUDED)
+        assert has_datapoint_with_dim(agent.fake_services, "plugin", "apache_hadoop"), "Didn't get hadoop datapoints"
+        assert has_datapoint(
+            agent.fake_services, "gauge.hadoop.cluster.metrics.active_nodes", {}, 1
+        ), "expected 1 hadoop worker node"
