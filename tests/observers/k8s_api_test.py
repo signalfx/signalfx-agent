d9684683e67aabe20b52f783474b6bcf514893c5
Ben Keith <bkeith@signalfx.com>
2019-05-20 19:41:20 -0400

    Kubernetes API observer annotation enhancements (#730)
    - Allow use of `kubernetes_annotations` in discovery rules
     - Add config to `k8s-api` observer that allows user to specify annotations that refer to additional ports to be discovered.
    
    This is mainly to facilitate using Prometheus pod annotations, but some
    other uses could come out of this.

--- tests/observers/k8s_api_test.py
+++ tests/observers/k8s_api_test.py
@@ -44,3 +44,43 @@ def test_config_from_annotations(k8s_cluster):
         """
         with k8s_cluster.run_agent(config) as agent:
             assert wait_for(p(has_datapoint, agent.fake_services, dimensions={"plugin": "nginx", "source": "myapp"}))
+
+
+@pytest.mark.kubernetes
+def test_k8s_annotations_in_discovery(k8s_cluster):
+    nginx_yaml = TEST_SERVICES_DIR / "nginx/nginx-k8s.yaml"
+    with k8s_cluster.create_resources([nginx_yaml]):
+        config = """
+            observers:
+            - type: k8s-api
+
+            monitors:
+            - type: collectd/nginx
+              discoveryRule: 'Get(kubernetes_annotations, "allowScraping") == "true" && port == 80'
+        """
+        with k8s_cluster.run_agent(config) as agent:
+            assert wait_for(p(has_datapoint, agent.fake_services, dimensions={"plugin": "nginx"}))
+
+
+@pytest.mark.kubernetes
+def test_k8s_annotations_with_alt_ports(k8s_cluster):
+    consul_yaml = list(yaml.safe_load_all((TEST_SERVICES_DIR / "consul/k8s.yaml").read_bytes()))
+    # Remove the declared port so we make sure the additionalPortAnnotations is
+    # what is causing it to be discovered.
+    consul_yaml[0]["spec"]["template"]["spec"]["containers"][0]["ports"] = []
+
+    with k8s_cluster.create_resources([yaml.dump_all(consul_yaml)]):
+        config = """
+            observers:
+            - type: k8s-api
+              additionalPortAnnotations:
+               - prometheus.io/port
+
+            monitors:
+             - type: prometheus-exporter
+               discoveryRule: 'Get(kubernetes_annotations, "prometheus.io/scrape") == "true" && Get(kubernetes_annotations, "prometheus.io/port") == ToString(port)'
+               configEndpointMappings:
+                 metricPath: 'Get(kubernetes_annotations, "prometheus.io/path", "/metrics")'
+        """
+        with k8s_cluster.run_agent(config) as agent:
+            assert wait_for(p(has_datapoint, agent.fake_services, metric_name="consul_runtime_malloc_count"))
