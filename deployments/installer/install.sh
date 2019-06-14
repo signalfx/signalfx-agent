8f73b1ef4b81392ace631b84a45607ca778cf78e
Mark Stumpf <mstumpf@signalfx.com>
2019-06-14 10:17:59 -0400

    Force persmissions on gpg key to be default after downloading (#856)

--- deployments/installer/install.sh
+++ deployments/installer/install.sh
@@ -209,6 +209,7 @@ download_debian_key() {
     echo "Could not get the SignalFx Debian GPG signing key" >&2
     exit 1
   fi
+  chmod 644 /etc/apt/trusted.gpg.d/signalfx.gpg
 }
 
 install_debian_apt_source() {
