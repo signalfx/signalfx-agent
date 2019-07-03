d0916e6bd8887dd2868ae041fe8db41addfd26f4
Tamas Millian <tamas.millian@cloudreach.com>
2019-07-03 12:07:53 +0100

    fix ci failure with whitespace

--- deployments/chef/recipes/yum_repo.rb
+++ deployments/chef/recipes/yum_repo.rb
@@ -11,12 +11,12 @@ if Gem::Requirement.new('>= 12.14').satisfied_by?(Gem::Version.new(Chef::VERSION
 else
   file '/etc/yum.repos.d/signalfx-agent.repo' do
     content <<-EOH
-  [signalfx-agent]
-  name=SignalFx Agent Repository
-  baseurl=#{node['signalfx_agent']['rhel_repo_url']}/#{node['signalfx_agent']['package_stage']}
-  gpgcheck=1
-  gpgkey=#{node['signalfx_agent']['rhel_gpg_key_url']}
-  enabled=1
+[signalfx-agent]
+name=SignalFx Agent Repository
+baseurl=#{node['signalfx_agent']['rhel_repo_url']}/#{node['signalfx_agent']['package_stage']}
+gpgcheck=1
+gpgkey=#{node['signalfx_agent']['rhel_gpg_key_url']}
+enabled=1
     EOH
     mode '0644'
     notifies :run, 'execute[add-rhel-key]', :immediately
