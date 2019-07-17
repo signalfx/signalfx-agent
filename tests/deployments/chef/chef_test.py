1b21c7814da708a134f6acc8746f2ffae476b225
Jeff Cheng <36963516+jchengsfx@users.noreply.github.com>
2019-07-17 10:39:35 -0400

    Add chef test for windows (#878)

--- tests/deployments/chef/chef_test.py
+++ tests/deployments/chef/chef_test.py
@@ -2,19 +2,24 @@
 
 import json
 import os
+import subprocess
+import sys
 import tempfile
 from functools import partial as p
 from pathlib import Path
 
 import pytest
 
+from tests.helpers.agent import ensure_fake_backend
 from tests.helpers.assertions import has_datapoint_with_dim
+from tests.helpers.formatting import print_dp_or_event
 from tests.helpers.util import print_lines, wait_for
 from tests.packaging.common import (
     INIT_SYSTEMD,
     INIT_UPSTART,
     get_agent_logs,
     get_agent_version,
+    get_win_agent_version,
     is_agent_running_as_non_root,
     run_init_system_image,
 )
@@ -24,7 +29,7 @@ pytestmark = [pytest.mark.chef, pytest.mark.deployment]
 
 ATTRIBUTES_JSON = """
 {"signalfx_agent": {
-  "package_stage": "%s",
+  "package_stage": "final",
   "agent_version": null,
   "conf": {
     "signalFxAccessToken": "testing",
@@ -67,9 +72,10 @@ INITIAL_VERSION = os.environ.get("INITIAL_VERSION", "4.7.5")
 UPGRADE_VERSION = os.environ.get("UPGRADE_VERSION", "4.7.6")
 
 
-def run_chef_client(cont, chef_version, agent_version=None, stage="final"):
-    attributes = json.loads(ATTRIBUTES_JSON % stage)
+def run_chef_client(cont, chef_version, agent_version=None, stage=STAGE):
+    attributes = json.loads(ATTRIBUTES_JSON)
     attributes["signalfx_agent"]["agent_version"] = agent_version
+    attributes["signalfx_agent"]["package_stage"] = stage
     print(attributes)
     with tempfile.NamedTemporaryFile(mode="w", dir="/tmp/scratch") as fd:
         fd.write(json.dumps(attributes))
@@ -146,3 +152,64 @@ def test_chef(base_image, init_system, chef_version):
         finally:
             print("Agent log:")
             print_lines(get_agent_logs(cont, init_system))
+
+
+def run_win_chef_client(backend, agent_version=None, stage=STAGE):
+    attributes = json.loads(ATTRIBUTES_JSON)
+    attributes["signalfx_agent"]["agent_version"] = agent_version
+    attributes["signalfx_agent"]["package_stage"] = stage
+    attributes["signalfx_agent"]["conf"]["ingestUrl"] = backend.ingest_url
+    attributes["signalfx_agent"]["conf"]["apiUrl"] = backend.api_url
+    if os.environ.get("AZURE_HTTP_USER_AGENT"):
+        # running in Azure Pipelines; need to override Administrator user/group
+        attributes["signalfx_agent"]["user"] = os.environ.get("USERNAME")
+        attributes["signalfx_agent"]["group"] = os.environ.get("USERNAME")
+    print(attributes)
+    attributes_path = r"C:\chef\cookbooks\attributes.json"
+    with open(attributes_path, "w+") as fd:
+        fd.write(json.dumps(attributes))
+        fd.flush()
+        cmd = CHEF_CMD.format(attributes_path) + " --chef-license accept-silent"
+        print('running "%s" ...' % cmd)
+        proc = subprocess.run(
+            cmd, cwd=r"C:\chef\cookbooks", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+        )
+        output = proc.stdout.decode("utf-8")
+        assert proc.returncode == 0, output
+        print(output)
+    installed_version = get_win_agent_version()
+    if agent_version:
+        assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
+            installed_version,
+            agent_version,
+        )
+    return installed_version
+
+
+@pytest.mark.windows_only
+@pytest.mark.skipif(sys.platform != "win32", reason="only runs on windows")
+def test_chef_on_windows():
+    with ensure_fake_backend() as backend:
+        try:
+            run_win_chef_client(backend, INITIAL_VERSION)
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
+            ), "Datapoints didn't come through"
+            run_win_chef_client(backend, UPGRADE_VERSION)
+            backend.reset_datapoints()
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
+            ), "Datapoints didn't come through"
+            run_win_chef_client(backend, INITIAL_VERSION)
+            backend.reset_datapoints()
+            assert wait_for(
+                p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
+            ), "Datapoints didn't come through"
+        finally:
+            print("\nDatapoints received:")
+            for dp in backend.datapoints:
+                print_dp_or_event(dp)
+            print("\nEvents received:")
+            for event in backend.events:
+                print_dp_or_event(event)
+            print(f"\nDimensions set: {backend.dims}")
