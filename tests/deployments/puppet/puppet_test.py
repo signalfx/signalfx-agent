07ce96e937c40bc046bdcea7608440032326eae2
Jeff Cheng <jcheng@signalfx.com>
2019-07-29 11:32:01 -0400

    Parametrize puppet tests

--- tests/deployments/puppet/puppet_test.py
+++ tests/deployments/puppet/puppet_test.py
@@ -23,7 +23,6 @@ from tests.packaging.common import (
     is_agent_running_as_non_root,
     run_init_system_image,
     run_win_command,
-    running_in_azure_pipelines,
     uninstall_win_agent,
 )
 from tests.paths import REPO_ROOT_DIR
@@ -69,9 +68,16 @@ class { signalfx_agent:
 """
 )
 
+LINUX_PUPPET_RELEASES = os.environ.get("PUPPET_RELEASES", "5,latest").split(",")
+WIN_PUPPET_VERSIONS = os.environ.get("PUPPET_VERSIONS", "5.0.0,latest").split(",")
+
 STAGE = "final"
 INITIAL_VERSION = "4.7.5"
 UPGRADE_VERSION = "4.7.6"
+
+WIN_PUPPET_BIN_DIR = r"C:\Program Files\Puppet Labs\Puppet\bin"
+WIN_PUPPET_MODULE_SRC_DIR = os.path.join(WIN_REPO_ROOT_DIR, "deployments", "puppet")
+WIN_PUPPET_MODULE_DEST_DIR = r"C:\ProgramData\PuppetLabs\code\environments\production\modules\signalfx_agent"
 
 
 def get_config(backend, monitors, version, stage):
@@ -82,7 +88,7 @@ def get_config(backend, monitors, version, stage):
     )
 
 
-def run_puppet_agent(cont, backend, monitors, agent_version=None, stage=STAGE):
+def run_puppet_agent(cont, backend, monitors, agent_version, stage):
     with tempfile.NamedTemporaryFile(mode="w+") as fd:
         fd.write(get_config(backend, monitors, agent_version, stage))
         fd.flush()
@@ -91,12 +97,12 @@ def run_puppet_agent(cont, backend, monitors, agent_version=None, stage=STAGE):
     assert code in (0, 2), output.decode("utf-8")
     print_lines(output)
     installed_version = get_agent_version(cont)
-    if agent_version:
-        assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
-            installed_version,
-            agent_version,
-        )
-    return installed_version
+    assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
+        installed_version,
+        agent_version,
+    )
+    assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
+    backend.reset_datapoints()
 
 
 @pytest.mark.parametrize(
@@ -104,12 +110,18 @@ def run_puppet_agent(cont, backend, monitors, agent_version=None, stage=STAGE):
     [pytest.param(distro, init, marks=pytest.mark.deb) for distro, init in DEB_DISTROS]
     + [pytest.param(distro, init, marks=pytest.mark.rpm) for distro, init in RPM_DISTROS],
 )
-def test_puppet(base_image, init_system):
-    dockerfile = os.path.join(DOCKERFILES_DIR, "Dockerfile.%s" % base_image)
-    with run_init_system_image(base_image, path=REPO_ROOT_DIR, dockerfile=dockerfile, with_socat=False) as [
-        cont,
-        backend,
-    ]:
+@pytest.mark.parametrize("puppet_release", LINUX_PUPPET_RELEASES)
+def test_puppet(base_image, init_system, puppet_release):
+    buildargs = {"PUPPET_RELEASE": ""}
+    if puppet_release != "latest":
+        buildargs = {"PUPPET_RELEASE": puppet_release}
+    opts = {
+        "path": REPO_ROOT_DIR,
+        "dockerfile": DOCKERFILES_DIR / f"Dockerfile.{base_image}",
+        "buildargs": buildargs,
+        "with_socat": False,
+    }
+    with run_init_system_image(base_image, **opts) as [cont, backend]:
         if (base_image, init_system) in DEB_DISTROS:
             code, output = cont.exec_run(f"puppet module install puppetlabs-apt --version {APT_MODULE_VERSION}")
             assert code == 0, output.decode("utf-8")
@@ -117,26 +129,25 @@ def test_puppet(base_image, init_system):
         try:
             monitors = '{ type => "host-metadata" },'
             run_puppet_agent(cont, backend, monitors, INITIAL_VERSION, STAGE)
-            assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # upgrade agent
             run_puppet_agent(cont, backend, monitors, UPGRADE_VERSION, STAGE)
-            backend.reset_datapoints()
-            assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # downgrade agent
             run_puppet_agent(cont, backend, monitors, INITIAL_VERSION, STAGE)
-            backend.reset_datapoints()
-            assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # change agent config
             monitors = '{ type => "internal-metrics" },'
             run_puppet_agent(cont, backend, monitors, INITIAL_VERSION, STAGE)
-            backend.reset_datapoints()
-            assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
             assert wait_for(
                 p(has_datapoint_with_metric_name, backend, "sfxagent.datapoints_sent")
             ), "Didn't get internal metric datapoints"
@@ -145,55 +156,44 @@ def test_puppet(base_image, init_system):
             print_lines(get_agent_logs(cont, init_system))
 
 
-def get_win_puppet_version():
-    return run_win_command("puppet --version").stdout.decode("utf-8").strip()
-
-
-def run_win_puppet_agent(backend, monitors, agent_version=None, stage=STAGE):
-    manifest_path = "agent.pp"
-    with open(manifest_path, "w+") as fd:
-        fd.write(get_config(backend, monitors, agent_version, stage))
-    cmd = f"puppet apply {manifest_path}"
-    run_win_command(cmd, [0, 2])
+def run_win_puppet_agent(backend, monitors, agent_version, stage):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        manifest_path = os.path.join(tmpdir, "agent.pp")
+        config = get_config(backend, monitors, agent_version, stage)
+        print(config)
+        with open(manifest_path, "w+") as fd:
+            fd.write(config)
+        cmd = f"puppet apply {manifest_path}"
+        run_win_command(cmd, [0, 2])
     installed_version = get_win_agent_version()
-    if agent_version:
-        assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
-            installed_version,
-            agent_version,
-        )
-    return installed_version
+    assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
+        installed_version,
+        agent_version,
+    )
+    backend.reset_datapoints()
 
 
-if sys.platform == "win32":
-    if running_in_azure_pipelines() or has_choco():
-        WIN_PUPPET_VERSIONS = os.environ.get("PUPPET_VERSIONS", "latest").split(",")
+def run_win_puppet_setup(puppet_version):
+    assert has_choco(), "choco not installed!"
+    uninstall_win_agent()
+    if puppet_version == "latest":
+        run_win_command(f"choco upgrade -y -f puppet-agent")
     else:
-        # assume system already has puppet installed
-        WIN_PUPPET_VERSIONS = [get_win_puppet_version()]
-else:
-    WIN_PUPPET_VERSIONS = []
-WIN_PUPPET_BIN_DIR = r"C:\Program Files\Puppet Labs\Puppet\bin"
-WIN_PUPPET_MODULE_SRC_DIR = os.path.join(WIN_REPO_ROOT_DIR, "deployments", "puppet")
-WIN_PUPPET_MODULE_DEST_DIR = r"C:\ProgramData\PuppetLabs\code\environments\production\modules\signalfx_agent"
+        run_win_command(f"choco upgrade -y -f puppet-agent --version {puppet_version}")
+    if WIN_PUPPET_BIN_DIR not in os.environ.get("PATH"):
+        os.environ["PATH"] = WIN_PUPPET_BIN_DIR + ";" + os.environ.get("PATH")
+    if os.path.isdir(WIN_PUPPET_MODULE_DEST_DIR):
+        shutil.rmtree(WIN_PUPPET_MODULE_DEST_DIR)
+    shutil.copytree(WIN_PUPPET_MODULE_SRC_DIR, WIN_PUPPET_MODULE_DEST_DIR)
+    run_win_command("puppet module install puppet-archive")
+    run_win_command("puppet module install puppetlabs-powershell")
 
 
 @pytest.mark.windows_only
 @pytest.mark.skipif(sys.platform != "win32", reason="only runs on windows")
 @pytest.mark.parametrize("puppet_version", WIN_PUPPET_VERSIONS)
 def test_puppet_on_windows(puppet_version):
-    uninstall_win_agent()
-    if running_in_azure_pipelines() or has_choco():
-        if puppet_version == "latest":
-            run_win_command(f"choco upgrade -y -f puppet-agent")
-        else:
-            run_win_command(f"choco upgrade -y -f puppet-agent --version {puppet_version}")
-        if WIN_PUPPET_BIN_DIR not in os.environ.get("PATH"):
-            os.environ["PATH"] = WIN_PUPPET_BIN_DIR + ";" + os.environ.get("PATH")
-    if os.path.isdir(WIN_PUPPET_MODULE_DEST_DIR):
-        shutil.rmtree(WIN_PUPPET_MODULE_DEST_DIR)
-    shutil.copytree(WIN_PUPPET_MODULE_SRC_DIR, WIN_PUPPET_MODULE_DEST_DIR)
-    run_win_command("puppet module install puppet-archive")
-    run_win_command("puppet module install puppetlabs-powershell")
+    run_win_puppet_setup(puppet_version)
     with ensure_fake_backend() as backend:
         try:
             monitors = '{ type => "host-metadata" },'
@@ -201,19 +201,22 @@ def test_puppet_on_windows(puppet_version):
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # upgrade agent
             run_win_puppet_agent(backend, monitors, UPGRADE_VERSION, STAGE)
-            backend.reset_datapoints()
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # downgrade agent
             run_win_puppet_agent(backend, monitors, INITIAL_VERSION, STAGE)
-            backend.reset_datapoints()
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
+
+            # change agent config
             monitors = '{ type => "internal-metrics" },'
             run_win_puppet_agent(backend, monitors, INITIAL_VERSION, STAGE)
-            backend.reset_datapoints()
             assert wait_for(
                 p(has_datapoint_with_metric_name, backend, "sfxagent.datapoints_sent")
             ), "Didn't get internal metric datapoints"
