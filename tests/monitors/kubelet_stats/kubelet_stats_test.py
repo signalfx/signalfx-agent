a70c53c34f82c2206306dcd69c28d51e072613f9
Akash Suresh <akash@signalfx.com>
2019-09-23 18:42:55 -0400

    Add test for pod metric

--- tests/monitors/kubelet_stats/kubelet_stats_test.py
+++ tests/monitors/kubelet_stats/kubelet_stats_test.py
@@ -1,14 +1,14 @@
 from functools import partial as p
-
 import pytest
+
 from tests.helpers.assertions import has_datapoint, has_no_datapoint
 from tests.helpers.util import ensure_always, wait_for
 
 pytestmark = [pytest.mark.kubelet_stats, pytest.mark.monitor_without_endpoints]
 
-
 # A reliably present custom metric name
 CUSTOM_METRIC = "container_start_time_seconds"
+CUSTOM_METRIC_POD_METRIC = "pod_ephemeral_storage_capacity_bytes"
 
 
 @pytest.mark.kubernetes
@@ -46,3 +46,18 @@ def test_kubelet_stats_extra(k8s_cluster):
      """
     with k8s_cluster.run_agent(agent_yaml=config) as agent:
         assert wait_for(p(has_datapoint, agent.fake_services, metric_name=CUSTOM_METRIC))
+
+
+@pytest.mark.kubernetes
+def test_kubelet_stats_extra_pod_metric(k8s_cluster):
+    config = f"""
+     monitors:
+      - type: kubelet-stats
+        kubeletAPI:
+          skipVerify: true
+          authType: serviceAccount
+        extraMetrics:
+         - {CUSTOM_METRIC_POD_METRIC}
+     """
+    with k8s_cluster.run_agent(agent_yaml=config) as agent:
+        assert wait_for(p(has_datapoint, agent.fake_services, metric_name=CUSTOM_METRIC_POD_METRIC))
