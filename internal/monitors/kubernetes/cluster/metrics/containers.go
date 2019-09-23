cb6d96f6147366dfea22a50f1c262bc67ab61b84
Akash Suresh <akash@signalfx.com>
2019-09-23 14:42:20 -0400

    Collect CPU/Memory limits and requests

---
+++ internal/monitors/kubernetes/cluster/metrics/containers.go
@@ -0,0 +1,79 @@
+package metrics
+
+import (
+	"strings"
+	"time"
+
+	"github.com/signalfx/golib/datapoint"
+	"github.com/signalfx/signalfx-agent/internal/utils"
+	v1 "k8s.io/api/core/v1"
+)
+
+func stripContainerIDPrefix(id string) string {
+	out := strings.Replace(id, "docker://", "", 1)
+	out = strings.Replace(out, "cri-o://", "", 1)
+
+	return out
+}
+
+func datapointsForContainerStatus(cs v1.ContainerStatus, contDims map[string]string) []*datapoint.Datapoint {
+
+	dps := []*datapoint.Datapoint{
+		datapoint.New(
+			"kubernetes.container_restart_count",
+			contDims,
+			datapoint.NewIntValue(int64(cs.RestartCount)),
+			datapoint.Gauge,
+			time.Now()),
+		datapoint.New(
+			"kubernetes.container_ready",
+			contDims,
+			datapoint.NewIntValue(int64(utils.BoolToInt(cs.Ready))),
+			datapoint.Gauge,
+			time.Now()),
+	}
+
+	return dps
+}
+
+func datapointsForContainerSpec(c v1.Container, contDims map[string]string) []*datapoint.Datapoint {
+
+	dps := []*datapoint.Datapoint{
+		datapoint.New(
+			"kubernetes.container_cpu.request",
+			contDims,
+			datapoint.NewIntValue(c.Resources.Limits.Cpu().Value()),
+			datapoint.Gauge,
+			time.Now()),
+		datapoint.New(
+			"kubernetes.container_memory.request",
+			contDims,
+			datapoint.NewIntValue(c.Resources.Limits.Memory().Value()),
+			datapoint.Gauge,
+			time.Now()),
+		datapoint.New(
+			"kubernetes.container_cpu.limit",
+			contDims,
+			datapoint.NewIntValue(c.Resources.Limits.Cpu().Value()),
+			datapoint.Gauge,
+			time.Now()),
+		datapoint.New(
+			"kubernetes.container_memory.limit",
+			contDims,
+			datapoint.NewIntValue(c.Resources.Limits.Memory().Value()),
+			datapoint.Gauge,
+			time.Now()),
+	}
+
+	return dps
+}
+
+func getAllContainerDimensions(id string, name string, image string, dims map[string]string) map[string]string {
+	out := utils.CloneStringMap(dims)
+
+	out["container_id"] = stripContainerIDPrefix(id)
+	out["container_spec_name"] = name
+	out["container_image"] = image
+
+	return out
+}
