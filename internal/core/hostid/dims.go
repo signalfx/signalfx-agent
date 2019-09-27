04c1089bb7b8e095dd90c2ada369761adbf0c676
Ben Keith <bkeith@signalfx.com>
2019-09-27 13:07:51 -0400

    Replace kubernetes_node with kubernetes_node_uid (#941)
    This changes the global dimension and requires looking up the node UID
    from the K8s API server.  The lookup is triggered if the envvar MY_NODE_NAME
    is present in the agent.  The agent will refuse to start up if the node uid
    cannot be resolved and MY_NODE_NAME is present.
    
    Also removes old machineID and node name config options, as they are all replaced
    with node uid.

--- internal/core/hostid/dims.go
+++ internal/core/hostid/dims.go
@@ -4,24 +4,25 @@ import (
 	"os"
 	"sync"
 
+	"github.com/signalfx/signalfx-agent/internal/core/config"
 	log "github.com/sirupsen/logrus"
 )
 
 // Dimensions returns a map of host-specific dimensions that are derived from
 // the environment.
-func Dimensions(sendMachineID bool, hostname string, useFullyQualifiedHost *bool) map[string]string {
+func Dimensions(conf *config.Config) map[string]string {
 	log.Info("Fetching host id dimensions")
 
 	var g dimGatherer
 
 	g.GatherDim("host", func() string {
-		if hostname != "" {
-			return hostname
+		if conf.Hostname != "" {
+			return conf.Hostname
 		}
 		// Using the FQDN needs to default to true but the defaults lib that we
 		// use can't distinguish between false and unspecified, so figure out
 		// if the user specified it explicitly as false with this logic.
-		return getHostname(useFullyQualifiedHost == nil || *useFullyQualifiedHost)
+		return getHostname(conf.UseFullyQualifiedHost == nil || *conf.UseFullyQualifiedHost)
 	})
 
 	// The envvar exists primarily for testing but could be useful otherwise.
@@ -29,15 +30,9 @@ func Dimensions(sendMachineID bool, hostname string, useFullyQualifiedHost *bool
 	if os.Getenv("SKIP_PLATFORM_HOST_DIMS") != "yes" {
 		g.GatherDim("AWSUniqueId", AWSUniqueID)
 		g.GatherDim("gcp_id", GoogleComputeID)
-		if sendMachineID {
-			g.GatherDim("machine_id", MachineID)
-		} else {
-			// If not running on k8s, this will be blank and thus omitted.  It is
-			// only sent as an alternative to machine id because k8s node labels
-			// are synced as properties to this instead of machine_id when
-			// machine_id isn't available.
-			g.GatherDim("kubernetes_node", KubernetesNodeName)
-		}
+		g.GatherDim("kubernetes_node_uid", func() string {
+			return KubernetesNodeUID(conf.Monitors)
+		})
 		g.GatherDim("azure_resource_id", AzureUniqueID)
 	}
 
