74492a573d0318f2415a1cb3199b787858a09ec0
Jeff Cheng <jcheng@signalfx.com>
2019-10-14 11:37:18 -0400

    Clean up circleci config

---
+++ .circleci/scripts/setup-installer-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ "$CIRCLE_BRANCH" != "master" ]; then
+    if ! scripts/changes-include-dir deployments/installer tests/packaging/installer_test.py tests/packaging/common.py tests/packaging/images ${BASH_SOURCE[0]}; then
+        echo "Installer code has not changed, skipping tests!"
+        touch ~/.skip
+        exit 0
+    fi
+fi
