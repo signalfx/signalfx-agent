63d12d608d4babb1c0eb468fff7d85a7c2bd4513
Seon Kim <skim@signalfx.com>
2019-10-21 16:20:36 -0400

    Add monitor for kube-proxy

--- internal/monitors/kubernetes/modules.go
+++ internal/monitors/kubernetes/modules.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/apiserver"
 	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/cluster"
 	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/events"
+	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/proxy"
 	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/scheduler"
 	_ "github.com/signalfx/signalfx-agent/internal/monitors/kubernetes/volumes"
 )
