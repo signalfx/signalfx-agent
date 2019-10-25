8a9524a2a75c5ede2be09657dc1c3ec243373037
Jeff Cheng <jcheng@signalfx.com>
2019-10-25 17:20:05 -0400

    Move package builds to build job

--- .circleci/scripts/setup-package-tests.sh
+++ .circleci/scripts/setup-package-tests.sh
@@ -9,8 +9,15 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
         exit 0
     fi
 fi
-export PULL_CACHE=yes
-make ${PACKAGE_TYPE}-test-package
+
+if [ "$PACKAGE_TYPE" = "deb" ]; then
+    mkdir -p packaging/deb/output/
+    mv /tmp/workspace/signalfx-agent*.deb packaging/deb/output/
+elif [ "$PACKAGE_TYPE" = "rpm" ]; then
+    mkdir -p packaging/rpm/output/x86_64/
+    mv /tmp/workspace/signalfx-agent*.rpm packaging/rpm/output/x86_64/
+fi
+
 # Run non-upgrade tests on node 0, and upgrade tests on node 1
 if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
     echo "export MARKERS='$PACKAGE_TYPE and not upgrade'" >> $BASH_ENV
