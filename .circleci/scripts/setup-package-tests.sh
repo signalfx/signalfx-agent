05177d374ff19602c502961cadb6bd92659d1021
Jeff Cheng <36963516+jchengsfx@users.noreply.github.com>
2019-10-25 13:57:10 -0400

    Add crio support to circleci k8s tests (#1047)
    * Add crio support to circleci k8s tests
    
    * Copy minikube deps to system path for root
    
    * Delete jobs for k8s versions older than 1.12
    
    * Update default minikube version to v1.4.0
    
    * Install cni and cni-plugins
    
    * Helm test updates
    
    * Only test for network metrics for docker runtime
    
    * Get container runtimes during cluster init
    
    * Clean up circleci config
    
    * Update build job to make bundle

---
+++ .circleci/scripts/setup-package-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ "$CIRCLE_BRANCH" != "master" ]; then
+    if ! scripts/changes-include-dir Dockerfile packaging tests/packaging scripts/patch-interpreter scripts/patch-rpath ${BASH_SOURCE[0]}; then
+        echo "packaging code has not changed, skipping tests!"
+        touch ~/.skip
+        exit 0
+    fi
+fi
+export PULL_CACHE=yes
+make ${PACKAGE_TYPE}-test-package
+# Run non-upgrade tests on node 0, and upgrade tests on node 1
+if [ $CIRCLE_NODE_INDEX -eq 0 ]; then
+    echo "export MARKERS='$PACKAGE_TYPE and not upgrade'" >> $BASH_ENV
+else
+    echo "export MARKERS='$PACKAGE_TYPE and upgrade'" >> $BASH_ENV
+fi
