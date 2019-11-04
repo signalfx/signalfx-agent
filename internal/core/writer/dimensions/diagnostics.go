c2c2b47e86ef4038221022cfee1e3d60a0afcdfb
Ben Keith <benkeith@splunk.com>
2019-11-04 09:44:39 -0500

    Add a few more internal metrics around dimension updates

--- internal/core/writer/dimensions/diagnostics.go
+++ internal/core/writer/dimensions/diagnostics.go
@@ -17,7 +17,10 @@ func (dc *DimensionClient) InternalMetrics() []*datapoint.Datapoint {
 		sfxclient.Gauge("sfxagent.dim_request_senders", nil, atomic.LoadInt64(&dc.requestSender.RunningWorkers)),
 		sfxclient.Gauge("sfxagent.dim_updates_currently_delayed", nil, atomic.LoadInt64(&dc.DimensionsCurrentlyDelayed)),
 		sfxclient.CumulativeP("sfxagent.dim_updates_dropped", nil, &dc.TotalDimensionsDropped),
+		sfxclient.CumulativeP("sfxagent.dim_updates_invalid", nil, &dc.TotalInvalidDimensions),
 		sfxclient.CumulativeP("sfxagent.dim_updates_flappy_total", nil, &dc.TotalFlappyUpdates),
+		sfxclient.CumulativeP("sfxagent.dim_updates_duplicates", nil, &dc.TotalDuplicates),
+		// All 4xx HTTP responses that are not retried except 404 (which is retried)
 		sfxclient.CumulativeP("sfxagent.dim_updates_client_errors", nil, &dc.TotalClientError4xxResponses),
 		sfxclient.CumulativeP("sfxagent.dim_updates_retries", nil, &dc.TotalRetriedUpdates),
 	}
