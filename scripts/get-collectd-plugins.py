bd7a9bd01adaa10f5d93fe941a807b0a0760a9c7
Ben Keith <benkeith@splunk.com>
2019-12-12 11:08:37 -0500

    Upgrade to Python 3.8 (#1103)
    Python 2 is EOL as of Jan 1, 2019 so we should not support it anymore.
    Everything that uses Python (collectd-python, the SignalFx python
    subprocess runner) will use Python 3, and it will be the Python runtime
    bundled with the agent.  The integration test suite was already using
    Python 3.
    
    Also upgraded all collectd python monitors to support Python 3 and
    pointed to the new tags in collectd-plugins.yaml.
    
    Removed `collectd/haproxy` and `collectd/docker` monitors since they are
    deprecated and have equivalent Go-based monitors.  I left
    `collectd/elasticsearch` for now since it is used heavily internally,
    but it can probably be removed in the next major release.

--- scripts/get-collectd-plugins.py
+++ scripts/get-collectd-plugins.py
@@ -1,52 +1,54 @@
-#!/bin/python
+#!/usr/bin/env python3
 
 import contextlib
 import os
 import shutil
-from six.moves import urllib
 import subprocess
 import sys
 import tarfile
-import urllib2
+import urllib.request
+
 import yaml
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-target_dir = os.path.join("/", "usr","share","collectd") if len(sys.argv) < 2 else sys.argv[1]
-python_executable = sys.executable if sys.executable else 'python'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+TARGET_DIR = os.path.join("/", "usr", "share", "collectd") if len(sys.argv) < 2 else sys.argv[1]
+PYTHON_EXECUTABLE = sys.executable if sys.executable else "python"
 
-with file(os.path.join(script_dir, "..", "collectd-plugins.yaml"), 'r') as f:
-    plugins = yaml.safe_load(f)
+with open(os.path.join(SCRIPT_DIR, "..", "collectd-plugins.yaml"), "r") as f:
+    PLUGINS = yaml.safe_load(f)
 
-for p in plugins:
-    plugin_name = p.get('name')
-    version = p.get('version')
-    repo = p.get('repo')
+for p in PLUGINS:
+    plugin_name = p.get("name")
+    version = p.get("version")
+    repo = p.get("repo")
     url = "https://github.com/{repo}/archive/{version}.tar.gz".format(repo=repo, version=version)
 
-    print("""Bundling...
-plugin:  {p} 
-version: {v} 
+    print(
+        """Bundling...
+plugin:  {p}
+version: {v}
 repo:    {r}
-url:     {u}""".format(p=plugin_name, v=version, r=repo, u=url))
+url:     {u}""".format(
+            p=plugin_name, v=version, r=repo, u=url
+        )
+    )
 
     with contextlib.closing(urllib.request.urlopen(url)) as stream:
-        with tarfile.open(fileobj=stream, mode='r|gz') as tar_archive:
-            tar_archive.extractall(target_dir)
-            plugin_dir = os.path.join(target_dir, plugin_name)
-            os.rename(os.path.join(target_dir, tar_archive.getnames()[0]),
-                      plugin_dir)
+        with tarfile.open(fileobj=stream, mode="r|gz") as tar_archive:
+            tar_archive.extractall(TARGET_DIR)
+            plugin_dir = os.path.join(TARGET_DIR, plugin_name)
+            os.rename(os.path.join(TARGET_DIR, tar_archive.getnames()[0]), plugin_dir)
 
     # install pip deps
-    for package in p.get('pip_packages', []):
-        subprocess.check_call([python_executable, '-m', 'pip', 'install', '-qq', '--no-warn-script-location', package])
+    for package in p.get("pip_packages", []):
+        subprocess.check_call([PYTHON_EXECUTABLE, "-m", "pip", "install", "-qq", "--no-warn-script-location", package])
 
     requirements_file = os.path.join(plugin_dir, "requirements.txt")
     if os.path.isfile(requirements_file):
-        subprocess.check_call([python_executable, '-m', 'pip', 'install', '-qq', '--no-warn-script-location', '-r', requirements_file])
-
+        subprocess.check_call(
+            [PYTHON_EXECUTABLE, "-m", "pip", "install", "-qq", "--no-warn-script-location", "-r", requirements_file]
+        )
 
     # remove unecessary things
-    for elem in p.get('can_remove', []):
-        def rmtree_error_handler(*args):
-            print("unable to remove element {0}".format(elem))
-        shutil.rmtree(os.path.join(plugin_dir, elem), onerror=rmtree_error_handler)
+    for elem in p.get("can_remove", []):
+        shutil.rmtree(os.path.join(plugin_dir, elem))
