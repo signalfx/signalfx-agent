e2a2a07cb5861472062068943f2625297d22f6ef
Ben Keith <benkeith@splunk.com>
2019-12-19 16:19:14 -0500

    Allow setting event endpoint separate from datapoints and traces (#1124)
    This might be useful for a setup in which both the agent and OpenTelemetry
    Collector are used together.

---
+++ pkg/core/writer/writer_test.go
@@ -0,0 +1,39 @@
+package writer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/signalfx/signalfx-agent/pkg/core/config"
+	"github.com/stretchr/testify/require"
+)
+
+var essentialWriterConfig = config.WriterConfig{
+	PropertiesHistorySize:               100,
+	PropertiesSendDelaySeconds:          1,
+	TraceExportFormat:                   "zipkin",
+	TraceHostCorrelationMetricsInterval: 1 * time.Second,
+	StaleServiceTimeout:                 1 * time.Second,
+	EventSendIntervalSeconds:            1,
+}
+
+func TestWriterSetup(t *testing.T) {
+	t.Run("Overrides event URL", func(t *testing.T) {
+		t.Parallel()
+		conf := essentialWriterConfig
+		conf.EventEndpointURL = "http://example.com/v2/event"
+		writer, err := New(&conf, nil, nil, nil, nil, nil)
+
+		require.Nil(t, err)
+		require.Equal(t, "http://example.com/v2/event", writer.client.EventEndpoint)
+	})
+
+	t.Run("Sets default event URL", func(t *testing.T) {
+		t.Parallel()
+		conf := essentialWriterConfig
+		conf.IngestURL = "http://example.com"
+		writer, err := New(&conf, nil, nil, nil, nil, nil)
+		require.Nil(t, err)
+		require.Equal(t, "http://example.com/v2/event", writer.client.EventEndpoint)
+	})
+}
