dc505961febb740356854431f2b98b1ebfbf93a0
Jeff Cheng <jcheng@signalfx.com>
2020-01-28 16:34:15 -0500

    Update tests for artifactory and release stage
    * The deployment tests will also install the old keys and verify that
    they are deleted after the initial run.
    * All deployment tests have been updated to verify upgrade/downgrade
    and config changes.

--- tests/deployments/salt/salt_test.py
+++ tests/deployments/salt/salt_test.py
@@ -10,8 +10,10 @@ from tests.helpers.assertions import has_datapoint_with_dim, has_datapoint_with_
 from tests.helpers.util import print_lines, wait_for, copy_file_into_container
 from tests.packaging.common import (
     INIT_SYSTEMD,
+    assert_old_key_removed,
     get_agent_logs,
     get_agent_version,
+    import_old_key,
     is_agent_running_as_non_root,
     run_init_system_image,
 )
@@ -46,18 +48,19 @@ signalfx-agent:
 
 PILLAR_PATH = "/srv/pillar/signalfx-agent.sls"
 SALT_CMD = "salt-call --local state.apply"
-STAGE = os.environ.get("STAGE", "final")
-INITIAL_VERSION = os.environ.get("INITIAL_VERSION", "4.7.5-1")
-UPGRADE_VERSION = os.environ.get("UPGRADE_VERSION", "4.7.6-1")
+STAGE = os.environ.get("STAGE", "release")
+INITIAL_VERSION = os.environ.get("INITIAL_VERSION", "4.7.5")
+UPGRADE_VERSION = os.environ.get("UPGRADE_VERSION", "4.7.6")
 
 
 def get_config(backend, agent_version, monitors, stage):
     config_yaml = yaml.safe_load(CONFIG)
     config_yaml["signalfx-agent"]["package_stage"] = stage
-    config_yaml["signalfx-agent"]["version"] = agent_version
+    config_yaml["signalfx-agent"]["version"] = agent_version + "-1"
     config_yaml["signalfx-agent"]["conf"]["ingestUrl"] = backend.ingest_url
     config_yaml["signalfx-agent"]["conf"]["apiUrl"] = backend.api_url
     config_yaml["signalfx-agent"]["conf"]["monitors"] = monitors
+
     return yaml.dump(config_yaml)
 
 
@@ -73,7 +76,6 @@ def run_salt(cont, backend, agent_version, monitors, stage):
     print_lines(output)
     assert code == 0, f"'{SALT_CMD}' failed"
 
-    agent_version = agent_version.rstrip("-1").rstrip("~1")
     installed_version = get_agent_version(cont)
     assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
         installed_version,
@@ -89,28 +91,38 @@ def run_salt(cont, backend, agent_version, monitors, stage):
     + [pytest.param(distro, init, marks=pytest.mark.rpm) for distro, init in RPM_DISTROS],
 )
 def test_salt(base_image, init_system):
+    if (base_image, init_system) in DEB_DISTROS:
+        distro_type = "deb"
+    else:
+        distro_type = "rpm"
+
     opts = {"path": REPO_ROOT_DIR, "dockerfile": DOCKERFILES_DIR / f"Dockerfile.{base_image}", "with_socat": False}
     with run_init_system_image(base_image, **opts) as [cont, backend]:
         try:
+            import_old_key(cont, distro_type)
+
             monitors = [{"type": "host-metadata"}]
             run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
 
-            # upgrade agent
-            run_salt(cont, backend, UPGRADE_VERSION, monitors, STAGE)
-            backend.reset_datapoints()
-            assert wait_for(
-                p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
-            ), "Datapoints didn't come through"
+            assert_old_key_removed(cont, distro_type)
 
-            # downgrade agent
-            run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
-            backend.reset_datapoints()
-            assert wait_for(
-                p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
-            ), "Datapoints didn't come through"
+            if UPGRADE_VERSION:
+                # upgrade agent
+                run_salt(cont, backend, UPGRADE_VERSION, monitors, STAGE)
+                backend.reset_datapoints()
+                assert wait_for(
+                    p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
+                ), "Datapoints didn't come through"
+
+                # downgrade agent
+                run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
+                backend.reset_datapoints()
+                assert wait_for(
+                    p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
+                ), "Datapoints didn't come through"
 
             # change agent config
             monitors = [{"type": "internal-metrics"}]
