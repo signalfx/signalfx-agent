62c6cdaaea6481ff6306691b2689a2e98866206e
Ben Keith <benkeith@splunk.com>
2020-01-30 19:03:39 -0500

    Remove monitor-level metricsToExclude (#1163)
    This simplies the monitor configuration and removes a deprecated feature.
    
    The global (top-level) metricsToExclude config is still there since it is probably more
    commonly used and still has some value.
    
    This will break anybody using this config but if you are only doing blacklist filtering
    you can just switch metricsToExclude to datapointsToExclude without any additional
    changes.  If doing whitelist filtering, you will have to use an array and make the first
    filter item a * to match everything first.

--- tests/basic/filter_test.py
+++ tests/basic/filter_test.py
@@ -175,7 +175,7 @@ def test_monitor_filter():
 monitors:
   - type: filesystems
   - type: memory
-    metricsToExclude:
+    datapointsToExclude:
      - metricName: memory.used
   - type: collectd/uptime
 """
