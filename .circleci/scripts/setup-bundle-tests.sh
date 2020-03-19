f42c28e775af800bcd6f5ea8ba11248d1e42fcdc
Jeff Cheng <jcheng@signalfx.com>
2020-03-19 15:33:21 -0400

    Update circleci test jobs to check for pip requirement changes

--- .circleci/scripts/setup-bundle-tests.sh
+++ .circleci/scripts/setup-bundle-tests.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 if [ "$CIRCLE_BRANCH" != "master" ]; then
-    if ! scripts/changes-include-dir Dockerfile packaging tests/packaging scripts/patch-interpreter scripts/patch-rpath scripts/build ${BASH_SOURCE[0]}; then
+    if ! scripts/changes-include-dir Dockerfile packaging tests/packaging scripts/patch-interpreter scripts/patch-rpath scripts/build tests/requirements.txt ${BASH_SOURCE[0]}; then
         echo "packaging code has not changed, skipping tests!"
         touch ~/.skip
         exit 0
--- .circleci/scripts/setup-installer-tests.sh
+++ .circleci/scripts/setup-installer-tests.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 if [ "$CIRCLE_BRANCH" != "master" ]; then
-    if ! scripts/changes-include-dir deployments/installer tests/packaging/installer_test.py tests/packaging/common.py tests/packaging/images ${BASH_SOURCE[0]}; then
+    if ! scripts/changes-include-dir deployments/installer tests/packaging/installer_test.py tests/packaging/common.py tests/packaging/images tests/requirements.txt ${BASH_SOURCE[0]}; then
         echo "Installer code has not changed, skipping tests!"
         touch ~/.skip
         exit 0
