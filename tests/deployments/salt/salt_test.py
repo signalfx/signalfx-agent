9ea994add48763b21a6c7f4e4a42dce4f3eead2d
Jeff Cheng <jcheng@signalfx.com>
2020-04-09 18:18:15 -0400

    Update salt tests for service owner

--- tests/deployments/salt/salt_test.py
+++ tests/deployments/salt/salt_test.py
@@ -10,12 +10,14 @@ from tests.helpers.assertions import has_datapoint_with_dim, has_datapoint_with_
 from tests.helpers.util import print_lines, wait_for, copy_file_into_container
 from tests.packaging.common import (
     INIT_SYSTEMD,
+    INIT_UPSTART,
     assert_old_key_removed,
     get_agent_logs,
     get_agent_version,
     import_old_key,
     is_agent_running_as_non_root,
     run_init_system_image,
+    verify_override_files,
 )
 from tests.paths import REPO_ROOT_DIR
 
@@ -30,12 +32,19 @@ DEB_DISTROS = [
     ("ubuntu1804", INIT_SYSTEMD),
 ]
 
-RPM_DISTROS = [("amazonlinux2", INIT_SYSTEMD), ("centos7", INIT_SYSTEMD), ("centos8", INIT_SYSTEMD)]
+RPM_DISTROS = [
+    ("amazonlinux1", INIT_UPSTART),
+    ("amazonlinux2", INIT_SYSTEMD),
+    ("centos7", INIT_SYSTEMD),
+    ("centos8", INIT_SYSTEMD),
+]
 
 CONFIG = """
 signalfx-agent:
   package_stage: null
   version: null
+  service_user: null
+  service_group: null
   conf:
     signalFxAccessToken: 'testing123'
     ingestUrl: null
@@ -50,13 +59,18 @@ PILLAR_PATH = "/srv/pillar/signalfx-agent.sls"
 SALT_CMD = "salt-call --local state.apply"
 STAGE = os.environ.get("STAGE", "release")
 INITIAL_VERSION = os.environ.get("INITIAL_VERSION", "4.7.5")
-UPGRADE_VERSION = os.environ.get("UPGRADE_VERSION", "5.0.0")
+UPGRADE_VERSION = os.environ.get("UPGRADE_VERSION", "5.1.0")
 
 
-def get_config(backend, agent_version, monitors, stage):
+def get_config(backend, agent_version, monitors, stage, user, group=None):
+    if group is None:
+        group = user
+
     config_yaml = yaml.safe_load(CONFIG)
     config_yaml["signalfx-agent"]["package_stage"] = stage
     config_yaml["signalfx-agent"]["version"] = agent_version + "-1"
+    config_yaml["signalfx-agent"]["service_user"] = user
+    config_yaml["signalfx-agent"]["service_group"] = group
     config_yaml["signalfx-agent"]["conf"]["ingestUrl"] = backend.ingest_url
     config_yaml["signalfx-agent"]["conf"]["apiUrl"] = backend.api_url
     config_yaml["signalfx-agent"]["conf"]["monitors"] = monitors
@@ -64,9 +78,9 @@ def get_config(backend, agent_version, monitors, stage):
     return yaml.dump(config_yaml)
 
 
-def run_salt(cont, backend, agent_version, monitors, stage):
+def run_salt(cont, init_system, backend, agent_version, monitors, stage, user="signalfx-agent"):
     with tempfile.NamedTemporaryFile(mode="w+") as fd:
-        config_yaml = get_config(backend, agent_version, monitors, stage)
+        config_yaml = get_config(backend, agent_version, monitors, stage, user)
         print(config_yaml)
         fd.write(config_yaml)
         fd.flush()
@@ -76,13 +90,15 @@ def run_salt(cont, backend, agent_version, monitors, stage):
     print_lines(output)
     assert code == 0, f"'{SALT_CMD}' failed"
 
+    verify_override_files(cont, init_system, user)
+
     installed_version = get_agent_version(cont)
     assert installed_version == agent_version, "installed agent version is '%s', expected '%s'" % (
         installed_version,
         agent_version,
     )
 
-    assert is_agent_running_as_non_root(cont), "Agent is not running as non-root user"
+    assert is_agent_running_as_non_root(cont, user), f"Agent is not running as {user} user"
 
 
 @pytest.mark.parametrize(
@@ -102,7 +118,7 @@ def test_salt(base_image, init_system):
             import_old_key(cont, distro_type)
 
             monitors = [{"type": "host-metadata"}]
-            run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
+            run_salt(cont, init_system, backend, INITIAL_VERSION, monitors, STAGE)
             assert wait_for(
                 p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
             ), "Datapoints didn't come through"
@@ -111,14 +127,17 @@ def test_salt(base_image, init_system):
 
             if UPGRADE_VERSION:
                 # upgrade agent
-                run_salt(cont, backend, UPGRADE_VERSION, monitors, STAGE)
+                user = "signalfx-agent"
+                if tuple(UPGRADE_VERSION.split(".")) >= ("5", "1", "0"):
+                    user = "test-user"
+                run_salt(cont, init_system, backend, UPGRADE_VERSION, monitors, STAGE, user)
                 backend.reset_datapoints()
                 assert wait_for(
                     p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
                 ), "Datapoints didn't come through"
 
                 # downgrade agent
-                run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
+                run_salt(cont, init_system, backend, INITIAL_VERSION, monitors, STAGE)
                 backend.reset_datapoints()
                 assert wait_for(
                     p(has_datapoint_with_dim, backend, "plugin", "host-metadata")
@@ -126,7 +145,7 @@ def test_salt(base_image, init_system):
 
             # change agent config
             monitors = [{"type": "internal-metrics"}]
-            run_salt(cont, backend, INITIAL_VERSION, monitors, STAGE)
+            run_salt(cont, init_system, backend, INITIAL_VERSION, monitors, STAGE)
             backend.reset_datapoints()
             assert wait_for(
                 p(has_datapoint_with_metric_name, backend, "sfxagent.datapoints_sent")
