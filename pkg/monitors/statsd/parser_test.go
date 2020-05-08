2d291e68449693a360ff790157c0d095756f3a1a
Owais Lone <owais@users.noreply.github.com>
2020-05-09 01:02:48 +0530

    Add support for dogstatsd tags to statsd receiver (#1311)
    This commit add support for dogstatsd metric tags to the statsd
    receiver. The change is completely backward compatible and does not
    affect statsd senders in any way. This makes it possible for the smart
    agent to receive runtime metrics from a number of SingalFx
    instrumentation libraries.

--- pkg/monitors/statsd/parser_test.go
+++ pkg/monitors/statsd/parser_test.go
@@ -6,6 +6,98 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestParseMetrics(t *testing.T) {
+	cases := []struct {
+		name       string
+		raw        string
+		converters []*converter
+		parsed     statsDMetric
+	}{
+		{
+			"tags",
+			"runtime.node.heap.size.by.space:430080|g|#service:svc1,runtime-id:dcd3,space:code_space",
+			nil,
+			statsDMetric{
+				rawMetricName: "runtime.node.heap.size.by.space",
+				metricName:    "runtime.node.heap.size.by.space",
+				metricType:    "g",
+				value:         430080,
+				dimensions: map[string]string{
+					"service":    "svc1",
+					"runtime-id": "dcd3",
+					"space":      "code_space",
+				},
+			},
+		},
+		{
+			"no-tags",
+			"runtime.node.heap.size.by.space:43|g",
+			nil,
+			statsDMetric{
+				rawMetricName: "runtime.node.heap.size.by.space",
+				metricName:    "runtime.node.heap.size.by.space",
+				metricType:    "g",
+				value:         43,
+				dimensions:    nil,
+			},
+		},
+		{
+			"tags-with-converters",
+			"cluster.cds_egress_ecommerce-demo-mesh_gateway-vn_tcp_8080.update_success:100|g|#svc:svc2",
+			[]*converter{
+				{
+					pattern: parseFields("cluster.cds_{traffic}_{mesh}_{service}-vn_{}.{action}"),
+					metric:  parseFields("{traffic}.{action}"),
+				},
+			},
+			statsDMetric{
+				rawMetricName: "cluster.cds_egress_ecommerce-demo-mesh_gateway-vn_tcp_8080.update_success",
+				metricName:    "egress.update_success",
+				metricType:    "g",
+				value:         100,
+				dimensions: map[string]string{
+					"svc":     "svc2",
+					"traffic": "egress",
+					"mesh":    "ecommerce-demo-mesh",
+					"service": "gateway",
+					"action":  "update_success",
+				},
+			},
+		},
+		{
+			"converters-without-tags",
+			"cluster.cds_egress_ecommerce-demo-mesh_gateway-vn_tcp_8080.update_success:100|g",
+			[]*converter{
+				{
+					pattern: parseFields("cluster.cds_{traffic}_{mesh}_{service}-vn_{}.{action}"),
+					metric:  parseFields("{traffic}.{action}"),
+				},
+			},
+			statsDMetric{
+				rawMetricName: "cluster.cds_egress_ecommerce-demo-mesh_gateway-vn_tcp_8080.update_success",
+				metricName:    "egress.update_success",
+				metricType:    "g",
+				value:         100,
+				dimensions: map[string]string{
+					"traffic": "egress",
+					"mesh":    "ecommerce-demo-mesh",
+					"service": "gateway",
+					"action":  "update_success",
+				},
+			},
+		},
+	}
+
+	for i := range cases {
+		tt := cases[i]
+		t.Run(tt.name, func(t *testing.T) {
+			sm := parseMetrics([]string{tt.raw}, tt.converters, "")
+			require.Equal(t, 1, len(sm))
+			require.Equal(t, tt.parsed, *sm[0])
+		})
+	}
+}
+
 func TestParseFields(t *testing.T) {
 	cases := []struct {
 		pattern        string
