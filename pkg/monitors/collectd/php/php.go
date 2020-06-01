b22a1e89a4cdce3f1757c34d1db0dda996084716
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-06-01 22:48:20 +0200

    make php fpm more consistent with other monitors

--- pkg/monitors/collectd/php/php.go
+++ pkg/monitors/collectd/php/php.go
@@ -5,6 +5,8 @@ package php
 //go:generate ../../../../scripts/collectd-template-to-go php.tmpl
 
 import (
+	"fmt"
+
 	"github.com/signalfx/signalfx-agent/pkg/core/config"
 	"github.com/signalfx/signalfx-agent/pkg/monitors"
 	"github.com/signalfx/signalfx-agent/pkg/monitors/collectd"
@@ -22,15 +24,20 @@ func init() {
 type Config struct {
 	config.MonitorConfig `yaml:",inline" acceptsEndpoints:"true"`
 	// The hostname of the webserver (i.e. `127.0.0.1`)
-	Host string `yaml:"host" validate:"required"`
+	Host string `yaml:"host"`
 	// The port number of the webserver (i.e. `80`)
-	Port uint16 `yaml:"port" validate:"required"`
+	Port uint16 `yaml:"port"`
+	// If true, the monitor will connect to Supervisor via HTTPS instead of
+	// HTTP.
+	UseHTTPS bool `yaml:"useHTTPS" default:"false"`
+	// The URL path to use for the scrape URL for Supervisor.
+	Path string `yaml:"path" default:"/status"`
+	// The URL, either a final URL or a Go template that will be populated with
+	// the `host`, `port` and `path` values.
+	URL string `yaml:"url"`
 	// This will be sent as the `plugin_instance` dimension and can be any name
 	// you like.
 	Name string `yaml:"name"`
-	// The URL, either a final URL or a Go template that will be populated with
-	// the `host` and `port` values.
-	URL string `yaml:"url" default:"http://{{.host}}:{{.port}}/status?json"`
 }
 
 // Monitor is the main type that represents the monitor
@@ -38,7 +45,18 @@ type Monitor struct {
 	collectd.MonitorCore
 }
 
+// Scheme definition
+func (c *Config) Scheme() string {
+	if c.UseHTTPS {
+		return "https"
+	}
+	return "http"
+}
+
 // Configure configures and runs the plugin in collectd
 func (am *Monitor) Configure(conf *Config) error {
+	if conf.URL == "" {
+		conf.URL = fmt.Sprintf("%s://%s:%d%s?json", conf.Scheme(), conf.Host, conf.Port, conf.Path)
+	}
 	return am.SetConfigurationAndRun(conf)
 }
