cca1aa258e4d4e0085d44642b6968e1bc95b9ead
Dani Louca <59848726+dloucasfx@users.noreply.github.com>
2021-01-26 09:39:01 -0500

    Improve windows performance by fetching all processes name in one call (#1580)
    Signed-off-by: Dani Louca <dlouca@splunk.com>

---
+++ pkg/observers/host/host_others.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package host
+
+import (
+	"github.com/shirou/gopsutil/process"
+)
+
+func (p *processName) getName(proc *process.Process) (string, error) {
+	return proc.Name()
+}
+
+func (p *processName) setPidNameMap() error {
+	return nil
+}
