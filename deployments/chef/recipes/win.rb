38c0701de630252aec62dafa82aeff0e028bb5b6
jcheng-splunk <jeffreyc@splunk.com>
2021-05-04 14:39:43 -0400

    Update agent download/extract on windows

--- deployments/chef/recipes/win.rb
+++ deployments/chef/recipes/win.rb
@@ -1,14 +1,34 @@
-windows_service node['signalfx_agent']['service_name'] do
-  action :stop
-  only_if { ::File.exist?(node['signalfx_agent']['version_file']) && (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version']) }
-end
+if !::File.exist?(node['signalfx_agent']['version_file']) || (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version'])
+  windows_service node['signalfx_agent']['service_name'] do
+    action :stop
+  end
 
-windows_zipfile node['signalfx_agent']['install_dir'] do
-  source node['signalfx_agent']['package_url']
-  action :unzip
-  overwrite true
-  only_if { !::File.exist?(node['signalfx_agent']['version_file']) || (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version']) }
-  notifies :restart, 'service[signalfx-agent]', :delayed
+  if Gem::Requirement.new(">= 15.0").satisfied_by?(Gem::Version.new(Chef::VERSION))
+    tmpdir = Dir.mktmpdir
+    zipname = File.basename(node['signalfx_agent']['package_url'])
+    zippath = "#{tmpdir}\\#{zipname}"
+    remote_file zippath do
+      source node['signalfx_agent']['package_url']
+      action :create
+    end
+    archive_file zippath do
+      destination node['signalfx_agent']['install_dir']
+      action :extract
+      overwrite true
+      notifies :restart, 'service[signalfx-agent]', :delayed
+    end
+    directory tmpdir do
+      action :delete
+      recursive true
+    end
+  else
+    windows_zipfile node['signalfx_agent']['install_dir'] do
+      source node['signalfx_agent']['package_url']
+      action :unzip
+      overwrite true
+      notifies :restart, 'service[signalfx-agent]', :delayed
+    end
+  end
 end
 
 # Make a file that has the current installed version so that we can easily
