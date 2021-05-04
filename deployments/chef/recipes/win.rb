abadc4ab4261399dbdf7c4a7124582013f156fbe
Jeff Cheng <83052155+jcheng-splunk@users.noreply.github.com>
2021-05-04 17:08:41 -0400

    Update agent download/extract on windows (#1753)

--- deployments/chef/recipes/win.rb
+++ deployments/chef/recipes/win.rb
@@ -3,12 +3,33 @@ windows_service node['signalfx_agent']['service_name'] do
   only_if { ::File.exist?(node['signalfx_agent']['version_file']) && (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version']) }
 end
 
-windows_zipfile node['signalfx_agent']['install_dir'] do
-  source node['signalfx_agent']['package_url']
-  action :unzip
-  overwrite true
-  only_if { !::File.exist?(node['signalfx_agent']['version_file']) || (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version']) }
-  notifies :restart, 'service[signalfx-agent]', :delayed
+if !::File.exist?(node['signalfx_agent']['version_file']) || (::File.readlines(node['signalfx_agent']['version_file']).first.strip != node['signalfx_agent']['package_version'])
+  if Gem::Requirement.new('>= 15.0').satisfied_by?(Gem::Version.new(Chef::VERSION))
+    tmpdir = Dir.mktmpdir
+    zipname = File.basename(node['signalfx_agent']['package_url'])
+    zippath = "#{tmpdir}\\#{zipname}"
+    remote_file zippath do
+      source node['signalfx_agent']['package_url']
+      action :create
+    end
+    archive_file zippath do
+      destination node['signalfx_agent']['install_dir']
+      action :extract
+      overwrite true
+      notifies :restart, 'service[signalfx-agent]', :delayed
+    end
+    directory tmpdir do
+      action :delete
+      recursive true
+    end
+  else
+    windows_zipfile node['signalfx_agent']['install_dir'] do
+      source node['signalfx_agent']['package_url']
+      action :unzip
+      overwrite true
+      notifies :restart, 'service[signalfx-agent]', :delayed
+    end
+  end
 end
 
 # Make a file that has the current installed version so that we can easily
