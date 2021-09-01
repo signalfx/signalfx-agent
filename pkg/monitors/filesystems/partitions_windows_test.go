9da04b8eb1a24d3889769cb09a229d77cf9301f7
Bremer Jonathan <bjonathan@splunk.com>
2021-09-01 02:51:56 -0400

    Fix errors

--- pkg/monitors/filesystems/partitions_windows_test.go
+++ pkg/monitors/filesystems/partitions_windows_test.go
@@ -17,7 +17,6 @@ const uninitialized = 999
 const closed = 998
 const compressFlag = uint32(16)     // 0x00000010
 const readOnlyFlag = uint32(524288) // 0x00080000
-//type handle uintptr
 
 type volumeMock struct {
 	name string
@@ -63,6 +62,24 @@ var removableDriveVolume = func() *volumeMock {
 		err:       nil}
 }
 
+func TestGetPartitions_Supersets_gopsutil_PartitionStats(t *testing.T) {
+	// Partition stats from gopsutil are for drive mounts only.
+	want, err := gopsutil.Partitions(true)
+	require.NoError(t, err)
+
+	require.NotEmpty(t, want, "cannot find partition stats using gopsutil")
+
+	var got []gopsutil.PartitionStat
+	// getPartitions includes partition stats for drive and folder mounts.
+	got, err = getPartitions(true)
+	require.NoError(t, err)
+
+	require.NotEmpty(t, got, "cannot find partition stats using getPartitions")
+
+	// Asserting `got` getPartitions stats superset of `want` gopsutil stats.
+	assert.Subset(t, got, want)
+}
+
 func TestGetPartitionsWin(t *testing.T) {
 	type wantType struct {
 		stats    []gopsutil.PartitionStat
@@ -76,7 +93,7 @@ func TestGetPartitionsWin(t *testing.T) {
 		want wantType
 	}{
 		{
-			name: "all partition stats given no errors",
+			name: "should get all partition stats for no errors",
 			volumes: func() *volumesMock {
 				firstVolume, nextVolume1, nextVolume2 := driveVolume(), driveAndFolderVolume(), removableDriveVolume()
 				vols := append(make([]*volumeMock, 0), firstVolume, nextVolume1, nextVolume2)
@@ -93,7 +110,7 @@ func TestGetPartitionsWin(t *testing.T) {
 			},
 		},
 		{
-			name: "no partition stats for all partitions if first volume not found",
+			name: "should get no partition stats if first volume not found",
 			volumes: func() *volumesMock {
 				firstVolume, nextVolume1, nextVolume2 := driveVolume(), driveAndFolderVolume(), removableDriveVolume()
 				firstVolume.err = fmt.Errorf("volume not found")
@@ -107,7 +124,7 @@ func TestGetPartitionsWin(t *testing.T) {
 			},
 		},
 		{
-			name: "no partition stats for next volume partitions not found",
+			name: "should get partition stats for found next volumes",
 			volumes: func() *volumesMock {
 				firstVolume, nextVolume1, nextVolume2 := driveVolume(), driveAndFolderVolume(), removableDriveVolume()
 				nextVolume1.err = fmt.Errorf("volume not found")
@@ -249,78 +266,3 @@ func (v *volumesMock) getVolumeInformationMock(rootPath string, fsFlags *uint32,
 	}
 	return fmt.Errorf("cannot find volume information for volume path %s", rootPath)
 }
-
-//func TestGetAllMounts_ShouldInclude_gopsutil_Mounts(t *testing.T) {
-//	logger := logrus.WithFields(logrus.Fields{"monitorType": monitorType})
-//
-//	// Drive and folder mounts.
-//	got := (&Monitor{logger: logger}).getAllMounts()
-//	require.NotEmpty(t, got, "failed to find any mount points")
-//
-//	// Mounts from gopsutil are for drives only.
-//	want, err := getGopsutilMounts()
-//	require.NoError(t, err)
-//
-//	require.NotEmpty(t, want, "failed to find any mount points using gopsutil")
-//
-//	// Asserting `got` getAllMounts() mounts superset of `want` gopsutil mounts.
-//	assert.Subset(t, got, want)
-//}
-
-//func TestNewStats_SameAs_gopsutil_PartitionStats(t *testing.T) {
-//	// Partition stats from gopsutil are for drive mounts only.
-//	gopsutilStats, err := gopsutil.Partitions(true)
-//	require.NoError(t, err)
-//
-//	require.NotEmpty(t, gopsutilStats, "failed to find any partition stats using gopsutil")
-//
-//	logger := logrus.WithFields(logrus.Fields{"monitorType": monitorType})
-//	monitor := Monitor{logger: logger}
-//
-//	var got []gopsutil.PartitionStat
-//	for _, want := range gopsutilStats {
-//		volPathName, _ := windows.UTF16FromString(want.Mountpoint)
-//		got, err = monitor.getStats(volPathName)
-//		require.NoError(t, err)
-//
-//		// Asserting `got` getStats() stats equal `want` gopsutil stats.
-//		assert.Equal(t, got[0], want)
-//	}
-//}
-
-//VOLUME: \\?\Volume{692d8a75-0000-0000-0000-100000000000}\, PATHS: [C:\]
-//VOLUME: \\?\Volume{bf5d138f-0000-0000-0000-010000000000}\, PATHS: [C:\mnt\testHD\]
-//        \\?\Volume{bf5d138f-0000-0000-0000-010000000000}\
-//VOLUME: \\?\Volume{bf5d0775-0000-0000-0000-010000000000}\, PATHS: [D:\]
-
-func TestGetPartitions_ShouldInclude_gopsutil_PartitionStats(t *testing.T) {
-	// Partition stats from gopsutil are for drive mounts only.
-	want, err := gopsutil.Partitions(true)
-	require.NoError(t, err)
-
-	require.NotEmpty(t, want, "failed to find any partition stats using gopsutil")
-
-	var got []gopsutil.PartitionStat
-	// Partition stats for drive and folder mounts.
-	got, err = getPartitions(true)
-	require.NoError(t, err)
-
-	require.NotEmpty(t, got, "failed to find any partition stats")
-
-	// Asserting `got` getPartitions stats superset of `want` gopsutil stats.
-	assert.Subset(t, got, want)
-}
-
-//func getGopsutilMounts() ([]string, error) {
-//	partitionsStats, err := gopsutil.Partitions(true)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	mounts := make([]string, 0)
-//	for _, stats := range partitionsStats {
-//		mounts = append(mounts, stats.Mountpoint)
-//	}
-//
-//	return mounts, nil
-//}
