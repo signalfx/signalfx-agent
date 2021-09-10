1962916f91b81af961cb8d5686444fa5e88c7e49
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2021-09-10 12:37:07 +0000

    Update collectd-openstack with new config options

--- pkg/monitors/collectd/openstack/openstack.go
+++ pkg/monitors/collectd/openstack/openstack.go
@@ -1,8 +1,11 @@
 package openstack
 
 import (
+	"fmt"
+
 	"github.com/signalfx/signalfx-agent/pkg/core/config"
 	"github.com/signalfx/signalfx-agent/pkg/monitors/collectd"
+	"gopkg.in/yaml.v2"
 
 	"github.com/signalfx/signalfx-agent/pkg/monitors"
 	"github.com/signalfx/signalfx-agent/pkg/monitors/collectd/python"
@@ -40,6 +43,15 @@ type Config struct {
 	UserDomainID string `yaml:"userDomainID"`
 	// Skip SSL certificate validation
 	SkipVerify bool `yaml:"skipVerify"`
+	// The HTTP client timeout in seconds for all requests
+	HTTPTimeout float64 `yaml:"httpTimeout"`
+	// The maximum number of concurrent requests for each metric class
+	RequestBatchSize int `yaml:"requestBatchSize" default:"5"`
+	// Whether to query server metrics (useful to disable for TripleO Undercloud)
+	QueryServerMetrics *bool `yaml:"queryServerMetrics" default:"true"`
+	// Optional search_opts mapping for collectd-openstack Nova client servers.list(search_opts=novaListServerSearchOpts).
+	// For more information see https://docs.openstack.org/api-ref/compute/#list-servers.
+	NovaListServersSearchOpts map[string]string `yaml:"novaListServersSearchOpts"`
 }
 
 // PythonConfig returns the embedded python.Config struct from the interface
@@ -55,21 +67,33 @@ type Monitor struct {
 
 // Configure configures and runs the plugin in collectd
 func (m *Monitor) Configure(conf *Config) error {
+	novaListServersSearchOpts := "{}"
+	if len(conf.NovaListServersSearchOpts) > 0 {
+		marshalled, err := yaml.Marshal(conf.NovaListServersSearchOpts)
+		if err != nil {
+			return fmt.Errorf("failed to parse novaListServersSearchOpts: %w", err)
+		}
+		novaListServersSearchOpts = string(marshalled)
+	}
 	conf.pyConf = &python.Config{
 		ModuleName:    "openstack_metrics",
 		ModulePaths:   []string{collectd.MakePythonPluginPath("openstack")},
 		TypesDBPaths:  []string{collectd.DefaultTypesDBPath()},
 		MonitorConfig: conf.MonitorConfig,
 		PluginConfig: map[string]interface{}{
-			"AuthURL":         conf.AuthURL,
-			"Username":        conf.Username,
-			"Password":        conf.Password,
-			"ProjectName":     conf.ProjectName,
-			"ProjectDomainId": conf.ProjectDomainID,
-			"RegionName":      conf.RegionName,
-			"UserDomainId":    conf.UserDomainID,
-			"SSLVerify":       !conf.SkipVerify,
-			"Interval":        conf.IntervalSeconds,
+			"AuthURL":                   conf.AuthURL,
+			"Username":                  conf.Username,
+			"Password":                  conf.Password,
+			"ProjectName":               conf.ProjectName,
+			"ProjectDomainId":           conf.ProjectDomainID,
+			"RegionName":                conf.RegionName,
+			"UserDomainId":              conf.UserDomainID,
+			"SSLVerify":                 !conf.SkipVerify,
+			"Interval":                  conf.IntervalSeconds,
+			"HTTPTimeout":               conf.HTTPTimeout,
+			"RequestBatchSize":          conf.RequestBatchSize,
+			"QueryServerMetrics":        conf.QueryServerMetrics,
+			"NovaListServersSearchOpts": novaListServersSearchOpts,
 		},
 	}
 
