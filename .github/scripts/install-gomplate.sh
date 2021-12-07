15986f2552dc951ec0954137efbfe564462ed871
nitaliya <nitaliya@splunk.com>
2021-12-07 22:00:39 +0530

    Making suggested changes.

--- .github/scripts/install-gomplate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-sudo apt-get install jq -y
-curl -LOk https://github.com/hairyhenderson/gomplate/releases/download/v3.4.0/gomplate_linux-amd64
-sudo mv gomplate_linux-amd64 /usr/bin/gomplate
-chmod +x /usr/bin/gomplate
