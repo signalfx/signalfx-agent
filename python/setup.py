6042357040f9bd069781fe89a46d983068b0737b
jcheng-splunk <jeffreyc@splunk.com>
2022-01-24 14:21:56 -0500

    Bump sfxpython version to 0.5.0

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 # yapf: disable
 setup(
     name="sfxpython",
-    version="0.4.3",
+    version="0.5.0",
     author="SignalFx, Inc",
     description="Python packages used by the Python extension mechanism of the SignalFx Smart Agent",
     url="https://github.com/signalfx/signalfx-agent",
