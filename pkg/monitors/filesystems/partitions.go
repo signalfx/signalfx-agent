849296e050f1a1f0ef123b08b0db9e7318f79d0e
Dani Louca <59848726+dloucasfx@users.noreply.github.com>
2022-10-17 09:34:29 -0400

    adapting filesystem monitor to the gopsutil changes (#2514)
    Signed-off-by: Dani Louca <dlouca@splunk.com>

--- pkg/monitors/filesystems/partitions.go
+++ pkg/monitors/filesystems/partitions.go
@@ -3,8 +3,22 @@
 
 package filesystems
 
-import gopsutil "github.com/shirou/gopsutil/disk"
+import (
+	"path/filepath"
+
+	gopsutil "github.com/shirou/gopsutil/disk"
+)
 
 func getPartitions(all bool) ([]gopsutil.PartitionStat, error) {
 	return gopsutil.Partitions(all)
+}
+
+// getUsage prepends the hostFSPath to the partition mountpoint. This is needed
+// when reading from a mounted filesystem, ex: container, as the latest GoPsutil
+// now looks into 1/mountinfo and this file does not contain the relative path
+func getUsage(hostFSPath string, path string) (*gopsutil.UsageStat, error) {
+	if hostFSPath != "" {
+		path = filepath.Join(hostFSPath, path)
+	}
+	return gopsutil.Usage(path)
 }
