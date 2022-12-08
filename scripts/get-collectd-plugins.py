ccd998b27918702eaa852c61fd33fbcbe15afc74
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 03:14:00 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/get-collectd-plugins.py
+++ scripts/get-collectd-plugins.py
@@ -35,7 +35,26 @@ url:     {u}""".format(
 
     with contextlib.closing(urllib.request.urlopen(url)) as stream:
         with tarfile.open(fileobj=stream, mode="r|gz") as tar_archive:
-            tar_archive.extractall(TARGET_DIR)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_archive, TARGET_DIR)
             plugin_dir = os.path.join(TARGET_DIR, plugin_name)
             os.rename(os.path.join(TARGET_DIR, tar_archive.getnames()[0]), plugin_dir)
 
